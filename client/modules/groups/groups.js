990101e81515e30501c0275d7433e8ea03c7f26a
Mikhail Ivanov <mmikel@mail.ru>
2022-05-23 16:48:13 +0300

    CRUD (users & groups)

--- client/modules/groups/groups.js
+++ client/modules/groups/groups.js
@@ -63,7 +63,7 @@
                             ],
                         });
                     }
-                    
+
                     return rows;
                 },
                 target: "#mainForm",
