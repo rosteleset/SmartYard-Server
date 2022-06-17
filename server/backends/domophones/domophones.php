a5670131a3d2ad546760bdacef2bd2c2c5e0e698
Mikhail Ivanov <mmikel@mail.ru>
2022-06-17 16:48:47 +0300

    work-in-progress

--- server/backends/domophones/domophones.php
+++ server/backends/domophones/domophones.php
@@ -18,5 +18,55 @@
              * @return false|array
              */
             abstract public function getDomophones();
+
+            /**
+             * @param $enabled
+             * @param $model
+             * @param $version
+             * @param $cms
+             * @param $ip
+             * @param $credentials
+             * @param $callerId
+             * @param $comments
+             * @param $locksDisabled
+             * @param $cmsLevels
+             * @return false|integer
+             */
+            abstract public function addDomophone($enabled, $model, $version, $cms, $ip, $credentials, $callerId, $comments, $locksDisabled, $cmsLevels);
+
+            /**
+             * @param $domophoneId
+             * @param $enabled
+             * @param $model
+             * @param $version
+             * @param $cms
+             * @param $ip
+             * @param $credentials
+             * @param $callerId
+             * @param $comments
+             * @param $locksDisabled
+             * @param $cmsLevels
+             * @return boolean
+             */
+            abstract public function modifyDomophone($domophoneId, $enabled, $model, $version, $cms, $ip, $credentials, $callerId, $comments, $locksDisabled, $cmsLevels);
+
+            /**
+             * @param $domophoneId
+             * @return boolean
+             */
+            abstract public function deleteDomophone($domophoneId);
+
+            /**
+             * @param $domophoneId
+             * @return false|array
+             */
+            abstract public function getCms($domophoneId);
+
+            /**
+             * @param $domophoneId
+             * @param $cms
+             * @return boolean
+             */
+            abstract public function setCms($domophoneId, $cms);
         }
     }
--- server/backends/domophones/internal/internal.php
+++ server/backends/domophones/internal/internal.php
@@ -12,6 +12,64 @@
          */
         class internal extends domophones
         {
+            /**
+             * @inheritDoc
+             */
+            public function getDomophones()
+            {
+                return $this->db->get("select * from domophones order by domophone_id", false, [
+                    "domophone_id" => "domophoneId",
+                    "enabled" => "enabled",
+                    "model" => "model",
+                    "version" => "version",
+                    "cms" => "cms",
+                    "ip" => "ip",
+                    "credentials" => "credentials",
+                    "caller_id" => "callerId",
+                    "comments" => "comments",
+                    "locks_disabled" => "locksDisabled",
+                    "cms_levels" => "cmsLevels"
+                ]);
+            }
 
+            /**
+             * @inheritDoc
+             */
+            public function addDomophone($enabled, $model, $version, $cms, $ip, $credentials, $callerId, $comments, $locksDisabled, $cmsLevels)
+            {
+                // TODO: Implement addDomophone() method.
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function modifyDomophone($domophoneId, $enabled, $model, $version, $cms, $ip, $credentials, $callerId, $comments, $locksDisabled, $cmsLevels)
+            {
+                // TODO: Implement modifyDomophone() method.
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function deleteDomophone($domophoneId)
+            {
+                // TODO: Implement deleteDomophone() method.
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getCms($domophoneId)
+            {
+                // TODO: Implement getCms() method.
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function setCms($domophoneId, $cms)
+            {
+                // TODO: Implement setCms() method.
+            }
         }
     }
