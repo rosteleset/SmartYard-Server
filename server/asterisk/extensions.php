2975d48f7edd9ab12dec9092e95fc8e0a03ea5f0
Mikhail Ivanov <mmikel@mail.ru>
2022-06-21 05:38:16 +0300

    work-in-progress

--- server/asterisk/extensions.php
+++ server/asterisk/extensions.php
@@ -1,3 +1,11 @@
 <?php
 
     // extensions.lua helper
+
+    echo json_encode([
+        "a" => "b",
+    ]);
+
+    $_RAW = json_decode(file_get_contents("php://input"), true);
+
+    error_log(print_r($_RAW, true));
