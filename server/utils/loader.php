32bde915cba2edf7a2f044dc64fa3c6c5cece993
Mikhail Ivanov <mmikel@mail.ru>
2022-07-28 16:52:39 +0300

    work-in-progress

--- server/utils/loader.php
+++ server/utils/loader.php
@@ -16,7 +16,7 @@
             if (@$config["backends"][$backend]) {
                 try {
                     if (file_exists("backends/$backend/$backend.php") && !class_exists("backends\\$backend\\$backend")) {
-                        require_once "backends/$backend/$backend.php";
+                        require_once __DIR__ . "backends/$backend/$backend.php";
                     }
                     require_once __DIR__ . "/backends/$backend/" . $config["backends"][$backend]["backend"] . "/" . $config["backends"][$backend]["backend"] . ".php";
                     $className = "backends\\$backend\\" . $config["backends"][$backend]["backend"];
