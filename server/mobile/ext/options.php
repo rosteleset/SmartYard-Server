9ec4a6c0c2afcf78f986ebc9dcee438a71ba7a5a
Aleksander Vasilev <sbca68@gmail.com>
2022-07-30 21:00:48 +0400

    support phone option

--- server/mobile/ext/options.php
+++ server/mobile/ext/options.php
@@ -10,6 +10,7 @@
     response(200, [
         "cityCams" => "f",
         "payments" => "f",
-        
+        "paymentsUrl" => "https://your.url.of.payments.page", 
+        "supportPhone" => "+7(4752)429999"
     ]);
     
