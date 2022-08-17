cff8a541e0456f47b4011439535797e2eea2054f
Aleksander Vasilev <sbca68@gmail.com>
2022-08-17 19:42:14 +0300

    /call/live

--- server/mobile.php
+++ server/mobile.php
@@ -250,11 +250,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $m = explode('/', $_SERVER["REQUEST_URI"]);
-    if (count($m) == 5 && !$m[0] && $m[1] == 'mobile')
-    $module = $m[2];
-    $method = $m[3];
-    $param = $m[4];    
-    require_once __DIR__ . "/mobile/{$module}/{$method}.php";
+    if (count($m) == 5 && !$m[0] && $m[1] == 'mobile') {
+        $module = $m[2];
+        $method = $m[3];
+        $param = $m[4];    
+        require_once __DIR__ . "/mobile/{$module}/{$method}.php";
+    }
+    
 }
 
 response(404);
---
+++ server/mobile/call/live.php
@@ -0,0 +1,13 @@
+<?php
+$hash = $param;
+$json_camera = @$redis->get("live_" . $hash);
+$camera_params = @json_decode($json_camera, true);
+
+$camera = @loadCamera($camera_params["model"], $camera_params["url"], $camera_params["credentials"]);
+
+if (!$camera) {
+    response(404);
+}
+
+header('Content-Type: image/jpeg');
+echo $camera->camshot();
