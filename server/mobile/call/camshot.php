e741cdf152a9518a60d478ec10a7f13961907824
Mikhail Ivanov <mmikel@mail.ru>
2022-08-22 15:39:15 +0300

    work-in-progress

--- server/mobile/call/camshot.php
+++ server/mobile/call/camshot.php
@@ -1,12 +1,12 @@
 <?php
 
-$hash = $param;
+    $hash = $param;
 
-$img = $redis->get("shot_" . $hash);
+    $img = $redis->get("shot_" . $hash);
 
-if ($img)
-{
-    header('Content-Type: image/jpeg');
-    echo $img;
-    exit;
-}
+    if ($img)
+    {
+        header('Content-Type: image/jpeg');
+        echo $img;
+        exit;
+    }
--- server/mobile/call/live.php
+++ server/mobile/call/live.php
@@ -1,13 +1,16 @@
 <?php
-$hash = $param;
-$json_camera = @$redis->get("live_" . $hash);
-$camera_params = @json_decode($json_camera, true);
 
-$camera = @loadCamera($camera_params["model"], $camera_params["url"], $camera_params["credentials"]);
+    $hash = $param;
+    $json_camera = @$redis->get("live_" . $hash);
+    $camera_params = @json_decode($json_camera, true);
 
-if (!$camera) {
-    response(404);
-}
+    $camera = @loadCamera($camera_params["model"], $camera_params["url"], $camera_params["credentials"]);
 
-header('Content-Type: image/jpeg');
-echo $camera->camshot();
+    if (!$camera) {
+        response(404);
+    }
+
+    header('Content-Type: image/jpeg');
+    echo $camera->camshot();
+
+    exit;
