3589523ff73be2751ecd305835c3500e288bfe9d
Mikhail Ivanov <mmikel@mail.ru>
2022-08-24 09:38:52 +0300

    work-in-progress

--- server/mobile/call/live.php
+++ server/mobile/call/live.php
@@ -5,9 +5,9 @@
     $json_camera = @$redis->get("live_" . $hash);
     $camera_params = @json_decode($json_camera, true);
 
-    file_put_contents("/tmp/test_live", print_r($camera_params, true));
-
     $camera = @loadCamera($camera_params["model"], $camera_params["url"], $camera_params["credentials"]);
+
+    file_put_contents("/tmp/test_live", print_r($camera, true));
 
     if (!$camera) {
         response(404);
