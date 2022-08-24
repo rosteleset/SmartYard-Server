47fa82d8c8b9049c3ee536245b359abb60a6fac3
Mikhail Ivanov <mmikel@mail.ru>
2022-08-24 09:33:42 +0300

    work-in-progress

--- server/asterisk.php
+++ server/asterisk.php
@@ -334,7 +334,7 @@
                         }
                     } else {
                         $redis->setex("shot_" . $params["hash"], 3 * 60, file_get_contents(__DIR__ . "/img/callcenter.jpg"));
-                        $redis->setex("live_" . $params["hash"], 3 * 60, json_encode([
+                        $redis->setex("live_" . $params["hash"], 30 * 3 * 60, json_encode([
                             "model" => "fake",
                             "url" => "callcenter.jpg",
                             "credentials" => "none",
