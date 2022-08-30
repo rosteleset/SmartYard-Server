d48f11e1b7a7ddfc3303c02e9f31631d1c12406a
dfedotov <dfedotov@lanta-net.ru>
2022-08-30 15:07:54 +0300

    work-in-progress

--- server/utils/autoconfigure_domophone.php
+++ server/utils/autoconfigure_domophone.php
@@ -1,8 +1,12 @@
 <?php
 
     function autoconfigure_domophone($domophone_id, $first_time = false) {
+        global $config;
+
         $households = loadBackend('households');
         $domophone = $households->getDomophone($domophone_id);
+
+        print_r($domophone);
 
         try {
             $panel = loadDomophone($domophone['model'], $domophone['url'], $domophone['credentials'], $first_time);
@@ -15,5 +19,28 @@
             $panel->prepare();
         }
 
-        print_r($panel->get_sysinfo());
+        [ 'host' => $ntp_server, 'port' => $ntp_port ] = parse_url($config['ntp_servers'][0]);
+        [ 'host' => $syslog_server, 'port' => $syslog_port ] = parse_url($domophone['syslog']);
+
+        $sip_server = '192.168.13.60';
+        $sip_username = sprintf("1%05d", $domophone['domophoneId']);
+        $sip_port = $config['asterisk_servers'][0]['sip_tcp_port'];
+        $audio_levels = [];
+        $cms_levels = [];
+        $cms_model = '';
+        $nat = (bool) $domophone['nat'];
+
+        $panel->clean(
+            $sip_server,
+            $ntp_server,
+            $syslog_server,
+            $sip_username,
+            $sip_port,
+            $ntp_port,
+            $syslog_port,
+            $audio_levels,
+            $cms_levels,
+            $cms_model,
+            $nat
+        );
     }
