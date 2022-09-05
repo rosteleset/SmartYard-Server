68f5853983759deb3668b01b39fd38c041bd5d20
Aleksei Menshikh <amenshih@lanta-net.ru>
2022-09-05 17:20:00 +0300

    API метод address/registerQR

--- server/mobile/address/registerQR.php
+++ server/mobile/address/registerQR.php
@@ -15,8 +15,46 @@
  * @apiParam {String} QR QR код
  */
 
-    auth();
-    response(200, "");
+auth();
+$code = trim(@$postdata['QR']);
+if (!$code) {
+    response(404);
+}
+
+//полагаем, что хэш квартиры является суффиксом параметра QR
+$hash = '';
+for ($i = strlen($code) - 1; $i >= 0; --$i)
+{
+    if (!in_array($code[$i], ['/', '=', "_"]))
+        $hash = $code[$i] . $hash;
+    else
+        break;
+}
+
+if ($hash == '') {
+    response(200, "QR-код не является кодом для доступа к квартире");
+}
+
+$households = loadBackend("households");
+$flat = $households->getFlats("code", ["code" => $hash])[0];
+if (!$flat) {
+    response(200, "QR-код не является кодом для доступа к квартире");
+}
+
+$flat_id = (int)$flat["flatId"];
+
+//проверка регистрации пользователя в квартире
+foreach($subscriber['flats'] as $item) {
+    if ((int)$item['flatId'] == $flat_id) {
+        response(200, "У вас уже есть доступ к данной квартире");
+    }
+}
+
+if ($households->addSubscriber($subscriber["mobile"], null, null, $flat_id)) {
+    response(200, "Ваш запрос принят и будет обработан в течение одной минуты, пожалуйста подождите");
+} else {
+    response(422);
+}
 
 /*
     $phone = $bearer['id'];
