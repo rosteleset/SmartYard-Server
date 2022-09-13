d1777d77dd7fe15bd04ddf7c44b2e4000e4c8e32
dfedotov <dfedotov@lanta-net.ru>
2022-09-13 11:12:02 +0300

    work-in-progress

--- server/utils/autoconfigure_domophone.php
+++ server/utils/autoconfigure_domophone.php
@@ -1,21 +1,25 @@
 <?php
 
-    function autoconfigure_domophone($domophone_id, $first_time = false) {
+    function autoconfigure_domophone($domophoneId, $firstTime = false) {
         global $config;
 
         $households = loadBackend('households');
-        $domophone = $households->getDomophone($domophone_id);
+        $domophone = $households->getDomophone($domophoneId);
+        $entrance = $households->getEntrances('domophoneId', [ 'domophoneId' => $domophoneId, 'output' => '0' ])[0];
+        $asterisk_server = $households->getAsteriskServer($domophoneId);
 
+        print_r($entrance);
+        print_r($asterisk_server);
         print_r($domophone);
 
         try {
-            $panel = loadDomophone($domophone['model'], $domophone['url'], $domophone['credentials'], $first_time);
+            $panel = loadDomophone($domophone['model'], $domophone['url'], $domophone['credentials'], $firstTime);
         } catch (Exception $e) {
             echo $e->getMessage() . "\n";
             exit(1);
         }
 
-        if ($first_time) {
+        if ($firstTime) {
             $panel->prepare();
         }
 
@@ -23,11 +27,11 @@
         [ 'host' => $syslog_server, 'port' => $syslog_port ] = parse_url($domophone['syslog']);
 
         $sip_username = sprintf("1%05d", $domophone['domophoneId']);
-        $sip_server = $config['asterisk_servers'][0]['ip'];
-        $sip_port = $config['asterisk_servers'][0]['sip_tcp_port'];
+        $sip_server = $asterisk_server['ip'];
+        $sip_port = $asterisk_server['sip_tcp_port'];
 
         $nat = (bool) $domophone['nat'];
-        [, $stun_server, $stun_port ] = explode(':', $config['asterisk_servers'][0]['stun_server']);
+        [, $stun_server, $stun_port ] = explode(':', $asterisk_server['stun_server']);
 
         $audio_levels = [];
         $cms_levels = [];
@@ -48,4 +52,6 @@
             $stun_server,
             $stun_port
         );
+
+        $panel->keep_doors_unlocked($entrance['locksDisabled']);
     }
