99c5a446a5637ab6830797349c4c67f7a09f33d1
Mikhail Ivanov <mmikel@mail.ru>
2022-10-06 12:56:48 +0300

    work-in-progress

--- server/sql/sqlite/v1_inbox.sql
+++ server/sql/sqlite/v1_inbox.sql
@@ -1,20 +1,15 @@
 -- inbox
 CREATE TABLE inbox
 (
-    msg_id integer not null primary key autoincrement,
-    id text not null,
-    date text,
-    msg text,
-    action text,
-    bulk integer,
-    push_only integer,
-    push integer,
-    cascad integer,
-    delivered integer,
-    readed integer,
-    sms integer,
-    force_sms integer,
-    ext_id text,
-    archive integer,
-    code text
+    msg_id integer not null primary key autoincrement ,
+    id test not null,                                                                                                   -- phone number
+    date test,                                                                                                          -- send date
+    msg text,                                                                                                           -- message
+    action text,                                                                                                        -- application action (money, new_address, ...)
+    expire integer,                                                                                                     -- when need to move to archive or delete (time())
+    bulk integer,                                                                                                       -- bulk message (low priority send)
+    delivered integer,                                                                                                  -- delivered
+    readed integer,                                                                                                     -- readed
+    ext_id text,                                                                                                        -- external id from google, smssending, etc...
+    code character varying                                                                                              -- send code from google, smssending, etc...
 );
