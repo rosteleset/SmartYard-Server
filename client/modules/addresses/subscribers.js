fe11849e974eafb21774adebc7bc1eea4656e55d
Mikhail Ivanov <mmikel@mail.ru>
2022-10-18 14:14:37 +0300

    work-in-progress

--- client/modules/addresses/subscribers.js
+++ client/modules/addresses/subscribers.js
@@ -5,6 +5,8 @@
     },
 
     doAddSubscriber: function (subscriber) {
+        subscriber.title = i18n("addresses.addFlatTtitle");
+        subscriber.msg = i18n("addresses.addFlatMsg");
         loadingStart();
         POST("subscribers", "subscriber", false, subscriber).
         fail(FAIL).
