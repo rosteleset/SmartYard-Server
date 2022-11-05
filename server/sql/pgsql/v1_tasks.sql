1ad6cd04118f21166653adebb31b7ce80a808e52
Mikhail Ivanov <mmikel@mail.ru>
2022-11-05 05:11:51 +0300

    work-in-progress

--- server/sql/pgsql/v1_tasks.sql
+++ server/sql/pgsql/v1_tasks.sql
@@ -1,17 +1,17 @@
--- tasks
-CREATE TABLE tasks
+-- tasks_changes
+CREATE TABLE tasks_changes
 (
-    task_id serial primary key,
+    task_change_id serial primary key,
+    object_type character varying,
+    object_id integer
+);
+
+CREATE TABLE tasks_queue
+(
+    task_queue_id serial primary key,
     object_type character varying,
     object_id integer,
     task character varying,
     params character varying
-);
-
-CREATE TABLE tasks_config_queue
-(
-    task_queue_id serial primary key,
-    object_type character varying,
-    object_id integer
 );
 CREATE UNIQUE INDEX tasks_config_queue_uniq on tasks_config_queue(object_type, object_id);
--- server/sql/sqlite/v1_tasks.sql
+++ server/sql/sqlite/v1_tasks.sql
@@ -1,17 +1,17 @@
--- tasks
-CREATE TABLE tasks
+-- tasks_changes
+CREATE TABLE tasks_changes
 (
-    task_id integer not null primary key autoincrement,
+    task_change_id integer not null primary key autoincrement,
+    object_type text,
+    object_id integer
+);
+
+CREATE TABLE tasks_queue
+(
+    task_queue_id integer not null primary key autoincrement,
     object_type text,
     object_id integer,
     task text,
     params text
-);
-
-CREATE TABLE tasks_config_queue
-(
-    task_queue_id integer not null primary key autoincrement,
-    object_type text,
-    object_id integer
 );
 CREATE UNIQUE INDEX tasks_config_queue_uniq on tasks_config_queue(object_type, object_id);
