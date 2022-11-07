e2d7fa2d7601b3be987712609c42e34aff3d6f87
Mikhail Ivanov <mmikel@mail.ru>
2022-11-07 13:12:56 +0300

    work-in-progress

--- server/sql/pgsql/v1_houses.sql
+++ server/sql/pgsql/v1_houses.sql
@@ -11,7 +11,7 @@ CREATE TABLE houses_domophones
     dtmf character varying not null,
     first_time integer default 1,
     nat integer,
-    locks_are_open integer,
+    locks_are_open integer default 1,
     comment character varying
 );
 CREATE UNIQUE INDEX domophones_ip_port on houses_domophones(url);
