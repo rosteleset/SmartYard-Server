c27ae37d1df5eee600b59ad06a002fb53a30ca91
Aleksei Menshikh <amenshih@lanta-net.ru>
2022-11-09 15:44:54 +0300

    Изменения из-за новой структуры таблицы plog.

--- server/backends/events/internal/internal.php
+++ server/backends/events/internal/internal.php
@@ -11,6 +11,7 @@ namespace backends\events
     class internal extends events
     {
         protected $con;
+        protected $image_url_prefix;
 
         public function __construct($config, $db, $redis)
         {
@@ -23,14 +24,15 @@ namespace backends\events
                 "default",
                 $config["clickhouse"]["port"]
             );
+            $this->image_url_prefix = $config["backends"]["events"]["image_url_prefix"];
         }
 
         public function getEventsDays(int $flat_id, $filter_events)
         {
             if ($filter_events) {
-                $query = "select toYYYYMMDD(date) as day, count(date) as events from plog where not hidden and flat_id = $flat_id and object_type = 0 and event in ($filter_events) group by toYYYYMMDD(date) order by toYYYYMMDD(date) desc";
+                $query = "select toYYYYMMDD(date) as day, count(date) as events from plog where not hidden and flat_id = $flat_id and event in ($filter_events) group by toYYYYMMDD(date) order by toYYYYMMDD(date) desc";
             } else {
-                $query = "select toYYYYMMDD(date) as day, count(date) as events from plog where not hidden and flat_id = $flat_id and object_type = 0 group by toYYYYMMDD(date) order by toYYYYMMDD(date) desc";
+                $query = "select toYYYYMMDD(date) as day, count(date) as events from plog where not hidden and flat_id = $flat_id group by toYYYYMMDD(date) order by toYYYYMMDD(date) desc";
             }
 
             $resp = mysqli_fetch_all($this->con->query($query), MYSQLI_ASSOC);
@@ -46,28 +48,33 @@ namespace backends\events
 
         public function getDetailEventsByDay(int $flat_id, string $date)
         {
-            $query = "select * from plog where not hidden and toYYYYMMDD(date) = '$date' and flat_id = $flat_id and object_type = 0 order by date desc";
+            $query = "select date, event_uuid, hidden, image_uuid, flat_id, domophone_id, domophone_output, domophone_output_description, event, opened, toJSONString(face) face, rfid, code, user_phone, gate_phone, preview from plog where not hidden and toYYYYMMDD(date) = '$date' and flat_id = $flat_id order by date desc";
             $resp = mysqli_fetch_all($this->con->query($query), MYSQLI_ASSOC);
             $events_details = [];
             foreach ($resp as &$row) {
                 $e_details = [];
                 $e_details['date'] = $row['date'];
-                $e_details['uuid'] = $row['uuid'];
+                $e_details['uuid'] = $row['event_uuid'];
+                $e_details['image'] = $row['image_uuid'];
                 $e_details['previewType'] = $row['preview'];
-                $e_details['preview'] = $row['image'];
-                $e_details['objectId'] = $row['object_id'];
-                $e_details['objectType'] = $row['object_type'];
-                $e_details['objectMechanizma'] = $row['object_mechanizma'];
-                $e_details['mechanizmaDescription'] = $row['mechanizma_description'];
-                if ($row['face_width'] > 0 && $row['face_height']) {
+                $e_details['objectId'] = $row['domophone_id'];
+                $e_details['objectType'] = 0;
+                $e_details['objectMechanizma'] = $row['domophone_output'];
+                $e_details['mechanizmaDescription'] = $row['domophone_output_description'];
+                $e_details['event'] = $row['event'];
+                $face = json_decode($row['face']);
+                if ($face->width && $face->height) {
                     $e_details['detailX']['face'] = [
-                        'left' => $row['face_left'],
-                        'top' => $row['face_top'],
-                        'width' => $row['face_width'],
-                        'height' => $row['face_height']
+                        'left' => $face->left,
+                        'top' => $face->top,
+                        'width' => $face->width,
+                        'height' => $face->height
                     ];
                 }
-                $e_details['event'] = $row['event'];
+                if ($face->faceId) {
+                    $e_details['detailX']['faceId'] = $face->faceId;
+                }
+
                 switch ((int)$row['event']) {
                     case 1:
                     case 2:
@@ -75,15 +82,14 @@ namespace backends\events
                         break;
 
                     case 3:
-                        $e_details['detailX']['key'] = $row['rfid_key'];
+                        $e_details['detailX']['key'] = $row['rfid'];
                         break;
 
                     case 4:
-                        $e_details['detailX']['phone'] = $row['phone'];
+                        $e_details['detailX']['phone'] = $row['user_phone'];
                         break;
 
                     case 5:
-                        $e_details['detailX']['faceId'] = $row['face_id'];
                         break;
 
                     case 6:
@@ -91,9 +97,14 @@ namespace backends\events
                         break;
 
                     case 7:
-                        $e_details['detailX']['phoneFrom'] = $row['phone_from'];
-                        $e_details['detailX']['phoneTo'] = $row['phone_to'];
+                        $e_details['detailX']['phoneFrom'] = $row['user_phone'];
+                        $e_details['detailX']['phoneTo'] = $row['gate_phone'];
                         break;
+                }
+                if ((int)$row['preview']) {
+                    $date = explode('-', explode(' ', $row['date'])[0]);
+                    $url = "{$this->image_url_prefix}{$date[0]}-{$date[1]}-{$date[2]}/{$row['image_uuid'][0]}/{$row['image_uuid'][1]}/{$row['image_uuid'][2]}/{$row['image_uuid'][3]}/{$row['image_uuid']}.jpg";
+                    $e_details['preview'] = $url;
                 }
 
                 $events_details[] = $e_details;
