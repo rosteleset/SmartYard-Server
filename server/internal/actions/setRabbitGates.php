f60a7b2d950be31a6dcff7a90ea2535a317a9391
Anton Kulakov <anton.kulakoff@ya.ru>
2022-12-01 13:41:56 +0300

    Изменения в методе setRabbitGates internal api

--- server/internal/actions/setRabbitGates.php
+++ server/internal/actions/setRabbitGates.php
@@ -7,29 +7,21 @@
         "apartment" => $apartment
     ] = $postdata;
 
-    $query = 'SELECT house_flat_id, last_opened, white_rabbit 
-                FROM houses_flats 
-                WHERE  flat= :flat AND white_rabbit > 0 AND address_house_id = (
-                SELECT address_house_id from houses_houses_entrances 
-                WHERE prefix= :prefix AND house_entrance_id = (
-                SELECT house_entrance_id FROM houses_domophones LEFT JOIN houses_entrances USING (house_domophone_id) 
-                WHERE ip = :ip AND entrance_type = "wicket"))';
+
+    $query = 'UPDATE houses_flats SET last_opened = :last_opened 
+        WHERE  flat= :flat AND white_rabbit > 0 AND address_house_id = (
+        SELECT address_house_id from houses_houses_entrances 
+        WHERE prefix= :prefix AND house_entrance_id = (
+        SELECT house_entrance_id FROM houses_domophones LEFT JOIN houses_entrances USING (house_domophone_id) 
+        WHERE ip = :ip AND entrance_type = "wicket"))';
     $params = [
-        "ip" => $ip,
-        "flat" => $apartment,
-        "prefix" => $prefix,
-    ];
-    [
-        0 => [
-            'house_flat_id' => $house_flat_id,
-            "last_opened" => $last_opened,
-            "white_rabbit" => $white_rabbit
-        ]
-    ] = $db->get($query, $params);
+            "ip" => $ip,
+            "flat" => $apartment,
+            "prefix" => $prefix,
+            "last_opened" => $date
+        ];
 
-    $query_update = "UPDATE houses_flats SET last_opened = :last_opened WHERE house_flat_id= :house_flat_id ";
-    $params_update = ["last_opened" => $date, "house_flat_id" => $house_flat_id];
-    $res_update = $db->modify($query_update, $params_update);
+    $result = $db->modify($query, $params);
 
-    response(202, ["id" => $res_update]);
+    response(202, ["id" => $result]);
     exit();
