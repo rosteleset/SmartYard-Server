9c19387e41d5fe6b9992bdd0cad72fe3382ee4a4
Mikhail Ivanov <mmikel@mail.ru>
2022-12-10 09:08:13 +0300

    EoK

--- server/backends/isdn/lanta/lanta.php
+++ server/backends/isdn/lanta/lanta.php
@@ -70,9 +70,9 @@
                     $query = substr($query, 0, -1);
                 }
 
-                $result = file_get_contents("https://isdn.lanta.me/isdn_api.php?action=push&secret=" . $this->config["backends"]["isdn"]["common_secret"] . "&" . $query);
+                $result = trim(file_get_contents("https://isdn.lanta.me/isdn_api.php?action=push&secret=" . $this->config["backends"]["isdn"]["common_secret"] . "&" . $query));
 
-                if (strtolower(trim($result).split(":", 0)) !== "ok") {
+                if (strtolower(explode(":", $result)[0]) !== "ok") {
                     loadBackend("households")->dismissToken($push["token"]);
                 }
 
