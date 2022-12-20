353905d28996b83252eee862067e14d75eaf2235
Aleksander Vasilev <sbca68@gmail.com>
2022-12-20 23:15:28 +0300

    test

--- server/mobile/cctv/download.php
+++ server/mobile/cctv/download.php
@@ -15,13 +15,11 @@ $files = loadBackend("files");
 $stream = $files->getFileStream($fileName);
 $info = $files->getFileInfo($fileName);
 
-print_r($info);
-
-/*header("Content-type: $contentType");
+header("Content-type: $contentType");
 header("Content-Disposition: attachment; filename=$fileName");
 
 $begin  = 0;
-$size = strlen($body);
+$size = $info['length'];
 $end  = $size - 1;
 
 if (isset($_SERVER['HTTP_RANGE'])) {
@@ -36,13 +34,30 @@ if (isset($_SERVER['HTTP_RANGE'])) {
 } else {
     header('HTTP/1.1 200 OK');
 }
-
+$new_length = $end - $begin + 1;
 header('Cache-Control: public, must-revalidate, max-age=0');
 header('Pragma: no-cache');
 header('Accept-Ranges: bytes');
-header('Content-Length:' . ($size - $begin));
+header('Content-Length:' . $new_length);
 header('Content-Transfer-Encoding: binary');
 
-echo substr($body, $begin, $size - $begin);
-*/
+$chunksize = 1024*1024; //you may want to change this
+$bytes_send = 0;
+if ($stream) {
+    if (isset($_SERVER['HTTP_RANGE'])) {
+        fseek($stream, $range);
+    }
+
+    while (!feof($stream) && (!connection_aborted()) && ($bytes_send < $new_length) ) {
+        $buffer = fread($stream, $chunksize);
+        echo($buffer);
+        flush();
+        usleep($this->sec * 1000000);
+        $bytes_send += strlen($buffer);
+    }
+    fclose($stream);
+} else {
+    throw new \Exception('Error - can not open file.');
+}
+
 exit(0);
