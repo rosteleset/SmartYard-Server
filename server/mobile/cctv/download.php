b34c6e5bd9505c60f0cf1e9bb192aadad145aca1
Aleksander Vasilev <sbca68@gmail.com>
2022-12-20 20:20:13 +0300

    download

---
+++ server/mobile/cctv/download.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @api {get} /cctv/download скачать файл
+ * @apiVersion 1.0.0
+ * @apiDescription ***почти готов***
+ *
+ * @apiGroup CCTV
+ *
+ * @apiParam {String} id идентификатор файла
+ */
+
+echo $param;
