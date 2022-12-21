c1b5d76174c696472607edfa5b321e6947a3b18a
Mikhail Ivanov <mmikel@mail.ru>
2022-12-21 12:02:21 +0300

    WiP

--- server/backends/households/internal/internal.php
+++ server/backends/households/internal/internal.php
@@ -143,6 +143,47 @@
                         ];
                         break;
 
+                    case "apartment":
+                        // houses_entrances_flats
+                        $q = "
+                            select
+                                house_flat_id
+                            from
+                                houses_entrances_flats
+                            where
+                                house_flat_id in (
+                                    select
+                                        house_flat_id
+                                    from
+                                        houses_flats
+                                    where
+                                            address_house_id in (
+                                            select
+                                                address_house_id
+                                            from
+                                                houses_houses_entrances
+                                            where
+                                                    house_entrance_id in (
+                                                    select
+                                                        house_entrance_id
+                                                    from
+                                                        houses_entrances
+                                                    where
+                                                        house_domophone_id = :house_domophone_id
+                                                )
+                                        )
+                                )
+                                and
+                                apartment = :apartment
+                                group by
+                                    house_flat_id
+                        ";
+                        $p = [
+                            "house_domophone_id" => $params["domophoneId"],
+                            "apartment" => $params["flatNumber"],
+                        ];
+                        break;
+
                     case "code":
                         $q = "
                             select
