940d96543a8b15627878f905d3db8f083aeac279
Mikhail Ivanov <mmikel@mail.ru>
2022-12-22 12:35:47 +0300

    WiP

--- server/utils/i18n.php
+++ server/utils/i18n.php
@@ -1,35 +1,39 @@
 <?php
 
-    function i18n($msg, ...$args) {
+    function i18n($lang, $msg, ...$args) {
         try {
-            t = explode(".", msg);
-            if (t.length > 2) {
-                let t_ = [];
-                t_[0] = t.shift();
-                t_[1] = t.join(".");
-                t = t_;
+            $lang = json_decode(file_get_contents(__DIR__ . "../i18n/$lang.json"), true);
+        } catch (\Exception $e) {
+            $lang = [];
+        }
+        try {
+            $t = explode(".", msg);
+            if (count($t) > 2) {
+                $st = [];
+                $st[0] = array_shift($t);
+                $st[1] = implode(".", $t);
+                $t = $st;
             }
-            let loc;
-            if (t.length === 2) {
-                loc = lang[t[0]][t[1]];
+            if (count($t) === 2) {
+                $loc = $lang[$t[0]][$t[1]];
             } else {
-                loc = lang[t[0]];
+                $loc = $lang[$t[0]];
             }
-            if (loc) {
-                if (typeof loc === "object" && Array.isArray(loc)) {
-                    loc = nl2br(loc.join("\n"));
+            if ($loc) {
+                if (is_array($loc)) {
+                    $loc = nl2br(implode(".", $loc));
                 }
-                loc = sprintf(loc, ...args);
+                $loc = sprintf($loc, ...$args);
             }
-            if (!loc) {
-                if (t[0] === "errors") {
-                    return t[1];
+            if (!$loc) {
+                if ($t[0] === "errors") {
+                    return $t[1];
                 } else {
-                    return msg;
+                    return $msg;
                 }
             }
-            return loc;
-        } catch (_) {
-            return msg;
+            return $loc;
+        } catch (\Exception $e) {
+            return $msg;
         }
     }
