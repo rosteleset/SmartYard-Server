574ec3f23d577b366db3ea01efc7281eac3215a9
dfedotov <dfedotov@lanta-net.ru>
2022-12-30 15:32:54 +0300

    Syslog refactoring

--- server/syslog/is.js
+++ server/syslog/is.js
@@ -1,7 +1,7 @@
 const syslog = new (require("syslog-server"))();
 const { hw: { is } } = require("./config.json");
 const { getTimestamp } = require("./utils/formatDate");
-const { urlParser } = require("./utils/url_parser");
+const { urlParser } = require("./utils/urlParser");
 const API = require("./utils/api");
 const { mdTimer } = require("./utils/mdTimer");
 const { port } = urlParser(is);
@@ -11,41 +11,41 @@ const gateRabbits = [];
 let lastCallsDone = {};
 
 syslog.on("message", async ({ date, host, message }) => {
-    const now = parseInt(getTimestamp(date));
-    const is_msg = message.split(" - - ")[1].trim();
+    const now = getTimestamp(date);
+    const isMsg = message.split("- -")[1].trim();
 
-    // Фильтр сообщений, не несущих смысловой нагрузки
+    // Spam messages filter
     if (
-        !is_msg ||
-        is_msg.indexOf("STM32.DEBUG") >= 0 ||
-        is_msg.indexOf("Вызов метода") >= 0 ||
-        is_msg.indexOf("Тело запроса") >= 0 ||
-        is_msg.indexOf("libre") >= 0 ||
-        is_msg.indexOf("ddns") >= 0 ||
-        is_msg.indexOf("DDNS") >= 0 ||
-        is_msg.indexOf("Загружена конфигурация") >= 0 ||
-        is_msg.indexOf("Interval") >= 0 ||
-        is_msg.indexOf("[Server]") >= 0 ||
-        is_msg.indexOf("Proguard start") >= 0
+        !isMsg ||
+        isMsg.indexOf("STM32.DEBUG") >= 0 ||
+        isMsg.indexOf("Вызов метода") >= 0 ||
+        isMsg.indexOf("Тело запроса") >= 0 ||
+        isMsg.indexOf("libre") >= 0 ||
+        isMsg.indexOf("ddns") >= 0 ||
+        isMsg.indexOf("DDNS") >= 0 ||
+        isMsg.indexOf("Загружена конфигурация") >= 0 ||
+        isMsg.indexOf("Interval") >= 0 ||
+        isMsg.indexOf("[Server]") >= 0 ||
+        isMsg.indexOf("Proguard start") >= 0
     ) {
         return;
     }
 
-    console.log(`${now} || ${host} || ${is_msg}`);
+    console.log(`${now} || ${host} || ${isMsg}`);
 
-    // Отправка сообщения в syslog storage
-    await API.sendLog({ date: now, ip: host, unit: "is", msg: is_msg });
+    // Send message to syslog storage
+    await API.sendLog({ date: now, ip: host, unit: "is", msg: isMsg });
 
-    // Детектор движения: старт
-    if (is_msg.indexOf("EVENT: Detected motion") >= 0) {
-        await API.motionDetection({ date: now, ip: host, motionStart: true });
+    // Motion detection: start
+    if (isMsg.indexOf("EVENT: Detected motion") >= 0) {
+        await API.motionDetection({ date: now, ip: host, motionActive: true });
         await mdTimer(host, 5000);
     }
 
-    // Вызов квартиры в режиме калитки с префиксом
-    if (/^Calling to \d+ house \d+ flat/.test(is_msg)) {
-        const house = is_msg.split("to")[1].split("house")[0].trim();
-        const flat = is_msg.split("house")[1].split("flat")[0].trim();
+    // Call in gate mode with prefix: potential white rabbit
+    if (/^Calling to \d+ house \d+ flat/.test(isMsg)) {
+        const house = isMsg.split("to")[1].split("house")[0].trim();
+        const flat = isMsg.split("house")[1].split("flat")[0].trim();
 
         gateRabbits[host] = {
             ip: host,
@@ -54,33 +54,33 @@ syslog.on("message", async ({ date, host, message }) => {
         };
     }
 
-    // Открытие двери DTMF кодом
-    if (is_msg.indexOf("Open main door by DTMF") >= 0) {
+    // Incoming DTMF for white rabbit: sending rabbit gate update
+    if (isMsg.indexOf("Open main door by DTMF") >= 0) {
         if (gateRabbits[host]) {
             const { ip, prefix, apartment } = gateRabbits[host];
             await API.setRabbitGates({ date: now, ip, prefix, apartment });
         }
     }
 
-    // Открытие двери RFID ключом
-    if (/^Opening door by RFID [a-fA-F0-9]+, apartment \d+$/.test(is_msg)) {
-        const rfid = is_msg.split("RFID")[1].split(",")[0].trim();
+    // Opening door by RFID key
+    if (/^Opening door by RFID [a-fA-F0-9]+, apartment \d+$/.test(isMsg)) {
+        const rfid = isMsg.split("RFID")[1].split(",")[0].trim();
         await API.openDoor({ date: now, ip: host, detail: rfid, by: "rfid" });
     }
 
-    // Открытие двери персональным кодом
-    if (is_msg.indexOf("Opening door by code") >= 0) {
-        const code = parseInt(is_msg.split("code")[1].split(",")[0]);
+    // Opening door by personal code
+    if (isMsg.indexOf("Opening door by code") >= 0) {
+        const code = parseInt(isMsg.split("code")[1].split(",")[0]);
         await API.openDoor({ date: now, ip: host, detail: code, by: "code" });
     }
 
-    // Открытие двери кнопкой
-    if (is_msg.indexOf("Main door button press") >= 0) {
+    // Opening door by button pressed
+    if (isMsg.indexOf("Main door button press") >= 0) {
         await API.openDoor({ date: now, ip: host, door: 0, detail: "main", by: "button" });
     }
 
-    // Все вызовы завершены
-    if (is_msg.indexOf("All calls are done for apartment") >= 0 || is_msg.indexOf("UART_EVENT_BYE") >= 0) {
+    // All calls are done
+    if (isMsg.indexOf("All calls are done for apartment") >= 0 || isMsg.indexOf("UART_EVENT_BYE") >= 0) {
         if (!lastCallsDone[host] || now - lastCallsDone[host] > 1) {
             lastCallsDone[host] = now
             await API.callFinished({ date: now, ip: host });
