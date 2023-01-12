c8ab3f88e630d985f0139a38e42e63ee054880ff
Anton Kulakov <anton.kulakoff@ya.ru>
2023-01-12 15:43:54 +0300

    Internal API bugfixes

--- server/internal/actions/motionDetection.php
+++ server/internal/actions/motionDetection.php
@@ -2,19 +2,30 @@
     /** Get ip motion detection device, find stream_id and frs url.
      * Send POST request to FRS.
      */
+    if (!isset($postdata["date"], $postdata["ip"], $postdata["motionActive"])) {
+        response(404);
+        exit();
+    }
+
     [
         "date" => $date,
         "ip" => $ip,
         "motionActive" => $motionActive
     ] = $postdata;
 
-    $query = 'SELECT camera_id, frs FROM cameras WHERE ip = :ip';
-    $params = ["ip" => $ip];
+    $query = 'SELECT camera_id, frs FROM cameras WHERE frs != :frs AND ip = :ip';
+    $params = ["ip" => $ip, "frs" => "-"];
+    $result = $db->get($query, $params, []);
+
+    if ( !$result ) {
+        response(200, "FRS not enabled on this stream");
+        exit();
+    }
 
     [0 => [
         "camera_id" => $streamId,
         "frs" => $frsUrl
-    ]] = $db->get($query, $params, []);
+    ]] = $result;
 
     $payload = ["streamId" => $streamId, "start" => $motionActive ? "t" : "f"];
 
@@ -23,5 +34,4 @@
         response(201, $apiResponse);
     }
 
-    response(200);
     exit();
