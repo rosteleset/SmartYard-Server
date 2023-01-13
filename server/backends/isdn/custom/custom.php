dc61818f4bb3b73bab01be52892578571869ff89
Aleksander Vasilev <sbca68@gmail.com>
2023-01-13 11:49:05 +0300

    implementation of backend for sms-code sending via external sms-provider

--- server/backends/isdn/custom/custom.php
+++ server/backends/isdn/custom/custom.php
@@ -12,25 +12,59 @@
          */
 
         require_once __DIR__ . "/../teledome/push.php";
-        require_once __DIR__ . "/../teledome/flashCall.php";
-        require_once __DIR__ . "/../teledome/sms.php";
-        require_once __DIR__ . "/../teledome/incoming.php";
-
-        class lanta extends isdn
+       
+        class custom extends isdn
         {
-            use push, flashCall, incoming;
+            use push;
+            
+            /**
+             * @inheritDoc
+             */
+            function flashCall($id)
+            {
+                // write your own implementation if you will use it!
+                return false;
+            }
 
+            /**
+             * @inheritDoc
+             */
+            function getCode($id)
+            {
+                // write your own implementation if you will use it!
+                return false;
+            }
+            
+            /**
+             * @inheritDoc
+             */
+            function confirmNumbers()
+            {
+                // write your own implementation if you will use it!
+                return [
+                ];
+            }
+
+            /**
+             * @inheritDoc
+             */
+            function checkIncoming($id)
+            {
+                // write your own implementation if you will use it!
+                return false;
+            }
             /**
              * @inheritDoc
              */
             function sendCode($id)
             {
+                // Example implementation of backend for sms-code sending via external sms-provider
                 $phone = $id;
                 $pin = sprintf("%04d", rand(0, 9999));
                 $msg = "Ваш код подтверждения: $pin";
                 $login = "your_login";
                 $password = "your_password";
-                echo "$pin:" . trim(@file_get_contents("https://your.sms.provider/sendsms.php?login=$login&password=$password&phone=$phone&text=".urlencode($msg)));
+                return "$pin:" . trim(@file_get_contents("https://your.sms.provider/sendsms.php?login=$login&password=$password&phone=$phone&text=".urlencode($msg)));
             }
         }
     }
