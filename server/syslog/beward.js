19c564d06843928d54f093d252dfb6adcf2f6396
Anton Kulakov <anton.kulakoff@ya.ru>
2023-01-16 17:49:02 +0300

    bugfix beward setRabbitGates, update install/00.jammy.md

--- server/syslog/beward.js
+++ server/syslog/beward.js
@@ -10,6 +10,9 @@ const { port } = urlParser(board);
 const gateRabbits = [];
 
 syslog.on("message", async ({date, host, message}) => {
+    console.log("|| 0 || DEBUG: start ||");
+    console.log(gateRabbits);
+    
     const now = getTimestamp(date);
     const bwMsg = message.split("- -")[1].trim();
 
@@ -47,19 +50,23 @@ syslog.on("message", async ({date, host, message}) => {
     // Call in gate mode with prefix: potential white rabbit
     if (bwMsg.indexOf("Redirecting CMS call to") >= 0) {
         const dst = bwMsg.split("to")[1].split("for")[0];
-
+        console.log("|| DEBUG: Redirecting CMS ||");
+        console.log(gateRabbits);
         gateRabbits[host] = {
             ip: host,
-            prefix: parseInt(dst.substring(0, 4)),
-            apartment: parseInt(dst.substring(4)),
+            prefix: parseInt(dst.substring(0, 5)),
+            apartment: parseInt(dst.substring(5)),
         };
     }
 
     // Incoming DTMF for white rabbit: sending rabbit gate update
     if (bwMsg.indexOf("Incoming DTMF RFC2833 on call") >= 0) {
+        console.log("|| DEBUG: Incoming DTMF ||");
+        console.log(gateRabbits);
         if (gateRabbits[host]) {
             const { ip, prefix, apartment } = gateRabbits[host];
-            await API.setRabbitGates({ date: now, ip, prefix, apartment });
+            const setRabbitRes = await API.setRabbitGates({ date: now, ip, prefix, apartment });
+            console.log(setRabbitRes.data);
         }
     }
 
