62e0ff3f5ac3b3f9138cb976d5b8cac1c997ac1d
dfedotov <dfedotov@lanta-net.ru>
2023-01-25 12:32:13 +0300

    Add DKS15105 panel

---
+++ server/hw/domophones/beward/dks/dks15105.php
@@ -0,0 +1,22 @@
+<?php
+
+    namespace hw\domophones {
+
+        require_once 'dks.php';
+        require_once 'separated_rfids.php';
+
+        class dks15105 extends dks {
+
+            use separated_rfids;
+
+            protected $cms_models = [
+                'KKM-100S2' => 0,
+                'KKM-105' => 1,
+                'KKM-108' => 3,
+                'KAD2501' => 2,
+                'KAD2502' => 4,
+            ];
+
+        }
+
+    }
