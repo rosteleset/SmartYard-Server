ad5410f768642fd5f2ce72ab823918ff689fe7d0
Mikhail Ivanov <mmikel@mail.ru>
2023-02-03 13:48:28 +0300

    WiP

--- client/modules/users/users.js
+++ client/modules/users/users.js
@@ -142,7 +142,7 @@
                 footer: true,
                 borderless: true,
                 topApply: true,
-                delete: (uid.toString() !== "0")?i18n("users.delete"):false,
+                delete: (uid.toString() !== "0" && uid.toString() !== myself.uid.toString())?i18n("users.delete"):false,
                 fields: [
                     {
                         id: "uid",
