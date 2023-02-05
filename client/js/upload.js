5002a9612ebaa720864a0773f7d7263cbdec36ee
Mikhail Ivanov <mmikel@mail.ru>
2023-02-05 13:15:48 +0300

    WiP

--- client/js/upload.js
+++ client/js/upload.js
@@ -1,5 +1,3 @@
-// modalUpload([ "image/jpeg", "image/png", "application/pdf" ], 2 * 1024 * 1024, '/server/');
-
 const mime2fa = {
     false: "far fa-fw fa-file",
     ".jpg": "far fa-fw fa-file-image",
@@ -48,97 +46,6 @@ function uploadForm(mimeTypes) {
     $("#modalUploadBody").html(h);
 }
 
-function modalUpload(mimeTypes, maxSize, url, postFields, callback) {
-
-    uploadForm(mimeTypes);
-
-    function progress(p) {
-        loadingProgress.set(p);
-    }
-
-    $("#chooseFileToUpload").off("click").on("click", () => {
-        $("#uploadFileInfo").parent().hide();
-        $("#uploadFileProgress").hide();
-        progress(0);
-        $("#fileInput").off("change").val("").click().on("change", () => {
-            let files = document.querySelector("#fileInput").files;
-            if (files && files.length && files[0].name) {
-                $("#uploadFileInfo").html(`
-                    ${files[0].name}<br />
-                    ${i18n("fileSize")}: ${formatBytes(files[0].size)}<br />
-                    ${i18n("fileDate")}: ${date("Y-m-d H:i", files[0].lastModified / 1000)}<br />
-                    ${i18n("fileType")}: ${files[0].type}<br />
-                `).parent().show();
-            }
-        });
-    });
-
-    $("#uploadButton").off("click").on("click", () => {
-        if (document.querySelector('#fileInput').files.length === 0) {
-            error(i18n("noFileSelected"));
-            return;
-        }
-
-        if (document.querySelector('#fileInput').files.length > 1) {
-            error(i18n("multiuploadNotSupported"));
-            return;
-        }
-
-        let file = document.querySelector('#fileInput').files[0];
-
-        if (mimeTypes && mimeTypes.indexOf(file.type) === -1) {
-            error("incorrectFileType");
-            return;
-        }
-
-        if (maxSize && file.size > maxSize) {
-            error("exceededSize");
-            return;
-        }
-
-        $('#modalUpload').modal('hide');
-
-        autoZ($('#progress').modal({
-            backdrop: 'static',
-            keyboard: false,
-        }));
-
-        let data = new FormData();
-
-        data.append('file', file);
-
-        for (let i in postFields) {
-            data.append(i, postFields[i]);
-        }
-
-        data.append("_token", $.cookie("_token"));
-
-        let request = new XMLHttpRequest();
-        request.open('POST', url);
-
-        request.upload.addEventListener('progress', function(e) {
-            progress(Math.floor((e.loaded / e.total)*100));
-        });
-
-        request.addEventListener("loadend", response => {
-            $('#progress').modal('hide');
-            if (request.status !== 200) {
-                error(request.statusText, request.status);
-            } else {
-                if (typeof callback === "function") {
-                    callback(response);
-                }
-            }
-        });
-
-        request.send(data);
-    });
-
-    autoZ($('#modalUpload')).modal('show');
-
-    xblur();
-}
-
 function loadFile(mimeTypes, maxSize, callback) {
     uploadForm(mimeTypes);
 
@@ -146,7 +53,7 @@ function loadFile(mimeTypes, maxSize, callback) {
     let file = false;
 
     $("#chooseFileToUpload").off("click").on("click", () => {
-        $("#fileIcon").html(`<h1><i class="fas fa-file-upload"></i></h1>`);
+        $("#fileIcon").html(`<h1><i class="far fa-folder-open"></i></h1>`);
         $("#fileIcon").attr("title", i18n("fileNotUploaded"));
         $("#uploadFileInfo").html(`
             ${i18n("fileNotUploaded")}<br />
