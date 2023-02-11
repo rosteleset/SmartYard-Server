939bddbb60bf6002d5a99b6a2f537d992f4708ff
Mikhail Ivanov <mmikel@mail.ru>
2023-02-11 07:19:59 +0300

    WiP

---
+++ server/api/tt/file.php
@@ -0,0 +1,46 @@
+<?php
+
+    /**
+     * tt api
+     */
+
+    namespace api\tt {
+
+        use api\api;
+
+        /**
+         * tt (task tracker metadata(s)) method
+         */
+
+        class file extends api {
+
+            public static function GET($params) {
+                $tt = loadBackend("tt");
+
+                if (!$tt) {
+                    return API::ERROR(500);
+                }
+
+                $project = explode("-", $params["issueId"])[0];
+                $filename = $params["filename"];
+
+                $issue = $tt->getIssues($project, [ "issueId" => $params["issueId"] ], [ "issueId" ]);
+
+                if (!$issue || !$issue["issues"] || !$issue["issues"][0]) {
+                    return API::ERROR(404);
+                }
+
+                return api::ANSWER($issue, "issue");
+            }
+
+            public static function index() {
+                if (loadBackend("tt")) {
+                    return [
+                        "GET" => "#same(tt,issue,GET)",
+                    ];
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
