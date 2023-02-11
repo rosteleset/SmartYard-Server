d898cbf0189e84aad83f935a37cd1e339aaae461
Mikhail Ivanov <mmikel@mail.ru>
2023-02-11 10:16:45 +0300

    WiP

--- server/api/tt/file.php
+++ server/api/tt/file.php
@@ -31,8 +31,48 @@
                 }
 
                 $files = loadBackend("files");
+                /*
+                 Array
+                    (
+                        [0] => Array
+                            (
+                                [chunkSize] => 261120
+                                [filename] => upgrade-t85_default.hex
+                                [length] => 6096
+                                [uploadDate] => Array
+                                    (
+                                        [$date] => Array
+                                            (
+                                                [$numberLong] => 1675852725589
+                                            )
+
+                                    )
+
+                                [md5] => c9845e138884b03218b736c563cbf3e1
+                                [metadata] => Array
+                                    (
+                                        [date] => 1673495418
+                                        [type] => text/x-hex
+                                        [issue] => 1
+                                        [project] => REM
+                                        [issueId] => REM-32
+                                        [attachman] => mmikel
+                                    )
+
+                                [id] => 63e37bb5a30d089fa90bcf30
+                            )
+
+                    )
+                */
+                $list = $files->searchFiles([ "metadata.issue" => true, "metadata.issueId" => $params["issueId"], "filename" => $filename ]);
+                $file = $files->getFileStream($list[0]["id"]);
 
                 header("Content-type: ".$lst[$f]['type']);
+                /*
+                 * Content-Disposition: inline
+                 * Content-Disposition: attachment
+                 * Content-Disposition: attachment; filename="filename.jpg"
+                 */
                 header("Content-Disposition: attachment; filename=".urlencode($lst[$f]['name']));
 
                 $begin = 0;
@@ -55,7 +95,7 @@
                 header('Cache-Control: public, must-revalidate, max-age=0');
                 header('Pragma: no-cache');
                 header('Accept-Ranges: bytes');
-                header('Content-Length:'.($size - $begin));
+                header('Content-Length:' . ($size - $begin));
                 header('Content-Transfer-Encoding: binary');
 
                 echo substr($mon, $begin, $size - $begin);
