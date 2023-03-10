60efdee87a06140d774956324e1135c9a61173ea
Aleksander Vasilev <sbca68@gmail.com>
2023-03-10 13:43:06 +0300

    push action bugfix

--- server/backends/inbox/clickhouse/clickhouse.php
+++ server/backends/inbox/clickhouse/clickhouse.php
@@ -81,7 +81,7 @@
                             "msg" => $msg,
                             "badge" => $unreaded,
                             "sound" => "default",
-                            "action" => $action,
+                            "pushAction" => $action,
                         ]);
                         $result = explode(":", $result);
                         if ($this->db->modify("update inbox set code = :code, push_message_id = :push_message_id where msg_id = :msg_id", [
