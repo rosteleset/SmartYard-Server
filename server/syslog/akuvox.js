c2266f9874addc1dcd6e1d39db261cfca5f3a197
dfedotov <dfedotov@lanta-net.ru>
2023-03-15 16:23:58 +0300

    WIP: Akuvox syslog

--- server/syslog/akuvox.js
+++ server/syslog/akuvox.js
@@ -9,9 +9,62 @@ const { port } = urlParser(akuvox);
 const gateRabbits = [];
 
 syslog.on("message", async ({ date, host, message }) => {
-    console.log(date);
-    console.log(host);
-    console.log(message);
+    const now = getTimestamp(date);
+    const msg = message.replace(/<\d+>[A-Za-z]+ \d+ \d+:\d+:\d+(?:\s*:)?\s*/, "").trim();
+
+    // Spam messages filter
+    if (
+        msg.indexOf("Couldn't resolve host name") >= 0 ||
+        msg.indexOf("AKUVOX DCLIENT") >=0 ||
+        msg.indexOf("Autoprovision") >= 0 ||
+        msg.indexOf("OPENDOOR_LOG") >= 0 ||
+        msg.indexOf("lighttpd") >= 0 ||
+        msg.indexOf("api.fcgi") >= 0 ||
+        msg.indexOf("fcgiserver") >= 0
+    ) {
+        return;
+    }
+
+    console.log(`${now} || ${host} || ${msg}`);
+
+    // Send message to syslog storage
+    // await API.sendLog({ date: now, ip: host, unit: "is", msg: msg }); TODO: uncomment later
+
+    // Motion detection: start
+    if (msg.indexOf("Requst SnapShot") >= 0) {
+        await API.motionDetection({ date: now, ip: host, motionActive: true });
+        await mdTimer(host, 5000);
+    }
+
+    // Call in gate mode with prefix: potential white rabbit
+    if (true) {
+
+    }
+
+    // Incoming DTMF for white rabbit: sending rabbit gate update
+    if (true) {
+
+    }
+
+    // Opening door by RFID key
+    if (true) {
+
+    }
+
+    // Opening door by personal code
+    if (true) {
+
+    }
+
+    // Opening door by button pressed
+    if (true) {
+
+    }
+
+    // All calls are done
+    if (true) {
+
+    }
 });
 
 syslog.on("error", (err) => {
