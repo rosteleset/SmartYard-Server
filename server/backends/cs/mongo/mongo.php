7f06ece10cc25cb67d9c0e5a77161888e9afe965
Mikhail Ivanov <mmikel@mail.ru>
2023-03-18 12:15:52 +0300

    WiP

---
+++ server/backends/cs/mongo/mongo.php
@@ -0,0 +1,36 @@
+<?php
+
+    /**
+     * backends cs namespace
+     */
+
+    namespace backends\cs {
+
+        /**
+         * mongo cs class
+         */
+
+        class mongo extends cs {
+
+            /**
+             * @inheritDoc
+             */
+            public function getCS()
+            {
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function putCS()
+            {
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getCSes()
+            {
+            }
+        }
+    }
