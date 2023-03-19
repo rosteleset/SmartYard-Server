2b2557f16a1e94cf4c6ad482f2466ee80ee0adc3
Mikhail Ivanov <mmikel@mail.ru>
2023-03-19 05:18:41 +0300

    WiP

---
+++ client/modules/cs/cs.js
@@ -0,0 +1,17 @@
+({
+    init: function () {
+        leftSide("fas fa-fw fa-table", i18n("cs.cs"), "#cs", "tt");
+        moduleLoaded("cs", this);
+    },
+
+    route: function (params) {
+        $("#subTop").html("");
+        $("#altForm").hide();
+
+        document.title = i18n("windowTitle") + " :: " + i18n("cs.cs");
+
+        $("#mainForm").html(i18n("cs.cs"));
+
+        loadingDone();
+    },
+}).init();
