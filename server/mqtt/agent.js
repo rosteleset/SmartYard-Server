9cf20f3935de6f60476c7e1d2ff73132299805e6
Mikhail Ivanov <mmikel@mail.ru>
2023-03-22 13:11:58 +0300

    WiP

--- server/mqtt/agent.js
+++ server/mqtt/agent.js
@@ -1,36 +1,34 @@
 #!/bin/env node
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const mqtt = require('mqtt');
 const redis = require('redis').createClient();
 const fs = require('fs');
 
-redis.connect().then(() => {
-    redis.sendCommand([ 'config', 'set', 'notify-keyspace-events', 'Ex' ]).then(() => {
-        redis.subscribe('__keyevent@0__:expired', (k, e) => {
-            if (e == '__keyevent@0__:expired') {
-                console.log(k);
-            }
-        });
-    });
-});
-
-let mqtt_config = JSON.parse(fs.readFileSync(__dirname + "/../config/config.json").toString()).backends.mqtt;
+const mqtt_config = JSON.parse(fs.readFileSync(__dirname + "/../config/config.json").toString()).backends.mqtt;
 
 const client = mqtt.connect(mqtt_config.ws, {
     username: mqtt_config.username,
     password: mqtt_config.password,
 });
 
-client.publish("cs/cell", JSON.stringify({
-    action: "claim",
-    sheet: "sheet",
-    date: "date",
-    col: "col",
-    row: "row",
-    uid: "uid",
-    login: "login",
-}));
+redis.connect().then(() => {
+    redis.sendCommand([ 'config', 'set', 'notify-keyspace-events', 'Ex' ]).then(() => {
+        redis.subscribe('__keyevent@0__:expired', (k, e) => {
+            if (e == '__keyevent@0__:expired') {
+                client.publish("redis/expire", JSON.stringify({ key: k }));
+            }
+        });
+    });
+});
+
+app.post('/broadcast', express.json({ type: '*/*' }), (req, res) => {
+    if (req.body && req.body.topic && req.body.payload) {
+        client.publish(req.body.topic, JSON.stringify(req.body.payload));
+    }
+    res.status(200).end();
+});
 
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.listen(8082, '127.0.0.1');
