f975ef1bcc1686cde2860d1252dc42b3c0700ebc
Mikhail Ivanov <mmikel@mail.ru>
2023-03-22 13:34:17 +0300

    WiP

--- server/api/cs/reserveCell.php
+++ server/api/cs/reserveCell.php
@@ -26,10 +26,23 @@
                 return api::ANSWER($success);
             }
 
+            public static function DELETE($params) {
+                $cs = loadBackend("cs");
+
+                $success = false;
+
+                if ($cs) {
+                    $success = $cs->setCell("freeCell", $params["sheet"], $patams["date"], $params["col"], $params["row"], $params["uid"]);
+                }
+
+                return api::ANSWER($success);
+            }
+
             public static function index() {
                 if (loadBackend("tt")) {
                     return [
                         "PUT",
+                        "DELETE",
                     ];
                 } else {
                     return false;
