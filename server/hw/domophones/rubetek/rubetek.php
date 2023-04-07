5d64eceb14e47b739f7f362d43242a8ec3e616ce
dfedotov <dfedotov@lanta-net.ru>
2023-04-07 17:38:42 +0300

    WIP: Rubetek hw class

--- server/hw/domophones/rubetek/rubetek.php
+++ server/hw/domophones/rubetek/rubetek.php
@@ -159,13 +159,38 @@
             }
 
             public function configure_md(
-                int $sensitivity = 4,
+                int $sensitivity = 50,
                 int $left = 0,
                 int $top = 0,
                 int $width = 705,
                 int $height = 576
             ) {
-                // TODO: Implement configure_md() method.
+                $detectionSettings = $this->getConfig()['face_detection'];
+
+                // Server
+                $detectionSettings['address'] = '1'; // Not used
+                $detectionSettings['reserved_address'] = '1'; // Not used
+                $detectionSettings['token'] = '1'; // Not used
+
+                // Detection settings
+                $detectionSettings['detection_mode'] = 1; // Detection on
+                $detectionSettings['threshold'] = $sensitivity; // Threshold of confidence
+                $detectionSettings['liveness_frame_num'] = 0; // Not used
+                $detectionSettings['frame_interval'] = 500; // Doesn't work
+                $detectionSettings['face_presence_time'] = 0; // Not used
+                $detectionSettings['min_dimension'] = 50; // Minimum face size px
+                $detectionSettings['max_dimension'] = 500; // Maximum face size px
+                $detectionSettings['rect_image_format'] = 1; // Not used
+
+                // Detection area
+                // TODO: get from params!
+                $detectionSettings['rec_area_top'] = 10;
+                $detectionSettings['rec_area_bottom'] = 10;
+                $detectionSettings['rec_area_left'] = 10;
+                $detectionSettings['rec_area_right'] = 10;
+                $detectionSettings['outMargin'] = 50; // Detection indent
+
+                $this->api_call('/configuration', 'PATCH', [ 'face_detection' => $detectionSettings ]);
             }
 
             public function configure_ntp(string $server, int $port, string $timezone) {
