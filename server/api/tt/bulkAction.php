df303522d9167d112ba03572ae0e828d2618ea89
Mikhail Ivanov <mmikel@mail.ru>
2023-04-10 09:59:11 +0300

    WiP

---
+++ server/api/tt/bulkAction.php
@@ -0,0 +1,47 @@
+<?php
+
+    /**
+     * tt api
+     */
+
+    namespace api\tt {
+
+        use api\api;
+
+        /**
+         * bulkAction method
+         */
+
+        class bulkAction extends api {
+
+            public static function PUT($params) {
+                $tt = loadBackend("tt");
+
+                if ($tt) {
+                    $issues = $tt->getIssues($params[ "project" ], $params[ "query" ], [ "issueId", "workflow" ]);
+
+                    $success = true;
+
+                    if ($issues && count($issues)) {
+                        foreach ($issues["issues"] as $issue) {
+                            $success = $success && $tt->loadWorkflow($issue["workflow"])->action($params["set"], $params["action"], $issue);
+                        }
+
+                        return api::ANSWER($success);
+                    }
+                }
+
+                return api::ERROR();
+            }
+
+            public static function index() {
+                if (loadBackend("tt")) {
+                    return [
+                        "PUT" => "tt",
+                    ];
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
