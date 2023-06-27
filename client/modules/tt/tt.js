9e081ae35cf42b769726a6b885cca24c1a4d682e
Mikhail Ivanov <mmikel@mail.ru>
2023-06-27 18:43:16 +0300

    WiP

--- client/modules/tt/tt.js
+++ client/modules/tt/tt.js
@@ -429,9 +429,14 @@
                                 cf.editor = "text";
                             }
 
-                            if ([ "date", "datetime-local" ].indexOf(cf.editor) >= 0 && issue && issue["_cf_" + fieldId]) {
-                                console.log(issue["_cf_" + fieldId], ttDate(issue["_cf_" + fieldId]));
-                                issue["_cf_" + fieldId] = ttDate(issue["_cf_" + fieldId]);
+                            if (cf.editor == "date") {
+                                let d = new Date(issue["_cf_" + fieldId] * 1000);
+                                issue["_cf_" + fieldId] = d.getFullYear() + "-" + pad2(d.getMonth() + 1) + "-" + pad2(d.getDate());
+                            }
+
+                            if (cf.editor == "datetime-local") {
+                                let d = new Date(issue["_cf_" + fieldId] * 1000);
+                                issue["_cf_" + fieldId] = d.getFullYear() + "-" + pad2(d.getMonth() + 1) + "-" + pad2(d.getDate()) + " " + pad2(d.getHours) + ":" + pad2(d.getMinutes());
                             }
 
                             return {
