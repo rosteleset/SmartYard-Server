4ea14818d431ad15866e6e7a7d60dac0af61adc8
Mikhail Ivanov <mmikel@mail.ru>
2023-06-27 20:22:20 +0300

    WiP

--- client/modules/tt/workspaces.js
+++ client/modules/tt/workspaces.js
@@ -25,7 +25,7 @@
             {
                 target: "#mainForm",
                 project: "RTL",
-                filter: "all",
+                filter: "delayed",
             },
         ];
 
