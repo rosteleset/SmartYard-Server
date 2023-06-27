c943b9e4b061f7f85b407e946a0463424a9d8795
Mikhail Ivanov <mmikel@mail.ru>
2023-06-27 18:51:16 +0300

    WiP

--- client/modules/tt/tt.js
+++ client/modules/tt/tt.js
@@ -435,6 +435,7 @@
                             }
 
                             if (cf.editor == "datetime-local") {
+                                console.log(issue["_cf_" + fieldId]);
                                 let d = new Date(issue["_cf_" + fieldId] * 1000);
                                 issue["_cf_" + fieldId] = d.getFullYear() + "-" + pad2(d.getMonth() + 1) + "-" + pad2(d.getDate()) + "T" + pad2(d.getHours) + ":" + pad2(d.getMinutes());
                             }
