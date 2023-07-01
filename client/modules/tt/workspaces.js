1bb5be6b5650ab0fccca03594b5f46dc55cd2253
Mikhail Ivanov <mmikel@mail.ru>
2023-07-01 10:15:05 +0300

    WiP

--- client/modules/tt/workspaces.js
+++ client/modules/tt/workspaces.js
@@ -48,6 +48,17 @@
             }
             lStore("_tt_workspace", currentWorkspace);
 
+            let rtd = "";
+            rtd += `<div class="form-inline"><div class="input-group input-group-sm"><select id="ttWorkspaceSelect" class="form-control" style="width: 259px;">`;
+            rtd += `</select></div>`;
+    
+            rtd += `<div class="nav-item mr-0 pr-0"><span id="editWorkspace" class="nav-link text-primary mr-0 pr-0" role="button" style="cursor: pointer" title="${i18n("tt.addModifyWorkspace")}"><i class="fas fa-lg fa-fw fa-pen-square"></i></span></div>`;
+            if (currentWorkspace) {
+                rtd += `<div class="nav-item mr-0 pr-0"><span id="deleteWorkspace" class="nav-link text-danger mr-1 pr-0" role="button" style="cursor: pointer" title="${i18n("tt.deleteWorkspace")}"><i class="fas fa-lg fa-fw fa-minus-square"></i></span></div>`;
+            }
+    
+            $("#rightTopDynamic").html(rtd);
+    
             let h = "";
             for (let i in result.files) {
                 if (result.files[i].filename == currentWorkspace) {
@@ -84,17 +95,6 @@
             } else {
                 $("#altForm").hide();
             }
-    
-            let rtd = "";
-            rtd += `<div class="form-inline"><div class="input-group input-group-sm"><select id="ttWorkspaceSelect" class="form-control" style="width: 259px;">`;
-            rtd += `</select></div>`;
-    
-            rtd += `<div class="nav-item mr-0 pr-0"><span id="editWorkspace" class="nav-link text-primary mr-0 pr-0" role="button" style="cursor: pointer" title="${i18n("tt.addModifyWorkspace")}"><i class="fas fa-lg fa-fw fa-pen-square"></i></span></div>`;
-            if (currentWorkspace) {
-                rtd += `<div class="nav-item mr-0 pr-0"><span id="deleteWorkspace" class="nav-link text-danger mr-1 pr-0" role="button" style="cursor: pointer" title="${i18n("tt.deleteWorkspace")}"><i class="fas fa-lg fa-fw fa-minus-square"></i></span></div>`;
-            }
-    
-            $("#rightTopDynamic").html(rtd);
     
             (function loadWorkspace() {
                 let block = workspace.shift();
