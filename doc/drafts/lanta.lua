1b2de4cfcb60e0f247c282b66514875b000bf133
Mikhail Ivanov <mmikel@mail.ru>
2023-07-01 06:46:47 +0300

    WiP

--- doc/drafts/lanta.lua
+++ doc/drafts/lanta.lua
@@ -72,6 +72,138 @@ function insertFirst(tab, val, withSep)
 end
 
 --
+-- Условия
+--
+
+-- Открытые заявки
+-- Статус == Открыто
+
+function isOpened(issue)
+    return
+        issue["status"] == "" or issue["status"] == nil
+        or
+        (
+            (issue["status"] == "opened" or issue["status"] == "open" or issue["status"] == "Открыта" or issue["status"] == "Открыто")
+            and
+            issue["status"] ~= "closed" and issue["status"] ~= "Закрыто" and issue["status"] ~= "Закрыта"
+        )
+end
+
+-- Скоординирована
+-- Стоит в листе координации
+
+function isCoordinated(issue)
+    return
+        issue["_cf_sheet"] ~= nil and issue["_cf_sheet"] ~= "" and
+        issue["_cf_sheet_date"] ~= nil and issue["_cf_sheet_date"] ~= "" and
+        issue["_cf_sheet_col"] ~= nil and issue["_cf_sheet_col"] ~= "" and
+        issue["_cf_sheet_cell"] ~= nil and issue["_cf_sheet_cell"] ~= "" and
+        (issue["_cf_install_done"] == nil or issue["_cf_install_done"] == "" or tonumber(issue["_cf_install_done"]) == 0)
+end
+
+-- СС. Позвонить сейчас
+-- (Колл-центр == "да" и Дата созвона пусто) или (Дата созвона >= Текущая дата) или (Дата координации == Завтра и Дата координации <= Вчера)
+	
+function callNow(issue)
+end
+
+-- Координация.Просроченные
+-- Заявка стоит в листе координации, имеет дату и время визита более чем на 3 дня вперед
+
+function coordinationExpired(issue)
+    return isCoordinated(issue) and utils.strtotime(issue["_cf_sheet_date"] .. " " .. issue["_cf_sheet_cell"] .. ":00") - utils.time() > 3 * 24 * 60 * 60
+end
+	
+-- Открытые заявки пользователя
+-- Заявка создана мной, она не закрыта
+
+function myIssue(issue)
+    return isCoordinated(issue) and issue["author"] == tt.login()
+end
+
+-- Добавлен в наблюдатели
+-- Заявка не закрыта, добавлен в наблюдатели
+
+function watching(issue)
+    return isCoordinated(issue) and issue["watchers"] ~= nil and utils.in_array(tt.login(), issue["watchers"])
+end
+	
+-- Связаться позже
+-- Связаться по заявке позже сегодняшнего дня
+
+function callLater(issue)
+end
+	
+-- Отстойник заявок
+-- Открытые заявки, Выполнено СИ=да
+
+function fitDone(issue)
+    return isOpened(issue) and issue["_cf_install_done"] == "Да"
+end
+
+-- Связаться сегодня
+-- Дата созвона >= Текущая дата и Дата созвона < Завтра
+
+function callToday(issue)
+    if issue["_cf_call_date"] ~= nil then
+        return isOpened(issue) and utils.strtotime(utils.date("Y-m-d")) <= issue["_cf_call_date"] and issue["_cf_call_date"] <= utils.strtotime(utils.date("Y-m-d", utils.strtotime("+1 day")))
+    else
+        return false
+    end
+end
+
+-- Есть тех. возможность подключения
+-- resolution = "Ожидает распределения"
+
+function waitingForCoordination(issue)
+end
+	
+-- ЮЛ в офисе
+-- Заявки с типом ЮЛ, в статусе "открыта", с любым вопросом: перерасчет, ремонт, пустышки, кроме авто подключений.
+
+function officeUL(issue)
+    return isOpened(issue) and issue["_cf_client_type"] ~= "ФЛ" and subject ~= "Подключение абонента"
+end
+
+-- Офис ЮЛ Автоподключения
+-- Заявки с типом ЮЛ, в статусе АВТО: Подключение абонента, то есть в этом фильтре заявки которые уже были в работе, или только планируем подключение
+
+function autoconnectUL(issue)
+    return isOpened(issue) and issue["_cf_client_type"] ~= "ФЛ" and subject == "Подключение абонента"
+end
+
+-- Офис Оформление договора
+-- Заявки с типом ЮЛ, в статусе АВТО: заявка с сайта - попадают в фильтр после того как в ней менеджер указывает условия подключения,
+-- то есть передает ее на заведение учетки в адинке и дальнейшему оформлению и координациии
+
+function waitingForContractUL(issue)
+end
+
+--
+-- Шаблоны и действия
+--
+
+function coordinationTemplate(issue)
+    return {
+        "_cf_sheet",
+        "_cf_sheet_date",
+        "_cf_sheet_cell",
+        "_cf_sheet_col",
+        "_cf_sheet_cells",
+        "_cf_installers",
+        "_cf_can_change",
+        "_cf_call_before_visit",
+    }
+end
+
+function coordinate(issue, workflow)
+    issue["_cf_install_done"] = ""
+    issue["_cf_coordination_date"] = utils.time()
+    issue["_cf_coordinator"] = tt.login()
+    return tt.modifyIssue(issue)
+end
+
+--
 -- Заявка
 --
 
@@ -136,23 +268,195 @@ end
 
 
 function getAvailableActions(issue)
-    local actions = {
-        "saAddComment",
-        "saAddFile",
-        "-",
-        "saDelete",
-    }
-
-    return actions
+    if isOpened(issue) then
+        local actions = {
+            "saAddComment",
+            "saAddFile",
+            "-",
+            "saDelete",
+        }
+        
+        if isOpened(issue) then
+            actions[#actions + 1] = "-"
+            actions[#actions + 1] = "saCoordinate"
+            actions[#actions + 1] = "Назначить исполнителей"
+        end
+        
+        if isCoordinated(issue) then
+            actions[#actions + 1] = "-"
+            actions[#actions + 1] = "Работы завершены"
+            actions[#actions + 1] = "Отменить"
+        end
+        
+        actions[#actions + 1] = "-"
+        actions[#actions + 1] = "Отложить"
+        actions[#actions + 1] = "Закрыть"
+        
+        if issue["subject"] == "Делопроизводство" then
+            actions[#actions + 1] = "-"
+            actions[#actions + 1] = "Делопроизводство"
+        end
+    
+        return actions
+    else
+        return {
+            "Переоткрыть",
+            "-",
+            "saDelete",
+        }
+    end
 end
 
 -- получить шаблон диалога для действия
 function getActionTemplate(issue, action)
+    if action == "Координация" then
+        return coordinationTemplate(issue)
+    end
+
+    if action == "Назначить исполнителей" then
+        return {
+            "_cf_installers",
+        }
+    end
+
+    if action == "Отменить" then
+        return {
+            "comment"
+        }
+    end
+    
+    local doneFilter = {}
+    
+    if tonumber(issue["_cf_object_id"]) > 0 then
+        doneFilter = {
+            "Выполнено",
+            "Камера установлена",
+            "Установлен микрофон",
+            "Установлена камера и микрофон",
+            "Проблема с доступом",
+            "Отмена",
+        }        
+    else
+        doneFilter = {
+            "Выполнено",
+            "Не доставлено",
+        }        
+    end
+
+    if action == "Работы завершены" then
+        return {
+            ["%0%_cf_install_done"] = doneFilter,
+            "%1%_cf_access_info",
+            "%2%comment",
+        }
+    end
+
+    if action == "Закрыть" then
+        return {
+            "optionalComment"
+        }
+    end
+
+    if action == "Отложить" then
+        return {
+            "_cf_delay",
+            "comment",
+        }
+    end
+
+    if action == "Делопроизводство" then
+        return {
+            "_cf_debt_date",
+            "_cf_debt_services",
+            "optionalComment",
+        }
+    end
+
+    if action == "Переоткрыть" then
+        return {
+            "comment"
+        }
+    end
+
     return false
 end
 
 -- выполнить действие
 function action(issue, action, original)
+    if action == "Координация" then
+        return coordinate(issue)
+    end
+    
+    if action == "Назначить исполнителей" then
+        issue["_cf_coordination_date"] = utils.time()
+        issue["_cf_coordinator"] = tt.login()
+        return tt.modifyIssue(issue)
+    end
+    
+    if action == "Работы завершены" then
+        issue["_cf_done_date"] = utils.time()
+        return tt.modifyIssue(issue)
+    end
+
+    if action == "Отменить" then
+        issue["_cf_sheet"] = ""
+        issue["_cf_sheet_date"] = ""
+        issue["_cf_sheet_col"] = ""
+        issue["_cf_sheet_cell"] = ""
+        issue["_cf_sheet_cells"] = 0
+        issue["_cf_installers"] = {}
+        issue["_cf_can_change"] = 0
+        issue["_cf_call_before_visit"] = 0
+        return tt.modifyIssue(issue)
+    end
+
+    if action == "Позвонить" then
+        issue["_cf_need_call"] = 1
+        issue["_cf_calls_count"] = 0
+        return tt.modifyIssue(issue)
+    end
+    
+    if action == "Звонок совершен" then
+        issue["_cf_need_call"] = 0
+        if original["_cf_calls_count"] == nil then
+            issue["_cf_calls_count"] = 1
+        else
+            issue["_cf_calls_count"] = original["_cf_calls_count"] + 1
+        end
+        return tt.modifyIssue(issue)
+    end
+    
+    if action == "Недозвон" then
+        issue["_cf_call_date"] = utils.time() + 30 * 60
+        if original["_cf_calls_count"] == nil then
+            issue["_cf_calls_count"] = 1
+        else
+            issue["_cf_calls_count"] = original["_cf_calls_count"] + 1
+        end
+        if issue["_cf_calls_count"] >= 3 then
+            issue["_cf_need_call"] = 0
+        end
+        return tt.modifyIssue(issue)
+    end
+    
+    if action == "Отложить" then
+        return tt.modifyIssue(issue)
+    end
+    
+    if action == "Делопроизводство" then
+        return tt.modifyIssue(issue)
+    end
+
+    if action == "Закрыть" then
+        issue["status"] = "Закрыта"
+        return tt.modifyIssue(issue)
+    end
+
+    if action == "Переоткрыть" then
+        issue["status"] = "Открыта"
+        return tt.modifyIssue(issue)
+    end
+
     return false
 end
 
@@ -164,7 +468,9 @@ function viewIssue(issue)
         "*author",
         "*assigned",
         "*watchers",
-        "*_cf_coordinated_on",
+        "*_cf_sheet",
+        "*_cf_sheet_cell",
+        "*_cf_install_done",
         "*_cf_sheet_cells",
         "*_cf_installers",
         "*_cf_can_change",
@@ -196,7 +502,18 @@ function viewIssue(issue)
             "_cf_call_before_visit",
         })
     end
-
+    
+    if not isCoordinated(issue) then
+        fields = removeValues(fields, {
+            "*_cf_call_before_visit",
+            "_cf_call_before_visit",
+            "*_cf_can_change",
+            "_cf_can_change",
+            "*_cf_sheet_cells",
+            "_cf_sheet_cells",
+        })
+    end
+    
     return {
         ["issue"] = issue,
         ["actions"] = getAvailableActions(issue),
@@ -217,4 +534,8 @@ function getWorkflowCatalog()
             "Пустышка",
         },
     }
+end
+
+function issueChanged(issue, action, old, new)
+    return mqtt.broadcast("issue/changed", issue["issueId"])
 end
