b9f11870dab0ef95a632f2c2184a6c31e8c66952
Mikhail Ivanov <mmikel@mail.ru>
2023-07-03 13:46:47 +0300

    WiP

--- server/api/cs/reserveCell.php
+++ server/api/cs/reserveCell.php
@@ -20,7 +20,7 @@
                 $success = false;
 
                 if ($cs) {
-                    $success = $cs->setCell("reserve", $params["sheet"], $params["date"], $params["col"], $params["row"], $params["uid"], (int)@$params["expire"], @$params["sid"], @$params["comment"]);
+                    $success = $cs->setCell("reserve", $params["sheet"], $params["date"], $params["col"], $params["row"], $params["uid"], (int)@$params["expire"], @$params["sid"], 0, @$params["comment"]);
                 }
 
                 return api::ANSWER($success);
