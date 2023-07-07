b363d3978ebb5fdb3a0cda7db5ee7f35a554b84a
Mikhail Ivanov <mmikel@mail.ru>
2023-07-07 11:57:16 +0300

    WiP

--- client/modules/groups/groups.js
+++ client/modules/groups/groups.js
@@ -208,9 +208,13 @@
                     }
                 }
 
+                console.log(defaults);
+
                 users_list.sort((a, b) => {
                     return a.text.localeCompare(b.text);
                 });
+
+                console.log(users_list);
 
                 cardForm({
                     title: i18n("groups.users") + " " + i18n("groups.gid") + gid,
