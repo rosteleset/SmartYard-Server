c9351e00252cce92062ed16502e3705839b4b727
Selpol <warfcevip@gmail.com>
2023-07-11 13:41:08 +0300

    Add flat sorting in houses

--- server/api/houses/house.php
+++ server/api/houses/house.php
@@ -1,48 +1,51 @@
 <?php
 
-    /**
-     * houses api
-     */
+/**
+ * houses api
+ */
 
-    namespace api\houses
+namespace api\houses {
+
+    use api\api;
+
+    /**
+     * house method
+     */
+    class house extends api
     {
 
-        use api\api;
-
-        /**
-         * house method
-         */
-
-        class house extends api
+        public static function GET($params)
         {
+            $households = loadBackend("households");
+            $configs = loadBackend("configs");
 
-            public static function GET($params)
-            {
-                $households = loadBackend("households");
-                $configs = loadBackend("configs");
+            if (!$households) {
+                return api::ERROR();
+            } else {
+                $flats = $households->getFlats("houseId", $params["_id"]);
 
-                if (!$households) {
-                    return api::ERROR();
-                } else {
-                    $house = [
-                        "flats" => $households->getFlats("houseId", $params["_id"]),
-                        "entrances" => $households->getEntrances("houseId", $params["_id"]),
-                        "cameras" => $households->getCameras("houseId", $params["_id"]),
-                        "domophoneModels" => $configs->getDomophonesModels(),
-                        "cmses" => $configs->getCMSes(),
-                    ];
+                if ($flats)
+                    usort($flats, static fn(array $a, array $b) => $a['flat'] > $b['flat'] ? 1 : -1);
 
-                    $house = ($house["flats"] !== false && $house["entrances"] !== false && $house["domophoneModels"] !== false && $house["cmses"] !== false)?$house:false;
-
-                    return api::ANSWER($house, "house");
-                }
-            }
-
-            public static function index()
-            {
-                return [
-                    "GET" => "#same(addresses,house,GET)",
+                $house = [
+                    "flats" => $flats,
+                    "entrances" => $households->getEntrances("houseId", $params["_id"]),
+                    "cameras" => $households->getCameras("houseId", $params["_id"]),
+                    "domophoneModels" => $configs->getDomophonesModels(),
+                    "cmses" => $configs->getCMSes(),
                 ];
+
+                $house = ($house["flats"] !== false && $house["entrances"] !== false && $house["domophoneModels"] !== false && $house["cmses"] !== false) ? $house : false;
+
+                return api::ANSWER($house, "house");
             }
         }
+
+        public static function index()
+        {
+            return [
+                "GET" => "#same(addresses,house,GET)",
+            ];
+        }
     }
+}
