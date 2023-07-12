b9742b73025eadab01d15e176453b667ecfdcbac
Mikhail Ivanov <mmikel@mail.ru>
2023-07-12 05:54:34 +0300

    WiP

--- client/modules/cs/cs.js
+++ client/modules/cs/cs.js
@@ -515,7 +515,7 @@
                 h += '<table width="100%" class="mt-3 table table-hover table-bordered" id="csSheet">';
                 for (let p in parts) {
                     if (p != cp) {
-                        h += "<tr><td style='border: none!important; font-weight: bold;' class='text-primary' colspan='" + (maxCols + 1).toString() + "'>" + p + "</td></tr>";
+                        h += "<tr><td>&nbsp;</td><td style='border: none!important; font-weight: bold;' class='text-primary' colspan='" + maxCols.toString() + "'>" + p + "</td></tr>";
                         cp = p;
                     }
                     h += '<tr>';
