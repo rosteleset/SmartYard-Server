976646ba348e240ae84a12f84f45eb9b0976fc5c
Mikhail Ivanov <mmikel@mail.ru>
2023-07-14 12:10:22 +0300

    WiP

--- client/modules/tt/issue.js
+++ client/modules/tt/issue.js
@@ -829,7 +829,7 @@
                     for (let i in response.journal) {
                         let o = response.journal[i].old && typeof response.journal[i].old.length == 'undefined';
                         let n = response.journal[i].new && typeof response.journal[i].new.length == 'undefined';
-                        if (!o && !n) {
+                        if ((!o && !n) || o == n) {
                             continue;
                         }
                         let action = response.journal[i].action.split("#")[0];
