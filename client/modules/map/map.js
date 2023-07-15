a6dd2466cb3eb13ab1a0308df9fb7eea9cfad969
Mikhail Ivanov <mmikel@mail.ru>
2023-07-15 10:32:47 +0300

    WiP

--- client/modules/map/map.js
+++ client/modules/map/map.js
@@ -1,6 +1,7 @@
 ({
     init: function () {
         leftSide("fas fa-fw fa-map-marked-alt", i18n("map.map"), "?#map", "map");
+        moduleLoaded("map", this);
     },
 
     render: function (params) {
