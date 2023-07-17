e4b5119e2a68dba0f71df8336f5070815c9a2493
Mikhail Ivanov <mmikel@mail.ru>
2023-07-17 06:02:48 +0300

    WiP

--- client/modules/map/map.js
+++ client/modules/map/map.js
@@ -9,23 +9,44 @@
     route: function (params) {
         let height = $(window).height() - mainFormTop;
 
+        document.title = i18n("windowTitle") + " :: " + i18n("map.map");
+
+        $("#altForm").hide();
         $("#mainForm").html(`<div id='mapContainer' style='width: 100%; height: ${height}px; border: solid thin #dee2e6;' class='mt-2'></div>`);
         
         modules.map.map = L.map('mapContainer');
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(modules.map.map);
 
-        if (!navigator.geolocation) {
-            modules.map.map.setView([51.505, -0.09], 13);
-            L.marker([51.5, -0.09]).addTo(modules.map.map);
+        let lat, lon;
+
+        if (params.coords) {
+            lat = parseFloat(coords.split(",")[0]);
+            lon = parseFloat(coords.split(",")[1]);
+        }
+
+        if (params.lat && params.lon) {
+            lon = parseFloat(params.lon);
+            lat = parseFloat(params.lat);
+        }
+
+        if (typeof lon != "undefined" && typeof lat != "undefined") {
+            modules.map.map.setView([lat, lon], 13);
+            L.marker([lat, lon]).addTo(modules.map.map);
         } else {
-            navigator.geolocation.getCurrentPosition(success => {
-                modules.map.map.setView([success.coords.latitude, success.coords.longitude], 13);
-                L.marker([success.coords.latitude, success.coords.longitude]).addTo(modules.map.map);
-            }, () => {
+            if (!navigator.geolocation) {
                 modules.map.map.setView([51.505, -0.09], 13);
                 L.marker([51.5, -0.09]).addTo(modules.map.map);
-            });
+            } else {
+                navigator.geolocation.getCurrentPosition(success => {
+                    console.log(success.coords.latitude, success.coords.longitude);
+                    modules.map.map.setView([success.coords.latitude, success.coords.longitude], 13);
+                    L.marker([success.coords.latitude, success.coords.longitude]).addTo(modules.map.map);
+                }, () => {
+                    modules.map.map.setView([51.505, -0.09], 13);
+                    L.marker([51.5, -0.09]).addTo(modules.map.map);
+                });
+            }
         }
 
         loadingDone();
