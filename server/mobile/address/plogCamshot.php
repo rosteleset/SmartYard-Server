6def52dcfd36e657d9c495b6f54aaf4781b07988
Selpol <warfcevip@gmail.com>
2023-08-07 15:11:05 +0300

    Refactoring code

--- server/mobile/address/plogCamshot.php
+++ server/mobile/address/plogCamshot.php
@@ -1,32 +1,14 @@
 <?php
-
-use logger\Logger;
-
-Logger::channel('plog')->debug('plogCamshot', ['uuid' => $param]);
-
-$files = loadBackend('files');
-$uuid = $files->fromGUIDv4($param);
-
-$file = $files->getFileStream($uuid);
-
-if ($file) {
-    header("Content-Type: image/jpeg");
-
-    echo $file;
-}
-// $img = $files->getFile($uuid);
-
-// if ($img) {
-//     $content_type = "image/jpeg";
-//     $meta_data = $files->getFileMetadata($uuid);
-
-//     if (isset($meta_data->contentType)) {
-//         $content_type = $meta_data->contentType;
-//     }
-
-//     header("Content-Type: $content_type");
-
-//     echo (stream_get_contents($img['stream']));
-
-//     exit;
-// }
+    $files = loadBackend('files');
+    $uuid = $files->fromGUIDv4($param);
+    $img = $files->getFile($uuid);
+    if ($img) {
+        $content_type = "image/jpeg";
+        $meta_data = $files->getFileMetadata($uuid);
+        if (isset($meta_data->contentType)) {
+            $content_type = $meta_data->contentType;
+        }
+        header("Content-Type: $content_type");
+        echo(stream_get_contents($img['stream']));
+        exit;
+    }
