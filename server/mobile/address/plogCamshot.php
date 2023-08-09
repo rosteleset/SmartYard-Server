499c91b08f0a0510b1215711382df31efaee69a4
Selpol <warfcevip@gmail.com>
2023-08-09 15:16:06 +0300

    Refactoring code

--- server/mobile/address/plogCamshot.php
+++ server/mobile/address/plogCamshot.php
@@ -1,12 +1,18 @@
 <?php
 
+use logger\Logger;
+
 $files = loadBackend('files');
 $uuid = $files->fromGUIDv4($param);
 $img = $files->getFile($uuid);
 
+Logger::channel('plog')->debug('plogCamshot', ['uuid' => $uuid]);
+
 if ($img) {
     $content_type = "image/jpeg";
     $meta_data = $files->getFileMetadata($uuid);
+
+    Logger::channel('plog')->debug('plogCamshot', ['uuid' => $uuid, 'meta' => $meta_data]);
 
     if (isset($meta_data->contentType))
         $content_type = $meta_data->contentType;
@@ -14,10 +20,13 @@ if ($img) {
     $image = imagecreatefromstring(stream_get_contents($img['stream']));
 
     if ($image) {
+        Logger::channel('plog')->debug('plogCamshot send', ['uuid' => $uuid]);
 
         header("Content-Type: $content_type");
 
-        imagejpeg($image);
+        if (!imagejpeg($image))
+            Logger::channel('plog')->debug('plogCamshot not send', ['uuid' => $uuid]);
+
         imagedestroy($image);
 
         exit;
