9ed4272b17bb4990726cc78c5792806678732797
Selpol <warfcevip@gmail.com>
2023-08-09 14:36:21 +0300

    Refactroing code

--- server/mobile/address/plogCamshot.php
+++ server/mobile/address/plogCamshot.php
@@ -1,14 +1,23 @@
 <?php
-    $files = loadBackend('files');
-    $uuid = $files->fromGUIDv4($param);
-    $img = $files->getFile($uuid);
-    if ($img) {
-        $content_type = "image/jpeg";
-        $meta_data = $files->getFileMetadata($uuid);
-        if (isset($meta_data->contentType)) {
-            $content_type = $meta_data->contentType;
-        }
+$files = loadBackend('files');
+$uuid = $files->fromGUIDv4($param);
+$img = $files->getFile($uuid);
+
+if ($img) {
+    $content_type = "image/jpeg";
+    $meta_data = $files->getFileMetadata($uuid);
+
+    if (isset($meta_data->contentType))
+        $content_type = $meta_data->contentType;
+
+    $image = imagecreatefromstring(stream_get_contents($img['stream']));
+
+    if ($image) {
         header("Content-Type: $content_type");
-        echo(stream_get_contents($img['stream']));
-        exit;
+
+        imagepng($image);
+        imagedestroy($image);
     }
+
+    exit;
+}
