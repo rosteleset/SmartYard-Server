fe654536ad1fb912742ebacff14fc019154184cf
Selpol <warfcevip@gmail.com>
2023-08-15 09:55:52 +0300

    Refactoring code

--- server/mobile/address/plogCamshot.php
+++ server/mobile/address/plogCamshot.php
@@ -1,14 +1,21 @@
 <?php
+
+use logger\Logger;
+
+$logger = Logger::channel('address-plog');
+
 $files = loadBackend('files');
 $uuid = $files->fromGUIDv4($param);
 $file = $files->getFile($uuid);
+
+$logger->debug('plogCamshot()', ['uuid' => $uuid, 'fileInfo' => $file['fileInfo']]);
 
 if ($file) {
     $metaData = $file['fileInfo']->metadata;
 
     header('Content-Type: ' . isset($metaData->contentType) ? $metaData->contentType : 'image/jpeg');
 
-    echo stream_get_contents($file['steam']);
+    echo(stream_get_contents($file['steam']));
 
     exit;
 }
