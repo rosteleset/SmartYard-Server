10be91abccbd2d86097dd6dc30373fa9ac071c52
Selpol <warfcevip@gmail.com>
2023-08-15 09:45:16 +0300

    Refactoring code

--- server/mobile/address/plogCamshot.php
+++ server/mobile/address/plogCamshot.php
@@ -1,8 +1,14 @@
 <?php
 
+use logger\Logger;
+
+$logger = Logger::channel('address-plog');
+
 $files = loadBackend('files');
 $uuid = $files->fromGUIDv4($param);
 $bytes = $files->getFileBytes($uuid);
+
+$logger->debug('plogCamshot()', ['uuid' => $uuid, 'bytes' => count($bytes)]);
 
 if ($bytes) {
     header("Content-Type: image/jpeg");
