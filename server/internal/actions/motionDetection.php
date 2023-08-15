7f7c1091a3bd02d91bd0ca697b9647e4b504dfed
Selpol <warfcevip@gmail.com>
2023-08-15 16:50:29 +0300

    Refactoring code

--- server/internal/actions/motionDetection.php
+++ server/internal/actions/motionDetection.php
@@ -1,39 +1,48 @@
 <?php
-    /** Get ip motion detection device, find stream_id and frs url.
-     * Send POST request to FRS.
-     */
-    if (!isset(
-        $postdata["date"],
-        $postdata["ip"],
-        $postdata["motionActive"]
-    )) {
-        response(406, "Invalid payload");
-        exit();
-    }
+/** Get ip motion detection device, find stream_id and frs url.
+ * Send POST request to FRS.
+ */
 
-    [
-        "date" => $date,
-        "ip" => $ip,
-        "motionActive" => $motionActive
-    ] = $postdata;
+use logger\Logger;
 
-    $query = 'SELECT camera_id, frs FROM cameras WHERE frs != :frs AND ip = :ip';
-    $params = ["ip" => $ip, "frs" => "-"];
-    $result = $db->get($query, $params, []);
-
-    if ( !$result ) {
-        response(200, "FRS not enabled on this stream");
-        exit();
-    }
-
-    [0 => [
-        "camera_id" => $streamId,
-        "frs" => $frsUrl
-    ]] = $result;
-
-    $payload = ["streamId" => $streamId, "start" => $motionActive ? "t" : "f"];
-
-   $apiResponse = apiExec("POST", $frsUrl . "/api/motionDetection", $payload);
-   response(201, $apiResponse);
-
+if (!isset(
+    $postdata["date"],
+    $postdata["ip"],
+    $postdata["motionActive"]
+)) {
+    response(406, "Invalid payload");
     exit();
+}
+
+$logger = Logger::channel('motion');
+
+[
+    "date" => $date,
+    "ip" => $ip,
+    "motionActive" => $motionActive
+] = $postdata;
+
+$query = 'SELECT camera_id, frs FROM cameras WHERE frs != :frs AND ip = :ip';
+$params = ["ip" => $ip, "frs" => "-"];
+$result = $db->get($query, $params, []);
+
+if (!$result) {
+    $logger->debug('Motion detection not enabled', ['frs' => '-', 'ip' => $ip]);
+
+    response(200, "FRS not enabled on this stream");
+    exit();
+}
+
+[0 => [
+    "camera_id" => $streamId,
+    "frs" => $frsUrl
+]] = $result;
+
+$payload = ["streamId" => $streamId, "start" => $motionActive ? "t" : "f"];
+
+$logger->debug('Motion detection', $payload);
+
+$apiResponse = apiExec("POST", $frsUrl . "/api/motionDetection", $payload);
+response(201, $apiResponse);
+
+exit();
