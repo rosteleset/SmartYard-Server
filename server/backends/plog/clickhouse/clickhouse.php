cdea27f21627f51f555d38192a35df7c87994e43
Selpol <warfcevip@gmail.com>
2023-08-18 11:46:43 +0300

    Refactoring code

--- server/backends/plog/clickhouse/clickhouse.php
+++ server/backends/plog/clickhouse/clickhouse.php
@@ -117,7 +117,7 @@ namespace backends\plog {
 
                     Logger::channel('plog')->debug('frs camshot', ['data' => $camshot_data]);
 
-                    if (!array_key_exists('data', $camshot_data) || count($camshot_data["data"]) === 0) {
+                    if (!$camshot_data) {
                         //получение кадра с DVR-серевера, если нет кадра от FRS
                         $prefix = $cameras[0]["dvrStream"];
                         if ($prefix) {
