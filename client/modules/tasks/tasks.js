65f34b68db983b505f39ebea768f541a2c4201fd
Selpol <warfcevip@gmail.com>
2023-08-24 15:17:17 +0300

    Refactoring code

--- client/modules/tasks/tasks.js
+++ client/modules/tasks/tasks.js
@@ -1,15 +1,19 @@
 ({
-    init: function () {
+    init() {
+        moduleLoaded("tasks", this)
+
         if (AVAIL("tasks", "queues", "GET")) {
             $(`
-                <li class="nav-item">
-                    <span class="nav-link text-primary" role="button" style="cursor: pointer" title="${i18n("tasks.title")}" id="taskMenuRight">
+                <li class="nav-item dropdown">
+                    <a class="nav-link text-dark tasks" data-toggle="dropdown" title="${i18n("tasks.title")}" href="#">
                         <i class="fas fa-lg fa-fw fa-server"></i>
-                    </span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="max-width: none;">
+                        <p>Статус текущих задач</p>
+                        <div class="dropdown-divider"></div>
+                    </div>
                 </li>
             `).insertAfter("#rightTopDynamic");
-
-            moduleLoaded("tasks", this)
         }
     }
 }).init()
