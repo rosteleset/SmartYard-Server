ce2ff8a95941d586a57a17ed74ce5399afb45614
Selpol <warfcevip@gmail.com>
2023-08-24 15:11:40 +0300

    Refactoring code

--- client/modules/tasks/tasks.js
+++ client/modules/tasks/tasks.js
@@ -1,5 +1,15 @@
 ({
     init: function () {
+        if (AVAIL("tasks", "queues", "GET")) {
+            $(`
+                <li class="nav-item">
+                    <span class="nav-link text-primary" role="button" style="cursor: pointer" title="${i18n("tasks.title")}" id="taskMenuRight">
+                        <i class="fas fa-lg fa-fw fa-server"></i>
+                    </span>
+                </li>
+            `).insertAfter("#rightTopDynamic");
 
+            moduleLoaded("tasks", this)
+        }
     }
 }).init()
