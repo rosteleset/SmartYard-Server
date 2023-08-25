18254c34c925e8969cc1b212f3a7b24c1152b63b
Selpol <warfcevip@gmail.com>
2023-08-25 13:18:39 +0300

    Refactoring code

--- client/modules/tasks/tasks.js
+++ client/modules/tasks/tasks.js
@@ -1,38 +1,24 @@
 ({
-    queues: [],
-
     init() {
         moduleLoaded("tasks", this)
 
-        if (AVAIL("tasks", "queues", "GET")) {
-            GET("tasks", "queues")
-                .done((response) => {
-                    modules.tasks.queues = response[0]
-
-                    $(`
+        if (AVAIL("tasks", "status", "GET")) {
+            $(`
                         <li class="nav-item dropdown">
                             <a id="tasksMenuRight" class="nav-link text-dark" data-toggle="dropdown" title="${i18n("tasks.title")}" href="#">
                                 <i class="fas fa-lg fa-fw fa-server"></i>
                             </a>
-                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-3" style="max-width: none;">
-                                <p>Статус текущих задач</p>
-                                <div class="dropdown-divider"></div>
-                                <div id="tasksMenuRightContainer">
-                                    ${modules.tasks.queues.map((queue) => `<p id="${queue}" class="m-0 p-0">Очередь (${queue}): 0</p>`).join("\n")}
-                                </div>
+                            <div id="tasksMenuRightContainer" class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-3" style="max-width: none;">
                             </div>
                         </li>
                     `).insertAfter("#rightTopDynamic");
 
-                    $("#tasksMenuRight").off("click", modules.tasks.tasksMenuRight).on("click", modules.tasks.tasksMenuRight);
-                })
+            $("#tasksMenuRight").off("click", modules.tasks.tasksMenuRight).on("click", modules.tasks.tasksMenuRight);
         }
     },
 
     tasksMenuRight() {
-        for (let queue of modules.tasks.queues) {
-            GET("tasks", "size", queue)
-                .done((response) => $("#tasksMenuRightContainer #" + queue).text(`Очередь (${queue}): ` + response[0]))
-        }
+        GET("tasks", "status")
+            .done((response) => $("#tasksMenuRightContainer").text(JSON.stringify(response)))
     }
 }).init()
