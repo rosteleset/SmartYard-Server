eda01ef0da395605b00b62dc35cd335adf082fe5
Selpol <warfcevip@gmail.com>
2023-08-28 16:08:01 +0300

    Refactoring code

--- server/task/Tasks/QrTask.php
+++ server/task/Tasks/QrTask.php
@@ -92,7 +92,7 @@ class QrTask extends Task
                 $template = new TemplateProcessor(path('private/qr-template.docx'));
 
                 $template->setValue('address', $qr['address'] . ', кв ' . $flat['flat']);
-                $template->setImageValue('qr', ['path' => $codeFile, 'width' => 256, 'height' => 256]);
+                $template->setImageValue('qr', ['path' => $codeFile, 'width' => 64, 'height' => 64]);
 
                 $templateFile = $template->save();
                 $files[] = $templateFile;
