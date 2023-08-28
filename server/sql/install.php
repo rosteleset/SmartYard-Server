c05551a8c1c6cc787963455c772d7076d09fad4f
Selpol <warfcevip@gmail.com>
2023-08-28 21:39:37 +0300

    Refactoring code

--- server/sql/install.php
+++ server/sql/install.php
@@ -1,53 +1,53 @@
 <?php
 
-    /**
-     * initialize or upgrade database
-     *
-     * @return void
-     */
+/**
+ * initialize or upgrade database
+ *
+ * @return void
+ */
 
-    function init_db() {
-        global $config, $db, $version;
+function init_db()
+{
+    global $config, $db, $version;
 
-        $install = json_decode(file_get_contents("sql/install.json"), true);
+    $install = json_decode(file_get_contents("sql/install.json"), true);
 
-        $driver = explode(":", $config["db"]["dsn"])[0];
+    $driver = explode(":", $config["db"]["dsn"])[0];
 
-        echo "current version $version\n";
+    echo "current version $version\n";
 
-        $db->exec("BEGIN TRANSACTION");
+    $db->exec("BEGIN TRANSACTION");
 
-        foreach ($install as $v => $steps) {
-            $v = (int)$v;
+    foreach ($install as $v => $steps) {
+        $v = (int)$v;
 
-            if ($version >= $v) {
-                echo "skipping version $v\n";
-                continue;
-            }
-
-            echo "upgradins to version $v\n";
-
-            try {
-                foreach ($steps as $step) {
-                    echo "\n================= $step\n\n";
-                    $sql = trim(file_get_contents("sql/$driver/$step"));
-                    echo "$sql\n";
-                    $db->exec($sql);
-                }
-            } catch (Exception $e) {
-                $db->exec("ROLLBACK");
-                print_r($e);
-                echo "\n================= fail\n\n";
-                exit(1);
-            }
-
-            $sth = $db->prepare("update core_vars set var_value = :version where var_name = 'dbVersion'");
-            $sth->bindParam('version', $v);
-            $sth->execute();
-
-            echo "\n================= done\n\n";
+        if ($version >= $v) {
+            echo "skipping version $v\n";
+            continue;
         }
 
-        $db->exec("COMMIT");
+        echo "upgradins to version $v\n";
+
+        try {
+            foreach ($steps as $step) {
+                echo "\n================= $step\n\n";
+                $sql = trim(file_get_contents("sql/$driver/$step"));
+                echo "$sql\n";
+                $db->exec($sql);
+            }
+        } catch (Exception $e) {
+            $db->exec("ROLLBACK");
+            print_r($e);
+            echo "\n================= fail\n\n";
+            exit(1);
+        }
+
+        $sth = $db->prepare("update core_vars set var_value = :version where var_name = 'dbVersion'");
+        $sth->bindParam('version', $v);
+        $sth->execute();
+
+        echo "\n================= done\n\n";
     }
 
+    $db->exec("COMMIT");
+}
