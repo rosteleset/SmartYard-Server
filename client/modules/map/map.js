bdaa39e1508e674157ed68fa379dc231b81e85a0
Mikhail Ivanov <mmikel@mail.ru>
2023-08-30 14:11:51 +0300

    WiP

--- client/modules/map/map.js
+++ client/modules/map/map.js
@@ -16,12 +16,27 @@
         
         modules.map.map = L.map('mapContainer');
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            minZoom: 4,
-            maxZoom: 18,
+        if (config.map && config.map.crs) {
+            switch (config.map.crs) {
+                case "EPSG3395":
+                    modules.map.map.options.crs = L.CRS.EPSG3395;
+                    break;
+                case "EPSG3857":
+                    modules.map.map.options.crs = L.CRS.EPSG3857;
+                    break;
+            }
+        }
+
+        L.tileLayer((config.map && config.map.tile)?config.map.tile:'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            minZoom: (config.map && config.map.min)?config.map.min:4,
+            maxZoom: (config.map && config.map.max)?config.map.max:18,
         }).addTo(modules.map.map);
 
-        let lat, lon, zoom = 18;
+        let 
+            lat = (config.map && config.map.default && config.map.default.lat)?config.map.default.lat:51.505,
+            lon = (config.map && config.map.default && config.map.default.lon)?config.map.default.lon:-0.09,
+            zoom = (config.map && config.map.default && config.map.default.zoom)?config.map.default.zoom:13
+        ;
 
         console.log(config.map);
 
@@ -43,14 +58,14 @@
             L.marker([lat, lon]).addTo(modules.map.map);
         } else {
             if (!navigator.geolocation) {
-                modules.map.map.setView([51.505, -0.09], zoom);
+                modules.map.map.setView([lat, lon], zoom);
             } else {
                 navigator.geolocation.getCurrentPosition(success => {
                     console.log(success.coords.latitude, success.coords.longitude);
                     modules.map.map.setView([success.coords.latitude, success.coords.longitude], zoom);
                     L.marker([success.coords.latitude, success.coords.longitude]).addTo(modules.map.map);
                 }, () => {
-                    modules.map.map.setView([51.505, -0.09], zoom);
+                    modules.map.map.setView([lat, lon], zoom);
                 });
             }
         }
