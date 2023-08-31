5fca9c807589d491db29d3ab07d95e137b506b8e
Selpol <warfcevip@gmail.com>
2023-08-31 14:50:46 +0300

    Refactoring code

--- server/controller/api/houses/domophone.php
+++ server/controller/api/houses/domophone.php
@@ -42,7 +42,7 @@ namespace api\houses {
             $households = loadBackend("households");
 
             if (array_key_exists('configure', $params) && $params['configure'])
-                task(new IntercomConfigureTask($params['_id'], $params["first"]))->high()->dispatch();
+                task(new IntercomConfigureTask($params['_id'], array_key_exists('first', $params) ? $params["first"] : false))->high()->dispatch();
 
             $success = $households->modifyDomophone($params["_id"], $params["enabled"], $params["model"], $params["server"], $params["url"], $params["credentials"], $params["dtmf"], $params["firstTime"], $params["nat"], $params["locksAreOpen"], $params["comment"]);
 
