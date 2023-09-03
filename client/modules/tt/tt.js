4bfb4076f10930d3efc817f5b87a4dc5b5e19420
Mikhail Ivanov <mmikel@mail.ru>
2023-09-03 16:38:35 +0300

    WiP

--- client/modules/tt/tt.js
+++ client/modules/tt/tt.js
@@ -1198,7 +1198,7 @@
                 }
             })(filtersTree);
     
-            filters += `</ul></span>`;
+            filters += `</ul></span> [${x}]`;
     
             let fp = -1;
             for (let i in project.filters) {
