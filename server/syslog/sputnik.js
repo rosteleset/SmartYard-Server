c99916d7cdcf67f2769597217febf0518e20ac1d
dfedotov <dfedotov@lanta-net.ru>
2023-09-04 13:13:38 +0300

    WIP: Sputnik events

--- server/syslog/sputnik.js
+++ server/syslog/sputnik.js
@@ -3,6 +3,7 @@ const url = require("url");
 const {hw: {sputnik}} = require("./config.json");
 const {getTimestamp} = require("./utils/getTimestamp");
 const API = require("./utils/api");
+const {mdTimer} = require("./utils/mdTimer");
 
 const port = new url.URL(sputnik).port;
 
@@ -14,8 +15,8 @@ const createLogMessage = data => {
 }
 
 const eventHandler = async data => {
-    const {device_id, date_time, event, Data: payload} = data;
-    const now = getTimestamp(new Date(date_time));
+    const {device_id: deviceId, date_time: datetime, event, Data: payload} = data;
+    const now = getTimestamp(new Date(datetime));
 
     switch (event) {
         case 'intercom.talking':
@@ -23,35 +24,41 @@ const eventHandler = async data => {
                 case 'cancel': // The call ended by pressing the cancel button or by timeout
                 case 'finish_handset': // CMS call ended
                 case 'finish_cloud': // SIP call ended
-                    await API.callFinished({date: now, ip: device_id});
+                    await API.callFinished({date: now, ip: deviceId});
                     break;
 
                 case 'open_door_handset': // Opening door by CMS handset
-                    await API.setRabbitGates({date: now, ip: device_id, apartmentNumber: parseInt(payload?.flat)});
+                    await API.setRabbitGates({date: now, ip: deviceId, apartmentNumber: parseInt(payload?.flat)});
                     break;
             }
             break;
 
-        case 'intercom.open_door':
-            await API.setRabbitGates({date: now, ip: device_id, apartmentNumber: parseInt(payload?.flat)});
+        case 'intercom.open_door': // Opening door by DTMF code
+            await API.setRabbitGates({date: now, ip: deviceId, apartmentNumber: parseInt(payload?.flat)});
             break;
 
         case 'intercom.key': // Opening door by RFID key
             if (payload.state === 'valid') {
                 const rfidParts = payload.id.match(/.{1,2}/g);
                 const rfid = rfidParts.reverse().join('').padStart(14, '0');
-                await API.openDoor({date: now, ip: device_id, door: 0, detail: rfid, by: 'rfid'});
+                await API.openDoor({date: now, ip: deviceId, door: 0, detail: rfid, by: 'rfid'});
             }
             break;
 
         case 'intercom.exit-button': // Opening main door by button pressed
-            await API.openDoor({date: now, ip: device_id, door: 0, detail: 'main', by: 'button'});
+            await API.openDoor({date: now, ip: deviceId, door: 0, detail: 'main', by: 'button'});
             break;
 
         default:
             if (payload?.action === 'digital_key') { // Opening door by personal code
-                await API.openDoor({date: now, ip: device_id, detail: payload.num, by: 'code'});
+                await API.openDoor({date: now, ip: deviceId, detail: payload.num, by: 'code'});
             }
+
+            if (payload?.msg === 'C pressed') { // Start face recognition (by cancellation button)
+                await API.motionDetection({date: now, ip: deviceId, motionActive: true});
+                await mdTimer(deviceId, 10000);
+            }
+
             break;
     }
 
