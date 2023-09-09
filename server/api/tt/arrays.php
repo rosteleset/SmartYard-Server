d7483c29c733f815f78c60273dd0a3bbd5197e3b
Mikhail Ivanov <mmikel@mail.ru>
2023-09-09 05:59:40 +0300

    WiP

---
+++ server/api/tt/arrays.php
@@ -0,0 +1,54 @@
+<?php
+
+    /**
+     * tt api
+     */
+
+    namespace api\tt {
+
+        use api\api;
+
+        /**
+         * tt (task tracker metadata(s)) method
+         */
+
+        class arrays extends api {
+
+            public static function POST($params)
+            {
+                $tt = loadBackend("tt");
+
+                if (!$tt) {
+                    return API::ERROR(500);
+                }
+
+                $success =  $tt->addArrayValue($params["issueId"], $param["field"], $params["value"]);
+
+                return api::ANSWER($success, ($success !== false)?false:"notAcceptable");
+            }
+
+            public static function DELETE($params)
+            {
+                $tt = loadBackend("tt");
+
+                if (!$tt) {
+                    return API::ERROR(500);
+                }
+
+                $success =  $tt->deleteArrayValue($params["issueId"], $param["field"], $params["value"]);
+
+                return api::ANSWER($success, ($success !== false)?false:"notAcceptable");
+            }
+
+            public static function index() {
+                if (loadBackend("tt")) {
+                    return [
+                        "POST" => "#same(tt,issue,PUT)",
+                        "DELETE" => "#same(tt,issue,DELETE)",
+                    ];
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
