70f63fea41068b8f244c2f9b2688e52a494a40d6
Mikhail Ivanov <mmikel@mail.ru>
2023-09-10 11:11:05 +0300

    WiP

--- client/modules/tt/settings.js
+++ client/modules/tt/settings.js
@@ -3305,6 +3305,7 @@
                 r.viewers.sort((a, b) => {
                     let f1 = (a.field.substring(0, 4) == "_cf_")?cf[a]:i18n("tt." + a.field);
                     let f2 = (b.field.substring(0, 4) == "_cf_")?cf[b]:i18n("tt." + b.field);
+                    console.log(f1, f2);
                     if (f1 > f2) {
                         return 1;
                     }
