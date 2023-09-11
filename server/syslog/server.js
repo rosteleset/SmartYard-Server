a6303798f1de5015ce04227679df64c2aed5741a
Anton Kulakov <anton.kulakoff@ya.ru>
2023-09-11 23:59:21 +0300

    refactor: update BewardService

--- server/syslog/server.js
+++ server/syslog/server.js
@@ -53,8 +53,6 @@ class SyslogService {
         const syslog = new syslogServer();
 
         syslog.on("message", async ({ date, host, message }) => {
-            console.log("DEBUG || createSyslogServer msg :");
-            console.log(message);
             // Get server timestamp
             const now = getTimestamp(date);
             let { host: addressFromMessageBody, message: msg } = parseSyslogMessage(message);
@@ -129,8 +127,74 @@ class BewardService extends SyslogService {
     }
 
     async handleSyslogMessage(now, host, msg) {
-        console.log("DEBUG || bw msg" + msg)
-        // filter logic
+        // Motion detection start
+        if (msg.indexOf("SS_MAINAPI_ReportAlarmHappen") >= 0) {
+            await API.motionDetection({ date: now, ip: host, motionActive: true });
+        }
+
+        // Motion detection: stop
+        if (msg.indexOf("SS_MAINAPI_ReportAlarmFinish") >= 0) {
+            await API.motionDetection({ date: now, ip: host, motionActive: false });
+        }
+
+        // Opening door by DTMF or CMS handset
+        if (msg.indexOf("Opening door by DTMF command") >= 0 || msg.indexOf("Opening door by CMS handset") >= 0) {
+            const apartmentNumber = parseInt(msg.split("apartment")[1]);
+            await API.setRabbitGates({ date: now, ip: host, apartmentNumber });
+        }
+
+        // Call in gate mode with prefix: potential white rabbit
+        if (msg.indexOf("Redirecting CMS call to") >= 0) {
+            const dst = msg.split("to")[1].split("for")[0];
+            gateRabbits[host] = {
+                ip: host,
+                prefix: parseInt(dst.substring(0, 5)),
+                apartmentNumber: parseInt(dst.substring(5)),
+            };
+        }
+
+        // Incoming DTMF for white rabbit: sending rabbit gate update
+        if (msg.indexOf("Incoming DTMF RFC2833 on call") >= 0) {
+            if (gateRabbits[host]) {
+                const { ip, prefix, apartmentNumber } = gateRabbits[host];
+                await API.setRabbitGates({ date: now, ip, prefix, apartmentNumber });
+            }
+        }
+
+        // Opening door by RFID key
+        if (
+            /^Opening door by RFID [a-fA-F0-9]+, apartment \d+$/.test(msg) ||
+            /^Opening door by external RFID [a-fA-F0-9]+, apartment \d+$/.test(msg)
+        ) {
+            const rfid = msg.split("RFID")[1].split(",")[0].trim();
+            const door = msg.indexOf("external") >= 0 ? "1" : "0";
+            await API.openDoor({ date: now, ip: host, door, detail: rfid, by: "rfid" });
+        }
+
+        // Opening door by personal code
+        if (msg.indexOf("Opening door by code") >= 0) {
+            const code = parseInt(msg.split("code")[1].split(",")[0]);
+            await API.openDoor({ date: now, ip: host, detail: code, by: "code" });
+        }
+
+        // Opening door by button pressed
+        if (msg.indexOf("door button pressed") >= 0) {
+            let door = 0;
+            let detail = "main";
+
+            if (msg.indexOf("Additional") >= 0) {
+                door = 1;
+                detail = "second";
+            }
+
+            await API.openDoor({ date: now, ip: host, door: door, detail: detail, by: "button" });
+        }
+
+        // All calls are done
+        if (msg.indexOf("All calls are done for apartment") >= 0) {
+            const callId = parseInt(msg.split("[")[1].split("]")[0]);
+            await API.callFinished({ date: now, ip: host, callId: callId });
+        }
     }
 }
 
@@ -148,11 +212,10 @@ const startDebugServer = (port) => {
 }
 
 // Check command-line parameter to start syslog service
-const serviceParam = process.argv[2].toLowerCase();
+const serviceParam = process.argv[2]?.toLowerCase();
 
 switch (serviceParam){
     case "beward":
-        // Running bewardService
         const bewardConfig = hw[serviceParam];
         const bewardService = new BewardService(bewardConfig);
         bewardService.createSyslogServer();
@@ -162,7 +225,6 @@ switch (serviceParam){
         console.log(`${serviceParam.toUpperCase()} syslog server running on port`)
         break;
     case "qtech":
-        // Running bewardService
         const qtechConfig = hw[serviceParam];
         const qtechService = new QtechService(qtechConfig);
         qtechService.createSyslogServer();
@@ -176,5 +238,5 @@ switch (serviceParam){
         // Running rebetekService
         break;
     default:
-        console.error('Invalid service parameter, Please use "bewart", "beward_ds", "qtech" ... on see documentation' )
+        console.error('Invalid service parameter, Please use "beward", "beward_ds", "qtech" ... on see documentation' )
 }
