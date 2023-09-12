21d1680c517f4902eb81180ec7dd8d67b3443a6a
Anton Kulakov <anton.kulakoff@ya.ru>
2023-09-12 09:33:57 +0300

    refactor: update intersvyaz and sputnik syslog services

--- server/syslog/server.js
+++ server/syslog/server.js
@@ -1,5 +1,7 @@
 const syslogServer = require("syslog-server");
 const net= require("net");
+const http = require("http");
+const url = require("url");
 const { hw, topology } = require("./config_v2.json");
 const { getTimestamp } = require("./utils/getTimestamp");
 const API = require("./utils/api");
@@ -323,13 +325,17 @@ class QtechService extends SyslogService {
     }
 }
 
-class IsService extends SyslogService {
+/** TODO:
+ *      -   check feature
+ *      -   think about the class name? (IsService || IntersvjazService SokolService || FalconService)
+ */
+class SokolService extends SyslogService {
     constructor(config) {
         super("is", config);
     }
 
     filterSpamMessages(msg) {
-        const isSpamKeywords = [
+        const sokolSpamKeywords = [
             "STM32.DEBUG",
             "Вызов метода",
             "Тело запроса",
@@ -343,7 +349,7 @@ class IsService extends SyslogService {
             "UART",
         ]
 
-        return isSpamKeywords.some(keyword => msg.includes(keyword));
+        return sokolSpamKeywords.some(keyword => msg.includes(keyword));
     }
 
     async handleSyslogMessage(now, host, msg) {
@@ -405,7 +411,6 @@ class IsService extends SyslogService {
  *      - add "qtech debug server"
  *      - check this feature
  */
-
 const startDebugServer = (port) => {
     const socket = net.createServer((socket) => {
         socket.on("data", async (data) => {
@@ -431,6 +436,88 @@ const startDebugServer = (port) => {
     });
 }
 
+// service for processing events from "Sputnik" cloud devices
+const startHttpServer = (port) => {
+    const createLogMessage = data => {
+    return Object.entries(data)
+        .filter(([key]) => key !== 'time')
+        .map(([key, value]) => `${key}: '${value}'`)
+        .join(', ');
+}
+
+const eventHandler = async data => {
+    const {device_id: deviceId, date_time: datetime, event, Data: payload} = data;
+    const now = getTimestamp(new Date(datetime));
+
+    switch (event) {
+        case 'intercom.talking':
+            switch (payload?.step) {
+                case 'cancel': // The call ended by pressing the cancel button or by timeout
+                case 'finish_handset': // CMS call ended
+                case 'finish_cloud': // SIP call ended
+                    await API.callFinished({date: now, ip: deviceId});
+                    break;
+
+                case 'open_door_handset': // Opening door by CMS handset
+                    await API.setRabbitGates({date: now, ip: deviceId, apartmentNumber: parseInt(payload?.flat)});
+                    break;
+            }
+            break;
+
+        case 'intercom.open_door': // Opening door by DTMF code
+            await API.setRabbitGates({date: now, ip: deviceId, apartmentNumber: parseInt(payload?.flat)});
+            break;
+
+        case 'intercom.key': // Opening door by RFID key
+            if (payload.state === 'valid') {
+                const rfidParts = payload.id.match(/.{1,2}/g);
+                const rfid = rfidParts.reverse().join('').padStart(14, '0');
+                await API.openDoor({date: now, ip: deviceId, door: 0, detail: rfid, by: 'rfid'});
+            }
+            break;
+
+        case 'intercom.exit-button': // Opening main door by button pressed
+            await API.openDoor({date: now, ip: deviceId, door: 0, detail: 'main', by: 'button'});
+            break;
+
+        default:
+            if (payload?.action === 'digital_key') { // Opening door by personal code
+                await API.openDoor({date: now, ip: deviceId, detail: payload.num, by: 'code'});
+            }
+
+            if (payload?.msg === 'C pressed') { // Start face recognition (by cancellation button)
+                await API.motionDetection({date: now, ip: deviceId, motionActive: true});
+                await mdTimer(deviceId, 10000);
+            }
+
+            break;
+    }
+
+    // await API.sendLog({date: now, ip: device_id, unit: "sputnik", msg: createLogMessage(payload)});
+}
+
+const httpServer = http.createServer((req, res) => {
+    let data = '';
+
+    req.on('data', chunk => {
+        data += chunk;
+    });
+
+    req.on('end', async () => {
+        try {
+            const jsonData = JSON.parse(data);
+            await eventHandler(jsonData);
+        } catch (error) {
+            console.error(error.message);
+        } finally {
+            res.writeHead(204).end();
+        }
+    });
+});
+
+httpServer.listen(port, () => console.log(`SPUTNIK HTTP server running on port ${port}`));
+}
+
 // Check command-line parameter to start syslog service
 const serviceParam = process.argv[2]?.toLowerCase();
 
@@ -452,12 +539,21 @@ switch (serviceParam){
         //Running debug server
         startDebugServer(qtechConfig.port)
         break;
+    case "is":
+        const sokolConfig = hw[serviceParam];
+        const sokolService = new SokolService(sokolConfig);
+        sokolService.createSyslogServer();
+        break;
     case "akuvox":
         // Running akuvoxService
         break;
     case "rebetek":
         // Running rebetekService
         break;
+    case "sputnik":
+        const sputnikConfig = hw[serviceParam];
+        startHttpServer(sputnikConfig.port)
+        break;
     default:
-        console.error('Invalid service parameter, Please use "beward", "beward_ds", "qtech" ... on see documentation' )
+        console.error('Invalid service parameter, Please use "beward", "qtech", "is" ... on see documentation' )
 }
