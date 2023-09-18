5c071925f41c79420dc916a8729df89b1754ef1a
Anton Kulakov <anton.kulakoff@ya.ru>
2023-09-18 18:06:41 +0300

    feat: update OmnyWebHookService

--- server/syslog/services/OmnyWebHookService.js
+++ server/syslog/services/OmnyWebHookService.js
@@ -1,11 +1,12 @@
 const {WebHookService} = require("./WebHookService");
-const { parseString } = require('xml2js');
-class OmnyWebHookService  extends  WebHookService{
+const {parseString} = require('xml2js');
+
+class OmnyWebHookService extends WebHookService {
     constructor(unit, config) {
         super(unit, config)
     }
 
-    async requestListener(request, response){
+    async requestListener(request, response) {
         let data = '';
         request.on('data', (chunk) => {
             data += chunk.toString();
@@ -13,16 +14,21 @@ class OmnyWebHookService  extends  WebHookService{
 
         request.on('end', async () => {
             parseString(data, (error, result) => {
-                if (error) console.error(error)
+                if (error) {
+                    console.error('Error parsing XML')
+                    res.writeHead(500, {'Content-Type': 'text/plain'});
+                    res.end('Error');
+                }
                 console.table(result)
             })
         })
     }
+
     async postEventHandler(req, data) {
 
     }
 
-    async getEventHandler (req, data){
+    async getEventHandler(req, data) {
 
     }
 
