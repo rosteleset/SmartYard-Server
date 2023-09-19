6f9281ca17fca0288f87d58fc541a3ecea12e937
Anton Kulakov <anton.kulakoff@ya.ru>
2023-09-19 17:39:58 +0300

    feat: update OmnyWebHookService

--- server/syslog/services/OmnyWebHookService.js
+++ server/syslog/services/OmnyWebHookService.js
@@ -1,5 +1,6 @@
 const {WebHookService} = require("./WebHookService");
 const {parseString} = require('xml2js');
+const {API, getTimestamp} = require("../utils");
 
 class OmnyWebHookService extends WebHookService {
     constructor(unit, config) {
@@ -7,8 +8,9 @@ class OmnyWebHookService extends WebHookService {
     }
 
     async requestListener(request, response) {
+        const now = getTimestamp(new Date());
         const sourceIP = request.connection.remoteAddress.split("::ffff:")[1];
-        if (request.url === this.config?.apiEndpoint && request.method === "POST"){
+        if (request.url === this.config?.apiEndpoint && request.method === "POST") {
             let data = '';
             request.on('data', (chunk) => {
                 data += chunk.toString();
@@ -16,8 +18,8 @@ class OmnyWebHookService extends WebHookService {
 
             request.on('end', async () => {
                 if (!data) {
-                    response.writeHead(400, {'Content-Type': 'application/json'})
-                    response.end(JSON.stringify({ message: "Request body is empty." }));
+                    response.writeHead(400, {'Content-Type': 'application/json'});
+                    response.end(JSON.stringify({message: "Request body is empty."}));
 
                     // TODO: make logger
                     console.error(`${new Date().toLocaleString("RU")} || ${request.connection.remoteAddress} || Request body is empty.`)
@@ -25,22 +27,16 @@ class OmnyWebHookService extends WebHookService {
                 }
 
                 parseString(data, async (error, result) => {
-                    if (error) {
-                        console.error("Error parsing XML:", error.message)
-                    }
-
-                    // console.table(request)
-                    await this.parsedDataHandler({sourceIP, ...result});
-
+                    if (error) console.error("Error parsing XML:", error.message);
+                    await this.parsedDataHandler({sourceIP, now, ...result});
                     response.writeHead(202, {'Content-Type': 'application/json'})
-                    response.end(JSON.stringify({ message: "Webhook received and processed." }));
+                    response.end(JSON.stringify({message: "Webhook received and processed."}));
                 })
 
             })
-        }
-        else {
-            response.writeHead(405, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify({ message: "Method not allowed." }));
+        } else {
+            response.writeHead(405, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({message: "Method not allowed."}));
         }
 
     }
@@ -53,31 +49,29 @@ class OmnyWebHookService extends WebHookService {
 
     async parsedDataHandler(parsedData) {
         try {
+            const {sourceIP, now, event: {title: [title], time: [time], status: [status]}} = parsedData;
+            // TODO : add event handlers
 
-        } catch (error){
+            // motion detect logic
+            if (title === "motion_dect") {
+                if (status === "start") {
+                    await API.motionDetection({date: now, ip: sourceIP, motionActive: true});
+                } else if (status === "end") {
+                    await API.motionDetection({date: now, ip: sourceIP, motionActive: false});
+                }
+            }
+
+            // Human detect logic
+            if (title === "human_dect") {
+            }
+
+            // Crossing line logic
+            if (title === "crossing_dect") {
+            }
+        } catch (error) {
             console.error()
         }
-        // TODO: - add check event param handler
-        const {sourceIP, event: {title:[title], time:[time], status:[status]}} = parsedData;
 
-        // TODO : add event handlers
-
-        // motion detect
-        if (title === "motion_dect" ) {
-            // motion handler logic
-            console.log("motion_dect handler start")
-        }
-
-       // Human detect
-        if (title === "human_dect" ) {
-            // motion handler logic
-            console.log("human_dect handler start")
-        }
-
-        // Crossing line
-        if (title === "crossing_dect" ) {
-
-        }
     }
 
 
