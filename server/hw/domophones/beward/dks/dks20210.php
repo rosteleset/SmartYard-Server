f43ecf1c40d79627db6b226651019e2fb7268928
dfedotov <dfedotov@lanta-net.ru>
2023-09-19 15:53:10 +0300

    Add DKS20210 (Bi-Scan)

---
+++ server/hw/domophones/beward/dks/dks20210.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace hw\domophones {
+
+        require_once 'dks.php';
+
+        class dks20210 extends dks {
+
+            protected array $cms_models = [
+                'KKM-100S2' => 0,
+                'KKM-105' => 1,
+                'KAD2501' => 2,
+            ];
+
+        }
+    }
