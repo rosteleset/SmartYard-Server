913f0324f6aa8e3e23b51c118efe6d1f3e98abb3
Mikhail Ivanov <mmikel@mail.ru>
2023-10-28 06:53:17 +0300

    WiP

--- server/sql/pgsql/v15_core.sql
+++ server/sql/pgsql/v15_core.sql
@@ -1,3 +1,4 @@
+ALTER TABLE core_users DROP COLUMN IF EXISTS push_token;
 CREATE TABLE IF NOT EXISTS core_users_tokens(uid integer not null, token character varying);
 CREATE INDEX IF NOT EXISTS core_users_tokens_uid on core_users_tokens(uid);
 CREATE UNIQUE INDEX IF NOT EXISTS core_users_tokens_uniq on core_users_tokens(uid, token);
