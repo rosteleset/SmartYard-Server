f97a09b8a395790378bf256eb9649c941976c004
Mikhail Ivanov <mmikel@mail.ru>
2023-10-28 06:50:23 +0300

    WiP

--- server/sql/pgsql/v15_core.sql
+++ server/sql/pgsql/v15_core.sql
@@ -1,1 +1,3 @@
-ALTER TABLE core_users ADD IF NOT EXISTS push_token character varying;
+CREATE TABLE IF NOT EXISTS core_users_tokens(uid integer not null, token character varying);
+CREATE INDEX IF NOT EXISTS core_users_tokens_uid on core_users_tokens(uid);
+CREATE UNIQUE INDEX IF NOT EXISTS core_users_tokens_uniq on core_users_tokens(uid, token);
--- server/sql/sqlite/v15_core.sql
+++ server/sql/sqlite/v15_core.sql
@@ -1,1 +1,3 @@
-ALTER TABLE core_users ADD COLUMN push_token text;
+CREATE TABLE core_users_tokens(uid integer not null, token character varying);
+CREATE INDEX core_users_tokens_uid on core_users_tokens(uid);
+CREATE UNIQUE INDEX core_users_tokens_uniq on core_users_tokens(uid, token);
