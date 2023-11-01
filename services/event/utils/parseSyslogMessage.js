c5dfcf400acace5295ba8c875589ac36bf32aff4
Anton Kulakov <anton.kulakoff@ya.ru>
2023-11-01 18:02:21 +0300

    refactor: update syslog parser, add BSD format for Akuvox

--- services/event/utils/parseSyslogMessage.js
+++ services/event/utils/parseSyslogMessage.js
@@ -5,24 +5,38 @@ const parseSyslogMessage = (str) => {
     str = str.trim();
 
     // Check if the message follows the RFC 5424 format
-    const regex = /<(?<priority>\d{1,3})>(?<version>\d+) (?<date>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?\w?(?:[+-]\d{2}:\d{2})?) (?<hostname>\S+) (?<app>\S+) (?<pid>\S+) (?<msg_id>\S+)\s-\s(?<message>.*)$/;
-    const parts = regex.exec(str);
+    const regexIETF = /<(?<priority>\d{1,3})>(?<version>\d+) (?<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?\w?(?:[+-]\d{2}:\d{2})?) (?<hostname>\S+) (?<app>\S+) (?<pid>\S+) (?<msg_id>\S+)\s-\s(?<message>.*)$/;
 
-    if (parts) {
-        const [, priority, version, date, hostname, app, pid, msg_id, message] = parts;
+    // BSD RFC 3164 format
+    // const regexBSB = /<(?<priority>\d{1,3})>(?<timestamp>\w+\s+\d{1,2}\s\d{2}:\d{2}:\d{2})\s(?<host>\S+)?\s(?<app>[\w.-]+)\s(?<pid>\S+):\s(?<message>.*)$/;
+    const regexBSB = /<(?<priority>\d{1,3})>(?<timestamp>\w+\s+\d{1,2}\s\d{2}:\d{2}:\d{2})\s(?<host>\S+)?\s(?<app>[\w\s.]+)\s(?<pid>\S+):\s(?<message>.*)$/;
+
+    const partsIETF = regexIETF.exec(str);
+    const partsBSD = regexBSB.exec(str);
+
+    if (partsIETF) {
+        const [, priority, version, timestamp, hostname, app, pid, msg_id, message] = partsIETF;
         return {
             format: 'RFC5424',
             priority: Number(priority),
             version: Number(version),
-            date: getTimestamp(new Date(date)),
+            timestamp: getTimestamp(new Date(timestamp)),
             hostname,
             app,
             pid,
             message
         };
+    } else if (partsBSD) {
+        const [, priority,timestamp ,host, app,pid, message] = partsBSD;
+        return {
+            format: 'BSD',
+            priority: Number(priority),
+            hostname: host,
+            pid,
+            app,
+            message
+        }
     } else return false;
-    //TODO:
-    //      - add checking BSD syslog format
 };
 
 module.exports = {parseSyslogMessage};
