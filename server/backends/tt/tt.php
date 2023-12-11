451f99af68bd43bec89820d6bfb9f6fff06f4a7d
Mikhail Ivanov <mmikel@mail.ru>
2023-12-11 09:50:05 +0300

    WiP

--- server/backends/tt/tt.php
+++ server/backends/tt/tt.php
@@ -245,7 +245,7 @@
                             return [ mb_substr(...$args) ];
                         },
                         "trim" => function ($str) {
-                            return [ preg_replace('~^\s+|\s+$~u', '', $str) ];
+                            return [ trim(preg_replace('~^\s+|\s+$~u', '', $str)) ];
                         }
                     ]);
 
