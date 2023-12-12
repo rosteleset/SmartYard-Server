94be8506e079480d3587cb0c57b8915743da976f
Anton Kulakov <anton.kulakoff@ya.ru>
2023-12-13 00:01:51 +0300

    doc: update jsdoc for mdTimer function

--- services/event/utils/mdTimer.js
+++ services/event/utils/mdTimer.js
@@ -3,6 +3,13 @@ const { getTimestamp } = require("./getTimestamp");
 
 const mdStorage = {};
 
+/**
+ * Stop-motion detection for a device.
+ * @param {string} deviceId - unique identifier for the device.
+ * @param {string} ip - IP address of the device.
+ * @param {string} subId - Sub-identifier of the device.
+ * @returns {Promise<void>} - A promise that resolves when motion detection is stopped.
+ */
 const mdStop = async (deviceId, ip, subId) => {
     const now = getTimestamp(new Date());
     await API.motionDetection({date: now, ip, subId, motionActive: false});
@@ -10,16 +17,17 @@ const mdStop = async (deviceId, ip, subId) => {
 }
 
 /**
- * Used for devices that do not have a “Stop motion detection” event, default is 5 seconds
- * @param host
- * @param delay
+ * Sets a timer to stop-motion detection for a device after a specified delay.
+ * @param {Object} options - options for the timer.
+ * @param {string|null} options.ip - IP address of the device.
+ * @param {string|null} options.subId - Sub-identifier of the device.
+ * @param {number} options.delay - Delay in milliseconds before stopping motion detection.
  */
 const mdTimer = ({ip = null, subId = null, delay = 5000}) => {
     const deviceId = ip || subId;
     if (deviceId && mdStorage[deviceId]) {
         clearTimeout(mdStorage[deviceId]);
         mdStorage[deviceId] = setTimeout(mdStop, delay, deviceId, ip, subId);
-
     }
 }
 
