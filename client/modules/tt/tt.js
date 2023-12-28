8ab2f3297da1802bc9e25709641733a2bd782465
Mikhail Ivanov <mmikel@mail.ru>
2023-12-28 10:49:20 +0300

    WiP

--- client/modules/tt/tt.js
+++ client/modules/tt/tt.js
@@ -1458,9 +1458,10 @@
             $(".tt_pager").off("click").on("click", function () {
                 if (target) {
                     loadingStart();
+                    console.log(params);
                     params.skip = Math.max(0, (parseInt($(this).attr("data-page")) - 1) * limit);
                     params.limit = limit?limit:modules.tt.defaultIssuesPerPage;
-                    console.log(params, $(this).attr("data-target"));
+                    console.log(params);
                     modules.tt.renderIssues(params, true, $(this).attr("data-target"), loadingDone);
                 } else {
                     modules.tt.selectFilter(false, Math.max(0, (parseInt($(this).attr("data-page")) - 1) * limit));
