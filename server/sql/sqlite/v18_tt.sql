8837c25dc36061c79834ecebca15f3b4e137c6d2
Mikhail Ivanov <mmikel@mail.ru>
2023-12-31 07:49:58 +0300

    WiP

---
+++ server/sql/sqlite/v18_tt.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tt_prints (
+    tt_print_id INTEGER AUTOINCREMENT PRIMARY KEY,
+    form_name TEXT NOT NULL,
+    extension TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX tt_prings_uniq ON tt_prints (form_name, extension);
