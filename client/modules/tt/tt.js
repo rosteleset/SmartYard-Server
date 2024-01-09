dc3b54310a9f745cb6ee3cf120e9ab4db6478f21
Mikhail Ivanov <mmikel@mail.ru>
2024-01-09 18:20:17 +0300

    WiP

--- client/modules/tt/tt.js
+++ client/modules/tt/tt.js
@@ -1266,9 +1266,11 @@
                 f[tree[tree.length - 1]] = project.filters[i];
             }
 
-            filters += `<span class="pointer dropdown-toggle dropdown-toggle-no-icon text-primary text-bold" id="ttFilter" data-toggle="dropdown" data-boundary="window" aria-haspopup="true" aria-expanded="false" style="margin-left: -4px;"><i class="far fa-fw fa-caret-square-down mr-1"></i>${(modules.tt.meta.filters[x]?modules.tt.meta.filters[x]:i18n("tt.filter")).replaceAll("/", "<i class='fas fa-fw fa-xs fa-angle-double-right'></i>")}</span>`;
+            let filter_name = (modules.tt.meta.filters[x]?modules.tt.meta.filters[x]:i18n("tt.filter")).replaceAll("/", "<i class='fas fa-fw fa-xs fa-angle-double-right'></i>");
+            filters += `<span class="pointer dropdown-toggle dropdown-toggle-no-icon text-primary text-bold" id="ttFilter" data-toggle="dropdown" data-boundary="window" aria-haspopup="true" aria-expanded="false" style="margin-left: -4px;"><i class="far fa-fw fa-caret-square-down mr-1"></i>${filter_name}</span>`;
             filters += `<ul class="dropdown-menu" aria-labelledby="ttFilter">`;
-    
+            document.title = i18n("windowTitle") + " :: " + filter_name;
+
             (function hh(t) {
                 let fMy = [];
                 let fFolders = [];
