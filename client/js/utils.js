4e6cb852b43d627e217e9590e6361e08dc28ec7a
Mikhail Ivanov <mmikel@mail.ru>
2024-02-02 07:28:46 +0300

    WiP

---
+++ client/js/utils.js
@@ -0,0 +1,272 @@
+function isEmpty(v) {
+    let f = !!v;
+
+    if (Array.isArray(v)) {
+        f = f && v.length;
+    }
+
+    if (typeof v == "object" && !Array.isArray(v)) {
+        f = f && Object.keys(v).length;
+    }
+
+    return !f;
+}
+
+function pad2(n) {
+    return (n < 10 ? '0' : '') + n;
+}
+
+function ttDate(date, dateOnly, skip) {
+    if (date) {
+        date = new Date(date * 1000);
+        if (dateOnly) {
+            return date.toLocaleDateString();
+        } else {
+            return date.toLocaleDateString() + " " + pad2(date.getHours()) + ":" + pad2(date.getMinutes());
+        }
+    } else {
+        if (skip) {
+            return false;
+        } else {
+            return "&nbsp;"
+        }
+    }
+}
+
+function utf8_to_b64(str) {
+    return window.btoa(unescape(encodeURIComponent(str)));
+}
+
+function b64_to_utf8(str) {
+    return decodeURIComponent(escape(window.atob(str)));
+}
+
+function trimStr(str, len, abbr) {
+    if (!len) {
+        len = 33;
+    }
+    let sub = Math.floor((len - 3) / 2);
+    if (str.length > len) {
+        if (abbr) {
+            return "<abbr title='" + escapeHTML(str) + "'>" + str.substring(0, sub) + "..." + str.substring(str.length - sub) + "</abbr>";
+        } else {
+            return str.substring(0, sub) + "..." + str.substring(str.length - sub);
+        }
+    } else {
+        return str;
+    }
+}
+
+function setFavicon(icon, unreaded) {
+    if (typeof unreaded == 'undefined') {
+        unreaded = 0;
+    }
+
+    if ($.browser.chrome) {
+        $('#favicon').attr('href', icon);
+    } else {
+        document.head || (document.head = document.getElementsByTagName('head')[0]);
+        let link = document.createElement('link');
+        let oldLink = document.getElementById('dynamic-favicon');
+        link.id = 'dynamic-favicon';
+        link.rel = 'shortcut icon';
+        link.href = icon;
+        if (oldLink){
+            document.head.removeChild(oldLink);
+        }
+        document.head.appendChild(link);
+    }
+
+    badge = new Favico({ animation: 'none', bgColor: '#000000' });
+
+    if (unreaded) {
+        if (unreaded <= 9 || !parseInt(unreaded)) {
+            badge.badge(unreaded);
+        } else {
+            badge.badge('9+');
+        }
+    }
+}
+
+function xblur() {
+    setTimeout(() => {
+        $('a, input, button, .nav-item').blur();
+    }, 100);
+}
+
+function autoZ(target) {
+    let maxZ = Math.max.apply(null, $.map($('body > *:visible'), function(e) {
+        if (e === target) {
+            return 1;
+        } else {
+            // no great than 9999999
+            let z = parseInt($(e).css('z-index'));
+            if (z < 9999999) {
+                return parseInt($(e).css('z-index')) || 1;
+            } else {
+                return 1;
+            }
+        }
+    }));
+
+    maxZ = Math.max(maxZ, 100500);
+
+    if (target) {
+        target.css('z-index', maxZ + 1);
+    }
+
+    return target;
+}
+
+function nl2br(str) {
+    if (str && typeof str == "string") {
+        return str.split("\n").join("<br />");
+    } else {
+        return "";
+    }
+}
+
+function formatBytes(bytes) {
+    let u = 0;
+    for (; bytes > 1024; u++) bytes /= 1024;
+    return Math.round(bytes) + ' ' + [ 'B', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y' ][u];
+}
+
+function escapeHTML(str) {
+    if (typeof str == "undefined" || !str) {
+        return "";
+    }
+    
+    str = str.toString();
+
+    let escapeChars = {
+        '¢': 'cent',
+        '£': 'pound',
+        '¥': 'yen',
+        '€': 'euro',
+        '©':'copy',
+        '®': 'reg',
+        '<': 'lt',
+        '>': 'gt',
+        '"': 'quot',
+        '&': 'amp',
+        '\'': '#39'
+    };
+
+    let regexString = '[';
+
+    for(let key in escapeChars) {
+        regexString += key;
+    }
+
+    regexString += ']';
+
+    let regex = new RegExp(regexString, 'g');
+
+    let result = str.replace(regex, function(m) {
+        return '&' + escapeChars[m] + ';';
+    });
+
+    return result;
+}
+
+function textRTrim(text) {
+    text = text.split("\n");
+
+    for (let i in text) {
+        text[i] = text[i].trimRight()
+    }
+
+    return text.join("\n");
+}
+
+function parseIntEx(i) {
+    i = parseInt(i);
+
+    if (isNaN(i)) {
+        return 0;
+    } else {
+        return i;
+    }
+}
+
+function parseFloatEx(f, r) {
+    f = parseFloat(`${f}`.toString().replaceAll(",", "."));
+
+    if (r !== false && typeof r !== "undefined") {
+        f = Math.round(f * (10 ** r)) / (10 ** r);
+    }
+
+    if (isNaN(f)) {
+        return 0;
+    } else {
+        return f;
+    }
+}
+
+function convertLinks(input) {
+    let text = input;
+    const aLink = [];
+    const linksFound = text.match(/(?:www|https?)[^(\s|\)|\])]+/g);
+
+    if (linksFound != null) {
+        for (let i = 0; i < linksFound.length; i++) {
+            let replace = linksFound[i];
+
+            if (!(linksFound[i].match(/(http(s?)):\/\//))) {
+                replace = 'http://' + linksFound[i];
+            }
+
+            let linkText = replace.split('/')[2];
+
+            if (linkText.substring(0, 3) == 'www') {
+                linkText = linkText.replace('www.', '');
+            }
+
+            if (linkText.match(/youtu/)) {
+                const youtubeID = replace.split('/').slice(-1)[0].split('=')[1];
+
+                if (youtubeID === undefined || youtubeID === '') {
+                    aLink.push('<a onclick="xblur()" href="' + replace + '" target="_blank">' + linkText + '</a>');
+                } else {
+                    aLink.push('<span class="video-wrapper"><iframe src="https://www.youtube.com/embed/' + youtubeID + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></span>');
+                }
+            } else {
+                aLink.push('<a onclick="xblur()" href="' + replace + '" target="_blank">' + linkText + '</a>');
+            }
+
+            text = text.split(linksFound[i]).map(item => {
+                return aLink[i].includes('iframe') ? item.trim() : item;
+            }).join(aLink[i]);
+        }
+        return text;
+    } else {
+        return input;
+    }
+}
+
+function getMonthDifference(startDate, endDate) {
+    return endDate.getMonth() - startDate.getMonth() + 12 * (endDate.getFullYear() - startDate.getFullYear());
+}
+
+function findBootstrapEnvironment() {
+    let envs = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+    let el = document.createElement('div');
+    document.body.appendChild(el);
+
+    let curEnv = envs.shift();
+
+    for (let env of envs.reverse()) {
+        el.classList.add(`d-${env}-none`);
+
+        if (window.getComputedStyle(el).display === 'none') {
+            curEnv = env;
+            break;
+        }
+    }
+
+    document.body.removeChild(el);
+    return curEnv;
+}
+
---
+++ client/js/widgets.js
@@ -0,0 +1,260 @@
+function message(message, caption, timeout) {
+    timeout = timeout?timeout:15;
+    toastr.info(message, caption?caption:i18n("message"), {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": false,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": true,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": timeout?(timeout * 1000):"0",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    });
+}
+
+function warning(message, caption, timeout) {
+    timeout = timeout?timeout:15;
+    toastr.warning(message, caption?caption:i18n("warning"), {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": false,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": true,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": timeout?(timeout * 1000):"0",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    });
+}
+
+function error(message, caption, timeout) {
+    timeout = timeout?timeout:15;
+    toastr.error(message, caption?caption:i18n("error"), {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": false,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": true,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": timeout?(timeout * 1000):"0",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    });
+}
+
+function mConfirm(body, title, button, callback) {
+    if (!title) {
+        title = i18n("confirm");
+    }
+    $('#confirmModalLabel').html(title);
+    $('#confirmModalBody').html(body);
+    let bc = 'btn-primary';
+    button = button.split(':');
+    if (button.length === 2) {
+        bc = 'btn-' + button[0];
+        button = button[1];
+    } else {
+        button = button[0];
+    }
+    $('#confirmModalButton').removeClass('btn-primary btn-secondary btn-success btn-danger btn-warning btn-info btn-light btn-dark btn-link').addClass(bc).html(button).off('click').on('click', () => {
+        $('#confirmModal').modal('hide');
+        if (typeof callback == 'function') callback();
+    });
+    autoZ($('#confirmModal')).modal('show');
+    xblur();
+}
+
+let mYesNoTimeout = 0;
+
+function mYesNo(body, title, callbackYes, callbackNo, yes, no, timeout) {
+    if (mYesNoTimeout) {
+        clearTimeout(mYesNoTimeout);
+    }
+
+    if (!title) {
+        title = i18n("confirm");
+    }
+
+    $('#yesnoModalLabel').html(title);
+    $('#yesnoModalBody').html(body);
+    let t = yes?yes:i18n("yes");
+    t = t.charAt(0).toUpperCase() + t.substring(1);
+    $('#yesnoModalButtonYes').html(t).off('click').on('click', () => {
+        $('#yesnoModal').modal('hide');
+        if (typeof callbackYes == 'function') callbackYes();
+    });
+    t = no?no:i18n("no");
+    t = t.charAt(0).toUpperCase() + t.substring(1);
+    $('#yesnoModalButtonNo').html(t).off('click').on('click', () => {
+        $('#yesnoModal').modal('hide');
+        if (typeof callbackNo == 'function') callbackNo();
+    });
+    autoZ($('#yesnoModal')).modal('show');
+    xblur();
+
+    if (timeout) {
+        mYesNoTimeout = setTimeout(() => {
+            mYesNoTimeout = 0;
+            $('#yesnoModal').modal('hide');
+        }, timeout);
+    }
+}
+
+function mAlert(body, title, callback, title_button, main_button) {
+    if (!title) {
+        title = i18n("message");
+    }
+    if (title.toLowerCase().indexOf(i18n("error").toLowerCase()) >= 0) {
+        title = '<span class="text-danger">' + title + '</span>';
+    }
+    if (title.toLowerCase().indexOf(i18n("warning").toLowerCase()) >= 0) {
+        title = '<span class="text-warning">' + title + '</span>';
+    }
+    if (title.toLowerCase().indexOf(i18n("message").toLowerCase()) >= 0) {
+        title = '<span class="text-success">' + title + '</span>';
+    }
+    let l = $('#alertModalLabel').html(title);
+    if (title_button) {
+        l.next().remove();
+        l.parent().append($(title_button));
+    }
+    $('#alertModalBody').html(body);
+    if (main_button) {
+        $('#alertModalButton').html(main_button);
+    } else {
+        $('#alertModalButton').html(i18n("ok"));
+    }
+    $('#alertModalButton').off('click').on('click', (e) => {
+        $('#alertModal').modal('hide');
+        if (typeof callback == 'function') callback();
+        e.stopPropagation();
+    });
+    autoZ($('#alertModal')).modal('show');
+    xblur();
+}
+
+function modal(body) {
+    $("#modalBody").html(body);
+    xblur();
+    return autoZ($('#modal')).modal({
+        backdrop: 'static',
+        keyboard: true,
+        show: true,
+    });
+}
+
+function FAIL(response) {
+    if (response && response.responseJSON && response.responseJSON.error) {
+        error(i18n("errors." + response.responseJSON.error), i18n("error"), 30);
+        if (response.responseJSON.error == "tokenNotFound") {
+            lStore("_token", null);
+            setTimeout(() => {
+                location.reload();
+            }, 5000);
+        }
+    } else {
+        error(i18n("errors.unknown"), i18n("errorCode", response.status), 30);
+    }
+}
+
+function FAILPAGE(response) {
+    if (response && response.responseJSON && response.responseJSON.error) {
+        error(i18n("errors." + response.responseJSON.error), i18n("error"), 30);
+        pageError(i18n("errors." + response.responseJSON.error));
+    } else {
+        error(i18n("errors.unknown"), i18n("errorCode", response.status), 30);
+        pageError();
+    }
+    loadingDone();
+}
+
+function loadingStart() {
+    autoZ($('#loading').modal({
+        backdrop: 'static',
+        keyboard: false,
+    }));
+//    autoZ($('#loading')).show();
+}
+
+function loadingDone(stayHidden) {
+    xblur();
+    
+    $('#loading').modal('hide');
+//    $('#loading').hide();
+
+    if (stayHidden === true) {
+        $('#app').addClass("invisible");
+    } else {
+        $('#app').removeClass("invisible");
+    }
+
+    $(window).resize();
+}
+
+function timeoutStart() {
+    autoZ($('#timeout').modal({
+        backdrop: 'static',
+        keyboard: false,
+    }));
+    $('.timeout-animate').each(function () {
+        this.beginElement();
+    });
+}
+
+function timeoutDone() {
+    $('#timeout').modal('hide');
+}
+
+function page404() {
+    $("#mainForm").html("");
+    $("#altForm").hide();
+    loadingDone();
+    document.title = `${i18n("windowTitle")} :: 404`;
+    $("#page404").html(`
+        <section class="content">
+            <div class="error-page">
+                <img src="img/404.png" style="border: none; width: 200px; height: 200px;">
+                <div class="error-content">
+                    <h3><i class="fas fa-exclamation-triangle text-danger"></i>${i18n("errors.404caption")}</h3>
+                    <p>${i18n("errors.404message")}</p>
+                </div>
+            </div>
+        </section>
+    `).show();
+}
+
+function pageError(error) {
+    $("#mainForm").html("");
+    $("#subTop").html("");
+    $("#altForm").hide();
+    loadingDone();
+    document.title = `${i18n("windowTitle")} :: ${i18n("error")}`;
+    $("#pageError").html(`
+        <section class="content">
+            <div class="error-page">
+                <h2 class="headline text-danger mr-4"> Error</h2>
+                <div class="error-content">
+                    <h3><i class="fas fa-exclamation-triangle text-danger"></i>${i18n("error")}</h3>
+                    <p>${error?error:i18n("errors.unknown")}</p>
+                </div>
+            </div>
+        </section>
+    `).show();
+}
+
