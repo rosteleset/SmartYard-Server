44d85065d1a713060eb8ea8d3c37b72a4a6eff04
Mikhail Ivanov <mmikel@mail.ru>
2024-02-09 04:58:25 +0300

    WiP

--- server/kamailio.php
+++ server/kamailio.php
@@ -1,6 +1,8 @@
 <?php
+
     mb_internal_encoding("UTF-8");
-    require_once "Kamailio/Kamailio.php";
+
+    require_once "kamailio/kamailio.php";
     require_once "utils/error.php";
     require_once "utils/loader.php";
     require_once "utils/checkint.php";
@@ -9,7 +11,7 @@
     require_once "utils/api_response.php";
     require_once "backends/backend.php";
 
-    use Kamailio\Kamailio;
+    use kamailio\kamailio;
 
     // get global configuration
     try {
@@ -22,14 +24,13 @@
         exit(1);
     }
 
-     try {
+    try {
         $db = new PDO_EXT(@$config["db"]["dsn"], @$config["db"]["username"], @$config["db"]["password"], @$config["db"]["options"]);
     } catch (Exception $err) {
-        response(500, false, false,[
-                "Can't open database " . $config["db"]["dsn"],
-                $err->getMessage(),
-            ],
-        );
+        response(500, false, false, [
+            "Can't open database " . $config["db"]["dsn"],
+            $err->getMessage(),
+        ]);
         exit(1);
     }
 
