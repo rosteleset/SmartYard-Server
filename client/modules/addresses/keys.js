04e6fe1ba3d0892ae86dcacefdd46121ad51814e
Mikhail Ivanov <mmikel@mail.ru>
2024-02-12 04:24:03 +0300

    WiP

--- client/modules/addresses/keys.js
+++ client/modules/addresses/keys.js
@@ -1,12 +1,34 @@
 ({
+    menuItem: false,
+
     init: function () {
         if (AVAIL("subscribers", "keys")) {
-            leftSide("fab fa-fw fa-keycdn", i18n("addresses.superkeys"), "?#addresses.keys", "households");
+            this.menuItem = leftSide("fab fa-fw fa-keycdn", i18n("addresses.superkeys"), "?#addresses.keys", "households");
         }
         moduleLoaded("addresses.keys", this);
     },
+    
+    renderKeys: function () {
+
+    },
+
+    addKey: function () {
+
+    },
+
+    removeKey: function () {
+
+    },
+
+    modifyKey: function () {
+
+    },
 
     route: function (params) {
+        if (modules.addresses.keys.menuItem) {
+            $("#" + modules.addresses.keys.menuItem).children().first().attr("href", "?#addresses.keys&_=" + Math.random());
+        }
+
         loadingDone();
     },
 }).init();
