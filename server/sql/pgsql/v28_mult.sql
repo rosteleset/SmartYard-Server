027e4dd1ba525ff0509cf039e0819506c60b0b8d
Mikhail Ivanov <mmikel@mail.ru>
2024-02-28 07:57:58 +0300

    WiP

---
+++ server/sql/pgsql/v28_mult.sql
@@ -0,0 +1,30 @@
+CREATE TABLE houses_subscribers_devices
+(
+    subscriber_device_id SERIAL PRIMARY KEY,
+    house_subscriber_id INTEGER,
+    device_token CHARACTER VARYING,
+    auth_token CHARACTER VARYING,
+    platform INTEGER,                                                                                                   -- 0 - android, 1 - ios
+    push_token CHARACTER VARYING,
+    push_token_type INTEGER,                                                                                            -- 0 - fcm, 1 - apple, 2 - apple (dev), 3 - huawei
+    voip_token CHARACTER VARYING,                                                                                       -- iOs only
+    registered INTEGER,                                                                                                 -- UNIX timestamp
+    last_seen INTEGER,                                                                                                  -- UNIX timestamp
+    voip_enabled INTEGER
+);
+CREATE UNIQUE INDEX houses_subscribers_devices_uniq_1 on houses_subscribers_devices(device_token);
+CREATE UNIQUE INDEX houses_subscribers_devices_uniq_2 on houses_subscribers_devices(auth_token);
+CREATE UNIQUE INDEX houses_subscribers_devices_uniq_3 on houses_subscribers_devices(push_token);
+CREATE INDEX houses_subscribers_devices_house_subscriber_id on houses_subscribers_devices(house_subscriber_id);
+
+-- cameras <-> entrances
+CREATE TABLE houses_entrances_cameras
+(
+    camera_id INTEGER NOT NULL,
+    house_entrance_id INTEGER NOT NULL,
+    path CHARACTER VARYING,
+    distance INTEGER                                                                                                    -- "distance" (order) (virtual) from camera to domophone
+);
+CREATE UNIQUE INDEX houses_entrances_cameras_uniq on houses_entrances_cameras(camera_id, house_entrance_id);
+CREATE INDEX houses_entrances_cameras_camera_id on houses_entrances_cameras(camera_id);
+CREATE INDEX houses_entrances_cameras_house_entrance_id on houses_entrances_cameras(house_entrance_id);
---
+++ server/sql/sqlite/v28_mult.sql
@@ -0,0 +1,30 @@
+CREATE TABLE houses_subscribers_devices
+(
+    subscriber_device_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    house_subscriber_id INTEGER,
+    device_token TEXT,
+    auth_token TEXT,
+    platform INTEGER,                                                                                                   -- 0 - android, 1 - ios
+    push_token TEXT,
+    push_token_type INTEGER,                                                                                            -- 0 - fcm, 1 - apple, 2 - apple (dev), 3 - huawei
+    voip_token TEXT,                                                                                                    -- iOs only
+    registered INTEGER,                                                                                                 -- UNIX timestamp
+    last_seen INTEGER,                                                                                                  -- UNIX timestamp
+    voip_enabled INTEGER
+);
+CREATE UNIQUE INDEX houses_subscribers_devices_uniq_1 on houses_subscribers_devices(device_token);
+CREATE UNIQUE INDEX houses_subscribers_devices_uniq_2 on houses_subscribers_devices(auth_token);
+CREATE UNIQUE INDEX houses_subscribers_devices_uniq_3 on houses_subscribers_devices(push_token);
+CREATE INDEX houses_subscribers_devices_house_subscriber_id on houses_subscribers_devices(house_subscriber_id);
+
+-- cameras <-> entrances
+CREATE TABLE houses_entrances_cameras
+(
+    camera_id INTEGER NOT NULL,
+    house_entrance_id INTEGER NOT NULL,
+    path TEXT,
+    distance INTEGER                                                                                                    -- "distance" (order) (virtual) from camera to domophone
+);
+CREATE UNIQUE INDEX houses_entrances_cameras_uniq on houses_entrances_cameras(camera_id, house_entrance_id);
+CREATE INDEX houses_entrances_cameras_camera_id on houses_entrances_cameras(camera_id);
+CREATE INDEX houses_entrances_cameras_house_entrance_id on houses_entrances_cameras(house_entrance_id);
