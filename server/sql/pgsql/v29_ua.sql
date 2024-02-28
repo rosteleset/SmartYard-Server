5078dfefd031a188bd1466a7c190b80abe115c7a
Mikhail Ivanov <mmikel@mail.ru>
2024-02-28 08:02:57 +0300

    WiP

--- server/sql/pgsql/v29_ua.sql
+++ server/sql/pgsql/v29_ua.sql
@@ -1,2 +1,2 @@
-ALTER TABLE houses_subscribers_devices ADD COLUMN ua CHARACTER VARYING;
+ALTER TABLE houses_subscribers_devices ADD IF NOT EXISTS ua CHARACTER VARYING;
 ALTER TABLE houses_subscribers_devices ADD IF NOT EXISTS ip TEXT;
