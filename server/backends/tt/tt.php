cb3a78154bc612349ffbe79051297043221e2122
Mikhail Ivanov <mmikel@mail.ru>
2024-03-05 05:20:16 +0300

    WiP

--- server/backends/tt/tt.php
+++ server/backends/tt/tt.php
@@ -2030,6 +2030,7 @@
              * @return mixed
              */
             public function printExec($id, $data) {
+                error_log("111");
                 $tmp = md5(time() . rand());
 
                 $print = $this->getPrint($id);
@@ -2039,7 +2040,9 @@
                     return false;
                 }
 
-//                foreach ($data as $id => &$value) {
+                error_log("222");
+
+                //                foreach ($data as $id => &$value) {
 //                    $data[$i] = (string)$value;
 //                }
 
@@ -2048,12 +2051,16 @@
                 $user = @$this->config["document_builder"]["www_user"]?:"www-data";
                 $group = @$this->config["document_builder"]["www_group"]?:"www-data";
 
+                error_log("333");
+
                 if (!is_dir($path)) {
                     mkdir($path);
                     chmod($path, 0775);
                     @chown($path, $user);
                     @chgrp($path, $group);
                 }
+
+                error_log("444");
 
                 $template = @$this->printGetTemplate($id);
                 $formatter = @$this->printGetFormatter($id);
@@ -2063,6 +2070,8 @@
                     return false;
                 }
 
+                error_log("555");
+
                 if ($template) {
                     $templateExt = explode(".", $template["name"]);
                     $templateExt = $templateExt[count($templateExt) - 1];
@@ -2070,6 +2079,8 @@
                     file_put_contents($templateName, $template["body"]);
                     $formatter = str_replace('${templateName}', $templateName, $formatter);
                 }
+
+                error_log("666");
 
                 $outFile = "$path/$tmp-out.{$print["extension"]}";
 
@@ -2080,6 +2091,8 @@
                 $formatter = "var data = " . json_encode($data). ";\n\n" . $formatter;
 
                 file_put_contents($path . "/" . $tmp . "-bld.docbuilder", $formatter);
+
+                error_log("777");
 
                 $log = [];
                 exec("$bin $path/$tmp-bld.docbuilder 2>&1", $log);
