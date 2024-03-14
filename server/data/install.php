fc15e2b74412d67815f3183e3447c3c81ff750be
Mikhail Ivanov <mmikel@mail.ru>
2024-03-14 10:34:00 +0300

    WiP

--- server/data/install.php
+++ server/data/install.php
@@ -9,7 +9,7 @@
     function initDB($_skip) {
         global $config, $db, $version;
 
-        $install = json_decode(file_get_contents("data/install.json"), true);
+        $install = @json_decode(file_get_contents("data/install.json"), true);
 
         $driver = explode(":", $config["db"]["dsn"])[0];
 
@@ -23,51 +23,53 @@
 
         $db->exec("BEGIN TRANSACTION");
 
-        foreach ($install as $v => $steps) {
-            $v = (int)$v;
-
-            $_v = sprintf("%06d", $v);
-
-            if ($version >= $v) {
-                echo "skipping DB version $_v\n";
-                continue;
-            }
-
-            if (@$skip[$v]) {
-                echo "force skipping DB version $_v\n";
-                continue;
-            }
-
-            echo "upgrading to DB version $_v\n";
-
-            try {
-                foreach ($steps as $step) {
-                    echo "\n================= $step\n\n";
-                    $path = pathinfo($step);
-                    if ($path['extension'] == "sql") {
-                        $sql = trim(file_get_contents("data/$driver/$step"));
-                        echo "$sql\n";
-                        $db->exec($sql);
-                    }
-                    if ($path['extension'] == "php") {
-                        require_once "data/$driver/$step";
-                        if ($path['filename']($db) !== true) {
-                            throw new \Exception("error calling function {$path['filename']}");
+        if (count($install) > 0) {
+            foreach ($install as $v => $steps) {
+                $v = (int)$v;
+    
+                $_v = sprintf("%06d", $v);
+    
+                if ($version >= $v) {
+                    echo "skipping DB version $_v\n";
+                    continue;
+                }
+    
+                if (@$skip[$v]) {
+                    echo "force skipping DB version $_v\n";
+                    continue;
+                }
+    
+                echo "upgrading to DB version $_v\n";
+    
+                try {
+                    foreach ($steps as $step) {
+                        echo "\n================= $step\n\n";
+                        $path = pathinfo($step);
+                        if ($path['extension'] == "sql") {
+                            $sql = trim(file_get_contents("data/$driver/$step"));
+                            echo "$sql\n";
+                            $db->exec($sql);
+                        }
+                        if ($path['extension'] == "php") {
+                            require_once "data/$driver/$step";
+                            if ($path['filename']($db) !== true) {
+                                throw new \Exception("error calling function {$path['filename']}");
+                            }
                         }
                     }
+                } catch (Exception $e) {
+                    $db->exec("ROLLBACK");
+                    print_r($e);
+                    echo "\n================= fail\n\n";
+                    exit(1);
                 }
-            } catch (Exception $e) {
-                $db->exec("ROLLBACK");
-                print_r($e);
-                echo "\n================= fail\n\n";
-                exit(1);
+    
+                $sth = $db->prepare("update core_vars set var_value = :version where var_name = 'dbVersion'");
+                $sth->bindParam('version', $v);
+                $sth->execute();
+    
+                echo "\n================= done\n\n";
             }
-
-            $sth = $db->prepare("update core_vars set var_value = :version where var_name = 'dbVersion'");
-            $sth->bindParam('version', $v);
-            $sth->execute();
-
-            echo "\n================= done\n\n";
         }
 
         $db->exec("COMMIT");
