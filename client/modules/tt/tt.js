db0ce3f86a929ec1c4c9430cf90b14fd3146bbe6
Mikhail Ivanov <mmikel@mail.ru>
2024-03-26 08:06:57 +0300

    WiP

--- client/modules/tt/tt.js
+++ client/modules/tt/tt.js
@@ -1346,10 +1346,13 @@
                 f[tree[tree.length - 1]] = project.filters[i];
             }
 
-            let filter_name = (modules.tt.meta.filters[x]?modules.tt.meta.filters[x]:i18n("tt.filter")).replaceAll("/", "<i class='fas fa-fw fa-xs fa-angle-double-right'></i>");
+            let filter_name =(modules.tt.meta.filters[x]?modules.tt.meta.filters[x]:i18n("tt.filter");
+            document.title = i18n("windowTitle") + " :: " + filter_name;
+
+            filter_name = filter_name.replaceAll("/", "<i class='fas fa-fw fa-xs fa-angle-double-right'></i>");
+
             filters += `<span class="pointer dropdown-toggle dropdown-toggle-no-icon text-primary text-bold" id="ttFilter" data-toggle="dropdown" data-boundary="window" aria-haspopup="true" aria-expanded="false" style="margin-left: -4px;"><i class="far fa-fw fa-caret-square-down mr-1"></i>${filter_name}</span>`;
             filters += `<ul class="dropdown-menu" aria-labelledby="ttFilter">`;
-            document.title = i18n("windowTitle") + " :: " + (modules.tt.meta.filters[x]?modules.tt.meta.filters[x]:i18n("tt.filter"));
 
             (function hh(t) {
                 let fMy = [];
@@ -1407,7 +1410,7 @@
                         fcount++;
                     } else {
                         filters += `<li class="dropdown-item pointer submenu" style="width: 350px;"><i class="far fa-fw fa-folder mr-2"></i><span>${i}</span></li>`;
-                        filters += '<ul class="dropdown-menu" style="width: 100%;">';
+                        filters += '<ul class="dropdown-menu">';
                         hh(t[i]);
                         filters += '</ul>';
                         filters += `</li>`;
