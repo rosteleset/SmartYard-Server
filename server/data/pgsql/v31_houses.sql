fd36a307e80cc44db51c05a999f15d9384b103f0
Mikhail Ivanov <mmikel@mail.ru>
2024-03-27 07:48:04 +0300

    WiP

---
+++ server/data/pgsql/v31_houses.sql
@@ -0,0 +1,1 @@
+ALTER TABLE houses_flats_subscribers ADD IF NOT EXISTS voip_enabled integer default 1;
---
+++ server/data/sqlite/v31_houses.sql
@@ -0,0 +1,1 @@
+ALTER TABLE houses_flats_subscribers ADD COLUMN voip_enabled integer default 1;
