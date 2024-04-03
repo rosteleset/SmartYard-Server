98c7502e4293f16401989ec6fb49f3a3325932c1
Mikhail Ivanov <mmikel@mail.ru>
2024-04-03 07:47:06 +0300

    WiP

--- client/modules/cdr/cdr.js
+++ client/modules/cdr/cdr.js
@@ -1,3 +1,5 @@
+// "fake" module, required for adding some i18n translations and API defenitions
+// for real CDR backend it must be realized at custom server backend extension
 ({
     init: function () {
         moduleLoaded("cdr", this);
