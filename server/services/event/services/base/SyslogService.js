e1b97245240078da5a45d01f92c44f51c0581ee6
dfedotov <dfedotov@lanta-net.ru>
2024-05-30 14:17:20 +0300

    Event service: JSDoc update

--- server/services/event/services/base/SyslogService.js
+++ server/services/event/services/base/SyslogService.js
@@ -11,6 +11,13 @@ const mode = process.env.NODE_ENV ?? 'normal';
  * @abstract
  */
 class SyslogService {
+    /**
+     * Constructs a new SyslogService instance.
+     * @param {string} unit - The unit identifier for the syslog service.
+     * @param {Object} config - The configuration object for the syslog server.
+     * @param {string[]} [spamWords=[]] - An array of words to filter out as spam.
+     * @throws {Error} Throws an error if instantiated directly.
+     */
     constructor(unit, config, spamWords = []) {
         if (this.constructor === SyslogService) {
             throw new Error('Abstract class SyslogService cannot be instantiated');
@@ -42,6 +49,33 @@ class SyslogService {
         throw new Error('Method "handleSyslogMessage()" must be implemented');
     }
 
+    /**
+     * Creates and configures a syslog server.
+     *
+     * This method initializes a new instance of the `SyslogServer`, sets up event handlers
+     * for incoming syslog messages and errors, and starts the server on a specified port.
+     *
+     * The server processes incoming syslog messages by:
+     * - Filtering out spam messages if not in debug mode.
+     * - Parsing the raw syslog message content.
+     * - Adjusting the host based on NAT settings if applicable.
+     * - Logging the parsed message to the console.
+     * - Sending the log to an external API.
+     * - Invoke an abstract method to further process the message depending on the unit.
+     *
+     * @example
+     * // Example subclass
+     * class MySyslogHandler extends SyslogService {
+     *     async handleSyslogMessage(date, host, message) {
+     *         // Implement custom handling logic here
+     *         console.log(`Handled message: ${message}`);
+     *     }
+     * }
+     *
+     * // Example usage
+     * const syslogHandler = new MySyslogHandler('unit1', config, ['spamWord1', 'spamWord2']);
+     * syslogHandler.createSyslogServer();
+     */
     createSyslogServer() {
         const syslog = new SyslogServer();
 
