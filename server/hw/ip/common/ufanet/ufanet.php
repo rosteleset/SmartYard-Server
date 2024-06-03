4c6aa26224fd9b4badf93eede67e32a00a00a94e
dfedotov <dfedotov@lanta-net.ru>
2024-06-03 18:31:46 +0300

    Add some methods to trait

--- server/hw/ip/common/ufanet/ufanet.php
+++ server/hw/ip/common/ufanet/ufanet.php
@@ -20,28 +20,81 @@ trait ufanet
 
     public function getSysinfo(): array
     {
-        // TODO: Implement getSysinfo() method.
+        $serialNumber = $this->apiCall('/cgi-bin/magicBox.cgi', ['action' => 'getSerialNo']);
+
+        if ($serialNumber !== null) {
+            return ['DeviceID' => $serialNumber];
+        }
+
         return [];
     }
 
     public function reboot()
     {
-        // TODO: Implement reboot() method.
+        $this->apiCall('/cgi-bin/magicBox.cgi', ['action' => 'reboot']);
     }
 
     public function reset()
     {
-        // TODO: Implement reset() method.
+        $this->apiCall('/cgi-bin/magicBox.cgi', ['action' => 'resetSystemEx']);
     }
 
     public function setAdminPassword(string $password)
     {
-        // TODO: Implement setAdminPassword() method.
+        $this->apiCall('/cgi-bin/userManager.cgi', [
+            'action' => 'modifyPassword',
+            'name' => $this->login,
+            'pwd' => $password,
+            'pwdOld' => $this->defaultPassword,
+        ]);
+
+        sleep(5);
     }
 
     public function syncData()
     {
         // TODO: Implement syncData() method.
+    }
+
+    /**
+     * Make an API call.
+     *
+     * @param string $resource API endpoint.
+     * @param array $payload (Optional) Query params or request body. Empty array by default.
+     * @param string $method (Optional) HTTP method. Default is "GET".
+     *
+     * @return array|string|null API response or null if an error occurred.
+     */
+    protected function apiCall(string $resource, array $payload = [], string $method = 'GET')
+    {
+        if (!empty($payload) && $method === 'GET') {
+            $queryString = http_build_query($payload);
+            $resource .= '?' . $queryString;
+        }
+
+        $req = $this->url . $resource;
+        $ch = curl_init($req);
+
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_USERPWD, "$this->login:$this->password");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, false);
+
+        if (!empty($payload) && $method !== 'GET') {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload, JSON_UNESCAPED_UNICODE));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, 'Content-Type: application/json');
+        }
+
+        $res = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpCode >= 400) {
+            return null;
+        }
+
+        return json_decode($res, true) ?? trim($res);
     }
 
     protected function getEventServer(): string
@@ -58,6 +111,7 @@ trait ufanet
 
     protected function initializeProperties()
     {
-        // TODO: Implement initializeProperties() method.
+        $this->login = 'admin';
+        $this->defaultPassword = '123456';
     }
 }
