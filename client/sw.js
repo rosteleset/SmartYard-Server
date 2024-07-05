445c90955c459286f79ed1f41d77fc11bfbd22e5
Mikhail Ivanov <mmikel@mail.ru>
2024-07-05 12:52:59 +0300

    WiP

--- client/sw.js
+++ client/sw.js
@@ -1,15 +1,54 @@
 var jQuery = {};
 var version = Math.random();
 
-importScripts('js/phpjs.js');
-importScripts('js/idbkvstore.min.js');
-importScripts('js/lstore.js');
-importScripts('js/cookie.js');
+function deparam(query) {
+    let setValue = function (root, path, value) {
+        if (path.length > 1) {
+            let  dir = path.shift();
+            if (typeof root[dir] == 'undefined') {
+                root[dir] = path[0] == '' ? [] : {};
+            }
+            arguments.callee(root[dir], path, value);
+        } else {
+            if (root instanceof Array) {
+                root.push(value);
+            } else {
+                root[path] = value;
+            }
+        }
+    };
 
-lStore(() => {
-    version = lStore("_version");
-    console.log("swVersion: " + version);
-});
+    let nvp = query.split('&');
+    let data = {};
+
+    for (let i = 0 ; i < nvp.length ; i++) {
+        let pair = nvp[i].split('=');
+        let name = decodeURIComponent(pair[0]);
+        let value = decodeURIComponent(pair[1]);
+
+        let path = name.match(/(^[^\[]+)(\[.*\]$)?/);
+        let first = path[1];
+        if (path[2]) {
+            path = path[2].match(/(?=\[(.*)\]$)/)[1].split('][')
+        } else {
+            path = [];
+        }
+        path.unshift(first);
+        setValue(data, path, value);
+    }
+
+    return data;
+}
+
+if (location.search) {
+    if (location.search[0] == "?") {
+        version = deparam(location.search.split('?')[1]).ver;
+    } else {
+        version = deparam(location.search).ver;
+    }
+}
+
+console.log("swVersion: " + version);
 
 const forceVersioning = [
     "app.js",
