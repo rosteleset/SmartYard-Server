1b47a3262d960e60497191214e80afecc2a765c7
Mikhail Ivanov <mmikel@mail.ru>
2024-08-04 07:20:46 +0300

    WiP

--- server/data/pgsql/v45_search.sql
+++ server/data/pgsql/v45_search.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 ALTER TABLE houses_subscribers_mobile ADD IF NOT EXISTS subscriber_full CHARACTER VARYING;
 UPDATE houses_subscribers_mobile set subscriber_full = TRIM(COALESCE(subscriber_last || ' ', '') || COALESCE(subscriber_name || ' ', '') || COALESCE(subscriber_patronymic));
-CREATE INDEX addresses_houses_house_full_trgm ON addresses_houses USING gist (house_full gist_trgm_ops);
-CREATE INDEX houses_subscribers_mobile_subscriber_full_trgm ON houses_subscribers_mobile USING gist (subscriber_full gist_trgm_ops);
+CREATE INDEX IF NOT EXISTS addresses_houses_house_full_trgm ON addresses_houses USING gist (house_full gist_trgm_ops);
+CREATE INDEX IF NOT EXISTS houses_subscribers_mobile_subscriber_full_trgm ON houses_subscribers_mobile USING gist (subscriber_full gist_trgm_ops);
