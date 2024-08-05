42209bae110df0b4bab183b1e36d89bec712bb9e
dfedotov <dfedotov@lanta-net.ru>
2024-08-05 18:04:19 +0300

    Fix v4_inbox.sql syntax error

--- server/data/clickhouse/v4_inbox.sql
+++ server/data/clickhouse/v4_inbox.sql
@@ -1,10 +1,6 @@
 ALTER TABLE inbox
-    (ADD COLUMN IF NOT EXISTS `msg_id` integer FIRST );
-ALTER TABLE inbox
-    (ADD COLUMN IF NOT EXISTS `house_subscriber_id` integer AFTER msg_id);
-ALTER TABLE inbox
-    (ADD COLUMN IF NOT EXISTS `title` String AFTER id);
-ALTER TABLE inbox
-    (ADD COLUMN IF NOT EXISTS `code` String AFTER action);
-ALTER TABLE inbox
-    (MODIFY COLUMN `id` String AFTER house_subscriber_id);
+    ADD COLUMN IF NOT EXISTS `msg_id` integer FIRST,
+    ADD COLUMN IF NOT EXISTS `house_subscriber_id` integer AFTER msg_id,
+    ADD COLUMN IF NOT EXISTS `title` String AFTER id,
+    ADD COLUMN IF NOT EXISTS `code` String AFTER action,
+    MODIFY COLUMN `id` String AFTER house_subscriber_id;
