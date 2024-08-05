2d69c160424082b6408bb21cc34814f3259e7a73
Mikhail Ivanov <mmikel@mail.ru>
2024-08-05 16:58:28 +0300

    WiP

--- server/data/sqlite/v45_search.sql
+++ server/data/sqlite/v45_search.sql
@@ -1,3 +1,4 @@
 ALTER TABLE houses_subscribers_mobile ADD COLUMN subscriber_full TEXT;
 UPDATE houses_subscribers_mobile set subscriber_full = TRIM(CONCAT(COALESCE(CONCAT(subscriber_last, ' '), ''), COALESCE(CONCAT(subscriber_name, ' '), ''), COALESCE(subscriber_last, '')));
-CREATE INDEX addresses_houses_house_full ON addresses_houses USING (house_full);
+CREATE INDEX addresses_houses_house_full ON addresses_houses(house_full);
+CREATE INDEX houses_subscribers_mobile_subscriber_full ON houses_subscribers_mobile(subscriber_full);
