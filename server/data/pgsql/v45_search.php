798111e00800bcd59db0d7c9a9287b80e74f8347
Mikhail Ivanov <mmikel@mail.ru>
2024-08-05 10:25:00 +0300

    WiP

--- server/data/pgsql/v45_search.php
+++ server/data/pgsql/v45_search.php
@@ -7,8 +7,8 @@ function v45_search($db)
     try {
         $text_search_config = $config["db"]["text_search_config"] ?? "simple";
         $db->modify("DROP INDEX IF EXISTS addresses_houses_house_full_gin");
-        $db->modify("CREATE INDEX IF NOT EXISTS addresses_houses_house_full_gin ON addresses_houses USING GIN('$text_search_config', to_tsvector(house_full))");
-        echo "CREATE INDEX IF NOT EXISTS addresses_houses_house_full_gin ON addresses_houses USING GIN('$text_search_config', to_tsvector(house_full))\n";
+        $db->modify("CREATE INDEX IF NOT EXISTS addresses_houses_house_full_gin ON addresses_houses USING GIN(to_tsvector('$text_search_config', house_full))");
+        echo "CREATE INDEX IF NOT EXISTS addresses_houses_house_full_gin ON addresses_houses USING GIN(to_tsvector('$text_search_config', house_full))\n";
         return true;
     } catch (\PDOException $e) {
         echo $e->getMessage() . "\n";
