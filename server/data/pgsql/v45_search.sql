4427fbd5e78f5fabff5c4dde4c5785ccfed0bc47
Mikhail Ivanov <mmikel@mail.ru>
2024-08-05 16:40:08 +0300

    WiP

--- server/data/pgsql/v45_search.sql
+++ server/data/pgsql/v45_search.sql
@@ -1,3 +1,5 @@
+DROP INDEX IF EXISTS addresses_houses_house_full_fts;
+DROP INDEX IF EXISTS houses_subscribers_mobile_subscriber_full_fts;
 ALTER TABLE houses_subscribers_mobile ADD IF NOT EXISTS subscriber_full CHARACTER VARYING;
 UPDATE houses_subscribers_mobile set subscriber_full = TRIM(COALESCE(subscriber_last || ' ', '') || COALESCE(subscriber_name || ' ', '') || COALESCE(subscriber_patronymic));
 CREATE INDEX IF NOT EXISTS addresses_houses_house_full ON addresses_houses(house_full);
