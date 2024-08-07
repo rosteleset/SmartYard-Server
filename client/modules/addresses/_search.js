b0811e54cd79ae819570755547642e9add1dfcfc
Mikhail Ivanov <mmikel@mail.ru>
2024-08-07 13:12:07 +0300

    WiP

--- client/modules/addresses/_search.js
+++ client/modules/addresses/_search.js
@@ -11,6 +11,52 @@
         })
     },
 
+/*
+<div class="list-group">
+    <div class="list-group-item">
+        <div class="row">
+            <div class="col px-4">
+                <div>
+                    <div class="float-right">2021-04-20 04:04pm</div>
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                    <p class="mb-0">consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="list-group-item">
+        <div class="row">
+            <div class="col-auto">
+                <img class="img-fluid" src="../../dist/img/photo1.png" alt="Photo" style="max-height: 160px;">
+            </div>
+            <div class="col px-4">
+                <div>
+                    <div class="float-right">2021-04-20 10:14pm</div>
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                    <p class="mb-0">consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="list-group-item">
+        <div class="row">
+            <div class="col-auto">
+                <iframe width="240" height="160" src="https://www.youtube.com/embed/WEkSYw3o5is?controls=0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="border-0" allowfullscreen=""></iframe>
+            </div>
+            <div class="col px-4">
+                <div>
+                    <div class="float-right">2021-04-20 11:54pm</div>
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                    <p class="mb-0">consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+*/
+
     renderSearch: function (search) {
         $("#searchInput").val(search);
 
