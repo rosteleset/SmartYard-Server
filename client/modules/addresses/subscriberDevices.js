7e236be5237fbb4c0220edc68c379af2d30a1fe8
Mikhail Ivanov <mmikel@mail.ru>
2024-08-16 08:42:02 +0300

    WiP

--- client/modules/addresses/subscriberDevices.js
+++ client/modules/addresses/subscriberDevices.js
@@ -290,21 +290,27 @@
 
         modules.addresses.topMenu();
 
-        QUERY("addresses", "addresses", {
-            houseId: params.houseId,
-        }).
-        done(modules.addresses.addresses).
-        fail(FAIL).
-        done(a => {
-            for (let i in a.addresses.houses) {
-                if (a.addresses.houses[i].houseId == params.houseId) {
-                    document.title = i18n("windowTitle") + " :: " + a.addresses.houses[i].houseFull + ", " + params.flat;
-                    subTop(modules.addresses.path((parseInt(params.settlementId) ? "settlement" : "street"), parseInt(params.settlementId) ? params.settlementId : params.streetId, true) + "<i class=\"fas fa-xs fa-angle-double-right ml-2 mr-2\"></i>" + `<a href="?#addresses.houses&houseId=${params.houseId}">${a.addresses.houses[i].houseFull}</a>` + '<i class="fas fa-xs fa-angle-double-right ml-2 mr-2"></i>' + `<a href='?#addresses.subscribers&flatId=${params.flatId}&houseId=${params.houseId}&flat=${params.flat}&settlementId=${params.settlementId}&streetId=${params.streetId}'>` +  params.flat + '</a><i class="fas fa-xs fa-angle-double-right ml-2 mr-2"></i>' + params.phone);
-                    break;
+        if (params.houseId) {
+            QUERY("addresses", "addresses", {
+                houseId: params.houseId,
+            }).
+            done(modules.addresses.addresses).
+            fail(FAIL).
+            done(a => {
+                for (let i in a.addresses.houses) {
+                    if (a.addresses.houses[i].houseId == params.houseId) {
+                        document.title = i18n("windowTitle") + " :: " + a.addresses.houses[i].houseFull + ", " + params.flat;
+                        subTop(modules.addresses.path((parseInt(params.settlementId) ? "settlement" : "street"), parseInt(params.settlementId) ? params.settlementId : params.streetId, true) + "<i class=\"fas fa-xs fa-angle-double-right ml-2 mr-2\"></i>" + `<a href="?#addresses.houses&houseId=${params.houseId}">${a.addresses.houses[i].houseFull}</a>` + '<i class="fas fa-xs fa-angle-double-right ml-2 mr-2"></i>' + `<a href='?#addresses.subscribers&flatId=${params.flatId}&houseId=${params.houseId}&flat=${params.flat}&settlementId=${params.settlementId}&streetId=${params.streetId}'>` +  params.flat + '</a><i class="fas fa-xs fa-angle-double-right ml-2 mr-2"></i>' + params.phone);
+                        break;
+                    }
                 }
-            }
+
+                modules.addresses.subscriberDevices.renderSubscriberDevices(params.subscriberId);
+            });
+        } else {
+            subTop(params.phone);
 
             modules.addresses.subscriberDevices.renderSubscriberDevices(params.subscriberId);
-        });
+        }
     }
 }).init();
