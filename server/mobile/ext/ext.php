be78546786eb455545b392f4f2fdce6570a118c8
Mikhail Ivanov <mmikel@mail.ru>
2024-08-21 08:08:56 +0300

    WiP

--- server/mobile/ext/ext.php
+++ server/mobile/ext/ext.php
@@ -1,24 +1,24 @@
 <?php
 
-/**
- * @api {post} /ext/ext расширение
- * @apiVersion 1.0.0
- * @apiDescription **[нет верстки]**
- *
- * @apiGroup Ext
- *
- * @apiHeader {String} authorization токен авторизации
- *
- * @apiParam {String} extId идентификатор расширения
- * @apiParam {Object[]} params параметры передаваемые в расширение
- * @apiParam {String} params.id идентификатор
- * @apiParam {String} params.value значение
- *
- * @apiSuccess {Object} - страничка которую надо отобразить во вьюшке
- * @apiSuccess {String} -.basePath базовый путь (от которго должна была загрузиться страница)
- * @apiSuccess {String} -.code html страница
- * @apiSuccess {Number=1,2} [-.version=1] версия реализации web-расширений (1 - по умолчанию)
- */
+    /**
+     * @api {post} /ext/ext расширение
+     * @apiVersion 1.0.0
+     * @apiDescription **[нет верстки]**
+     *
+     * @apiGroup Ext
+     *
+     * @apiHeader {String} authorization токен авторизации
+     *
+     * @apiParam {String} extId идентификатор расширения
+     * @apiParam {Object[]} params параметры передаваемые в расширение
+     * @apiParam {String} params.id идентификатор
+     * @apiParam {String} params.value значение
+     *
+     * @apiSuccess {Object} - страничка которую надо отобразить во вьюшке
+     * @apiSuccess {String} -.basePath базовый путь (от которго должна была загрузиться страница)
+     * @apiSuccess {String} -.code html страница
+     * @apiSuccess {Number=1,2} [-.version=1] версия реализации web-расширений (1 - по умолчанию)
+     */
 
     auth();
 
--- server/mobile/ext/list.php
+++ server/mobile/ext/list.php
@@ -1,21 +1,21 @@
 <?php
 
-/**
- * @api {post} /ext/list список глобальных расширений (меню)
- * @apiVersion 1.0.0
- * @apiDescription **[нет верстки]**
- *
- * @apiGroup Ext
- *
- * @apiHeader {String} authorization токен авторизации
- *
- * @apiSuccess {Object[]} - массив объектов
- * @apiSuccess {String} -.caption имя расширения (для отображения)
- * @apiSuccess {String} -.icon иконка расширения (svg)
- * @apiSuccess {String} -.extId идентификатор расширения
- * @apiSuccess {Number} -.order порядок следования (вес)
- * @apiSuccess {String="t","f"} [-.highlight="f"] "подсветка" (красная точка)
- */
+    /**
+     * @api {post} /ext/list список глобальных расширений (меню)
+     * @apiVersion 1.0.0
+     * @apiDescription **[нет верстки]**
+     *
+     * @apiGroup Ext
+     *
+     * @apiHeader {String} authorization токен авторизации
+     *
+     * @apiSuccess {Object[]} - массив объектов
+     * @apiSuccess {String} -.caption имя расширения (для отображения)
+     * @apiSuccess {String} -.icon иконка расширения (svg)
+     * @apiSuccess {String} -.extId идентификатор расширения
+     * @apiSuccess {Number} -.order порядок следования (вес)
+     * @apiSuccess {String="t","f"} [-.highlight="f"] "подсветка" (красная точка)
+     */
 
     auth();
     response();
