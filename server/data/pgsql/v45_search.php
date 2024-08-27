465470aa39dfb0cf391cf87957aee8813ed0a3cf
Mikhail Ivanov <mmikel@mail.ru>
2024-08-27 11:06:18 +0300

    WiP

--- server/data/pgsql/v45_search.php
+++ server/data/pgsql/v45_search.php
@@ -6,10 +6,12 @@
 
         try {
             $text_search_config = $config["db"]["text_search_config"] ?? "simple";
-            $db->modify("CREATE INDEX IF NOT EXISTS addresses_houses_house_full_fts ON addresses_houses USING GIN (to_tsvector('$text_search_config', house_full))");
-            $db->modify("CREATE INDEX IF NOT EXISTS houses_subscribers_mobile_subscriber_full_fts ON houses_subscribers_mobile USING GIN (to_tsvector('$text_search_config', subscriber_full))");
-            echo "CREATE INDEX IF NOT EXISTS addresses_houses_house_full_gin ON addresses_houses USING GIN (to_tsvector('$text_search_config', house_full))\n";
-            echo "CREATE INDEX IF NOT EXISTS houses_subscribers_mobile_subscriber_full_fts ON houses_subscribers_mobile USING GIN (to_tsvector('$text_search_config', subscriber_full))\n";
+            $i = "CREATE INDEX IF NOT EXISTS addresses_houses_house_full_fts ON addresses_houses USING GIN (to_tsvector('$text_search_config', house_full))";
+            $db->modify($i);
+            echo "$i\n";
+            $i = "CREATE INDEX IF NOT EXISTS houses_subscribers_mobile_subscriber_full_fts ON houses_subscribers_mobile USING GIN (to_tsvector('$text_search_config', subscriber_full))";
+            $db->modify($i);
+            echo "$i\n";
             return true;
         } catch (\PDOException $e) {
             echo $e->getMessage() . "\n";
