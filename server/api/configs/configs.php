d08909b3e8807c4a80f63c22bd36917f2eb4f0bc
Mikhail Ivanov <mmikel@mail.ru>
2024-08-31 09:46:46 +0300

    WiP

--- server/api/configs/configs.php
+++ server/api/configs/configs.php
@@ -1,6 +1,19 @@
 <?php
 
     /**
+     * @api {get} /api/configs/configs get configs
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName configs
+     * @apiGroup configs
+     *
+     * @apiHeader {String} token authentication token
+     *
+     * @apiSuccess {Object} configs
+     */
+
+    /**
      * configs api
      */
 
