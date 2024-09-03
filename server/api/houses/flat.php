0435119ba512c5396e9f85c195bfe1f5e3aaccc9
Mikhail Ivanov <mmikel@mail.ru>
2024-09-03 11:26:24 +0300

    WiP

--- server/api/houses/flat.php
+++ server/api/houses/flat.php
@@ -1,6 +1,77 @@
 <?php
 
     /**
+     * @api {post} /api/houses/flat add flat
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName addFlat
+     * @apiGroup houses
+     *
+     * @apiHeader {String} authorization authentication token
+     *
+     * @apiBody {Number} houseId
+     * @apiBody {Number} floor
+     * @apiBody {String} flat
+     * @apiBody {String} code
+     * @apiBody {Object[]} entrances
+     * @apiBody {Object[]} apartmentsAndLevels
+     * @apiBody {Boolean} manualBlock
+     * @apiBody {Boolean} adminBlock
+     * @apiBody {String} openCode
+     * @apiBody {Boolean} plog
+     * @apiBody {Number} autoOpen
+     * @apiBody {Number} whiteRabbit
+     * @apiBody {Boolean} sipEnabled
+     * @apiBody {String} sipPassword
+     *
+     * @apiSuccess {Number} flatId
+     */
+
+    /**
+     * @api {put} /api/houses/flat/:flatId modify flat
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName modifyFlat
+     * @apiGroup houses
+     *
+     * @apiHeader {String} authorization authentication token
+     *
+     * @apiParam {Number} flatId
+     * @apiBody {Number} [floor]
+     * @apiBody {String} [flat]
+     * @apiBody {String} [code]
+     * @apiBody {Object[]} [entrances]
+     * @apiBody {Object[]} [apartmentsAndLevels]
+     * @apiBody {Boolean} [manualBlock]
+     * @apiBody {Boolean} [adminBlock]
+     * @apiBody {String} [openCode]
+     * @apiBody {Boolean} [plog]
+     * @apiBody {Number} [autoOpen]
+     * @apiBody {Number} [whiteRabbit]
+     * @apiBody {Boolean} [sipEnabled]
+     * @apiBody {String} [sipPassword]
+     *
+     * @apiSuccess {Boolean} operationResult
+     */
+
+    /**
+     * @api {delete} /api/houses/flat/:flatId delete flat
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName deleteFlat
+     * @apiGroup houses
+     *
+     * @apiHeader {String} authorization authentication token
+     *
+     * @apiParam {Number} flatId
+     *
+     * @apiSuccess {Boolean} operationResult
+     */
+
+    /**
      * houses api
      */
 
