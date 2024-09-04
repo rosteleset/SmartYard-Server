ee922c9a9872afc7b6e988236df64d86f27133dc
Mikhail Ivanov <mmikel@mail.ru>
2024-09-04 11:19:47 +0300

    WiP

--- server/api/subscribers/subscribers.php
+++ server/api/subscribers/subscribers.php
@@ -1,48 +1,76 @@
 <?php
 
-/**
- * subscribers api
- */
-
-namespace api\subscribers
-{
-
-    use api\api;
-
     /**
-     * subscribers method
+     * @api {get} /subscribers/subscribers get subscribers by flat
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName subscribersByFlat
+     * @apiGroup subscribers
+     *
+     * @apiHeader {String} authorization authentication token
+     *
+     * @apiBody {String} by flatId
+     * @apiBody {Number} query
+     *
+     * @apiSuccess {Object[]} flat
      */
 
-    class subscribers extends api
-    {
+    /**
+     * @api {get} /subscribers/subscribers get subscribers by id
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName subscribersById
+     * @apiGroup subscribers
+     *
+     * @apiHeader {String} authorization authentication token
+     *
+     * @apiBody {String} by subscriberId
+     * @apiBody {Number} query
+     *
+     * @apiSuccess {Object[]} subscribers
+     */
 
-        public static function GET($params)
-        {
-            $households = loadBackend("households");
+    /**
+     * subscribers api
+     */
 
-            switch (@$params["by"]) {
-                case "flatId":
-                    $flat = [
-                        "subscribers" => $households->getSubscribers("flatId", @$params["query"]),
-                        "cameras" => $households->getCameras("flatId", @$params["query"]),
-                        "keys" => $households->getKeys("flatId", @$params["query"]),
-                    ];
+    namespace api\subscribers {
 
-                    return api::ANSWER($flat, $flat ? "flat" : false);
+        use api\api;
 
-                case "subscriberId":
-                    $subscribers = $households->getSubscribers("id", @$params["query"]);
-                    return api::ANSWER($subscribers, $subscribers ? "subscribers" : false);
+        /**
+         * subscribers method
+         */
+
+        class subscribers extends api {
+
+            public static function GET($params) {
+                $households = loadBackend("households");
+
+                switch (@$params["by"]) {
+                    case "flatId":
+                        $flat = [
+                            "subscribers" => $households->getSubscribers("flatId", @$params["query"]),
+                            "cameras" => $households->getCameras("flatId", @$params["query"]),
+                            "keys" => $households->getKeys("flatId", @$params["query"]),
+                        ];
+
+                        return api::ANSWER($flat, $flat ? "flat" : false);
+
+                    case "subscriberId":
+                        $subscribers = $households->getSubscribers("id", @$params["query"]);
+                        return api::ANSWER($subscribers, $subscribers ? "subscribers" : false);
+                }
+
+                return api::ERROR();
             }
 
-            return api::ERROR();
-        }
-
-        public static function index()
-        {
-            return [
-                "GET" => "#same(addresses,house,GET)",
-            ];
+            public static function index() {
+                return [
+                    "GET" => "#same(addresses,house,GET)",
+                ];
+            }
         }
     }
-}
