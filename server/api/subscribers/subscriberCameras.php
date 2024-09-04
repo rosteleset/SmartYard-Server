7352108ff6e9c096291510ccedfab4c3a999490b
Mikhail Ivanov <mmikel@mail.ru>
2024-09-04 11:22:35 +0300

    WiP

--- server/api/subscribers/subscriberCameras.php
+++ server/api/subscribers/subscriberCameras.php
@@ -1,41 +1,68 @@
 <?php
 
     /**
+     * @api {post} /subscribers/subscriberCameras add camera to subscriber
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName addSubscriberCamera
+     * @apiGroup subscribers
+     *
+     * @apiHeader {String} authorization authentication token
+     *
+     * @apiBody {Number} subscriberId
+     * @apiBody {Number} cameraId
+     *
+     * @apiSuccess {Number} cameraId
+     */
+
+    /**
+     * @api {delete} /subscribers/subscriberCameras add camera to subscriber
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiName deleteSubscriberCamera
+     * @apiGroup subscribers
+     *
+     * @apiHeader {String} authorization authentication token
+     *
+     * @apiBody {Number} subscriberId
+     * @apiBody {Number} cameraId
+     *
+     * @apiSuccess {Boolean} operationResult
+     */
+
+    /**
      * subscribers api
      */
 
-    namespace api\subscribers
-    {
+    namespace api\subscriber {
 
         use api\api;
 
         /**
-         * suvscriberCameras method
+         * subscriberCameras method
          */
 
-        class subscriberCameras extends api
-        {
+        class subscriberCameras extends api {
 
-            public static function POST($params)
-            {
+            public static function POST($params) {
                 $households = loadBackend("households");
 
                 $cameraId = $households->addCamera("subscriber", $params["subscriberId"], $params["cameraId"]);
 
-                return api::ANSWER($cameraId, ($cameraId !== false)?"cameraId":"notAcceptable");
+                return api::ANSWER($cameraId, ($cameraId !== false) ? "cameraId" : "notAcceptable");
             }
 
-            public static function DELETE($params)
-            {
+            public static function DELETE($params) {
                 $households = loadBackend("households");
 
                 $success = $households->unlinkCamera("subscriber", $params["subscriberId"], $params["cameraId"]);
 
-                return api::ANSWER($success, ($success !== false)?false:"notAcceptable");
+                return api::ANSWER($success, ($success !== false) ? false : "notAcceptable");
             }
 
-            public static function index()
-            {
+            public static function index() {
                 return [
                     "POST" => "#same(addresses,house,POST)",
                     "DELETE" => "#same(addresses,house,DELETE)",
