aded6a2ec2f5b62c62225eaa0307575f5d87b74e
Aleksander Vasilev <sbca68@gmail.com>
2024-10-01 11:30:03 +0300

    support for multi-level path for Flussonic

--- server/backends/dvr/internal/internal.php
+++ server/backends/dvr/internal/internal.php
@@ -88,23 +88,8 @@
 
                     $parsed_url = parse_url($cam['dvrStream']);
                     
-                    $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-                    $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-                    $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-                    $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-                    $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
-                    $pass     = ($user || $pass) ? "$pass@" : '';
-                    $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
                     $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
                     
-                    if ($path && $path[0] == '/') {
-                        $path = substr($path, 1);
-                    }
-                
-                    $path = explode("/", $path);
-                    
-                    $stream_name = $path[0];
-
                     $token = false;
                     if (isset($parsed_url['query'])) {
                         parse_str($parsed_url['query'], $parsed_query);
@@ -148,7 +133,10 @@
                 
                     $path = explode("/", $path);
                     
-                    $stream_name = $path[0];
+                    if ($path && (count($path) > 0) && str_contains('.m3u8', end($path))) {
+                        array_pop($path);
+                    }
+                    $stream_name = implode("/", $path);
 
                     $dvrStream = "$scheme$user$pass$host$port/$stream_name";
 
