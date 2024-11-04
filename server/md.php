b1b0ddbe9c468e63ee9b7ff633465e2d95b915db
Mikhail Ivanov <mmikel@mail.ru>
2024-11-04 07:35:31 +0300

    WiP

--- server/md.php
+++ server/md.php
@@ -1,1 +1,9 @@
 <?php
+
+    require __DIR__ . 'mzfc/mobiledetect/vendor/autoload.php';
+
+    use Detection\MobileDetect;
+    $detect = new MobileDetect();
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($detect->isMobile());
