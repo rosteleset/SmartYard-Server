594988d95c40616dff750f974ecb4a10dbcdaa89
Mikhail Ivanov <mmikel@mail.ru>
2024-11-07 17:34:13 +0300

    WiP

--- server/data/pgsql/v54_households.sql
+++ server/data/pgsql/v54_households.sql
@@ -1,3 +1,4 @@
 ALTER TABLE houses_flats_devices DROP CONSTRAINT IF EXISTS houses_flats_devices_pkey;
+ALTER TABLE houses_flats_devices DROP COLUMN IF EXISTS house_flat_device_id;
 ALTER TABLE houses_flats_devices ADD COLUMN IF NOT EXISTS houses_flat_device_id SERIAL NOT NULL;
 ALTER TABLE houses_flats_devices ADD CONSTRAINT houses_flats_devices_pkey PRIMARY KEY (houses_flat_device_id);
