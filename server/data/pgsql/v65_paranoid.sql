4c1632a009196947d9b548bb13278271b49dc665
Mikhail Ivanov <mmikel@mail.ru>
2025-03-07 13:13:37 +0300

    WiP

--- server/data/pgsql/v65_paranoid.sql
+++ server/data/pgsql/v65_paranoid.sql
@@ -1,2 +1,3 @@
 ALTER TABLE houses_flats_devices ADD IF NOT EXISTS paranoid INTEGER DEFAULT 0;
 CREATE INDEX IF NOT EXISTS houses_flats_devices_paranoid ON houses_flats_devices(paranoid);
+ALTER TABLE houses_rfids ADD IF NOT EXISTS watch INTEGER DEFAULT 0;
--- server/data/sqlite/v65_paranoid.sql
+++ server/data/sqlite/v65_paranoid.sql
@@ -1,2 +1,3 @@
 ALTER TABLE houses_flats_devices ADD COLUMN paranoid INTEGER DEFAULT 0;
 CREATE INDEX houses_flats_devices_paranoid ON houses_flats_devices(paranoid);
+ALTER TABLE houses_rfids ADD COLUMN watch INTEGER DEFAULT 0;
