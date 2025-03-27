19c094dc247e2ff25a4fdaa634d2c110926a742b
Mikhail Ivanov <mmikel@mail.ru>
2025-03-27 09:36:49 +0300

    WiP

--- server/data/pgsql/v66_custom_fields.sql
+++ server/data/pgsql/v66_custom_fields.sql
@@ -1,2 +1,9 @@
 ALTER TABLE custom_fields ADD IF NOT EXISTS magic_icon CHARACTER VARYING;
 ALTER TABLE custom_fields ADD IF NOT EXISTS magic_function CHARACTER VARYING;
+
+CREATE TABLE IF NOT EXISTS custom_fileds_values
+(
+    custom_fileds_value_id SERIAL PRIMARY KEY,
+    apply_to INTEGER,
+    value CHARACTER VARYING
+);
--- server/data/sqlite/v66_custom_fields.sql
+++ server/data/sqlite/v66_custom_fields.sql
@@ -1,2 +1,9 @@
 ALTER TABLE custom_fields ADD COLUMN magic_icon TEXT;
 ALTER TABLE custom_fields ADD COLUMN magic_function TEXT;
+
+CREATE TABLE custom_fileds_values
+(
+    custom_fileds_value_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    apply_to INTEGER,
+    value CHARACTER VARYING
+);
