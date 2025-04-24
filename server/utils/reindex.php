bb17a65fe589f5e2985916bc07fef6d33f4e5acd
Mikhail Ivanov <mmikel@mail.ru>
2025-04-24 11:13:09 +0300

    WiP

--- server/utils/reindex.php
+++ server/utils/reindex.php
@@ -101,5 +101,5 @@
 
         $db->exec("delete from core_api_methods as a1 where permissions_same is not null and permissions_same not in (select aid from core_api_methods as a2)");
 
-        echo "reindex done ()\n";
+        echo "reindex done ($n)\n";
     }
