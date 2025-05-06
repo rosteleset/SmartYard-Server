996f29b276f78849f6805b109c20563eac047646
Mikhail Ivanov <mmikel@mail.ru>
2025-05-06 15:36:49 +0300

    WiP

--- client/modules/tt/issue.js
+++ client/modules/tt/issue.js
@@ -789,7 +789,7 @@
             h += "<table>";
             for (let i in issue.issue.childrens.issues) {
                 h += "<tr>";
-                h += `<td class='text-bold hoverable ttIssue'>${issue.issue.childrens.issues[i].issueId}</td>`;
+                h += `<td><a class='text-bold hoverable ttIssue' href='/?#tt&issue=${issue.issue.childrens.issues[i].issueId}'>${issue.issue.childrens.issues[i].issueId}</a></td>`;
                 h += `<td class='pl-2'>${ttDate(issue.issue.childrens.issues[i].created, true)}</td>`;
                 h += `<td class='pl-2'>${modules.tt.issueField2Html(issue.issue.childrens.issues[i], "author", undefined, "left")}</td>`;
                 h += `<td class='pl-2'>${issue.issue.childrens.issues[i].subject}</td>`;
