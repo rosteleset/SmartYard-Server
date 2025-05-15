b82337bc4b2d6292f53b71b5ae8d819e0f597c38
Mikhail Ivanov <mmikel@mail.ru>
2025-05-15 08:20:42 +0300

    WiP

--- client/modules/tt/issue.js
+++ client/modules/tt/issue.js
@@ -1090,7 +1090,7 @@
                             }
                         }
                     } else {
-                        if (project.comments && comments.split("\n").length > 0 && trim(comments.split("\n")[0])) {
+                        if (project.comments && project.comments.split("\n").length > 0 && trim(project.comments.split("\n")[0])) {
                             h += `<tr><td style="width: 100%"><hr class='hr-text-pointer mt-1 mb-1 commentsMenu' data-content='&#x2630; ${i18n("tt.comments")}' style="font-size: 11pt;"/></td></tr>`;
                         } else {
                             h += `<tr><td style="width: 100%"><hr class='hr-text mt-1 mb-1' data-content='${i18n("tt.comments")}' style="font-size: 11pt;"/></td></tr>`;
