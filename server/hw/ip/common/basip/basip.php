a0ebfdb705435b950d95f01bea2d18e9f514f42c
dfedotov <dfedotov@lanta-net.ru>
2025-09-02 18:27:11 +0300

    Basip: add templates

---
+++ server/hw/ip/common/basip/basip.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace hw\ip\common\basip;
+
+/**
+ * Trait providing common functionality related to BAS-IP devices.
+ */
+trait basip
+{
+}
---
+++ server/hw/ip/domophone/basip/aa07fb.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace hw\ip\domophone\basip;
+
+/**
+ * Class representing an BAS-IP AA-07FB intercom.
+ */
+class aa07fb extends basip
+{
+}
