8d9be4c0b6a2f4679ccf7470c9cb8360914fb39f
Mikhail Ivanov <mmikel@mail.ru>
2025-09-03 11:29:03 +0300

    WiP

--- server/wh.php
+++ server/wh.php
@@ -36,81 +36,115 @@
     }
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'GET') {
-        $m = explode('/', trim($_SERVER["REQUEST_URI"], '/'));
-        if (count($m) >= 2 && $m[0] === 'wh') {
-            $method = $m[1];
+        $request = explode("?", $_SERVER["REQUEST_URI"])[0];
 
-            if ($m[2]){
-                $param = $m[2] ?? null;
+        $wh = parse_url(@$config["api"]["wh"]);
+
+        $path = "";
+
+        if ($wh && $wh['path'] && strpos($request, $wh["path"]) === 0) {
+            $path = substr($request, strlen($wh['path']));
+        }
+
+        if ($path && $path[0] == '/') {
+            $path = substr($path, 1);
+        }
+
+        if (!$path) {
+            response(403);
+        }
+
+        $m = explode('/', $path);
+
+        $method = @$m[0];
+
+        $params = [];
+
+        if (count($m) >= 2) {
+            $params["_id"] = urldecode($m[1]);
+        }
+
+        $params["_path"] = [
+            "method" => $method,
+        ];
+
+        $params["_request_method"] = @$_SERVER['REQUEST_METHOD'];
+        $params["_ua"] = @$_SERVER["HTTP_USER_AGENT"];
+
+        if (file_exists(__DIR__ . "/wh/{$method}.php")) {
+            // Redis connection
+            try {
+                $redis = new Redis();
+                $redis->connect($config["redis"]["host"], $config["redis"]["port"]);
+                if (@$config["redis"]["password"]) {
+                    $redis->auth($config["redis"]["password"]);
+                }
+                $redis->setex("iAmOk", 1, "1");
+            } catch (Exception $err) {
+                error_log(print_r($err, true));
+                response(555, false, false, "Can't connect to Redis");
+                exit(1);
             }
 
-            if (file_exists(__DIR__ . "/wh/{$method}.php")) {
-                // Redis connection
-                try {
-                    $redis = new Redis();
-                    $redis->connect($config["redis"]["host"], $config["redis"]["port"]);
-                    if (@$config["redis"]["password"]) {
-                        $redis->auth($config["redis"]["password"]);
-                    }
-                    $redis->setex("iAmOk", 1, "1");
-                } catch (Exception $err) {
-                    error_log(print_r($err, true));
-                    response(555, false, false, "Can't connect to Redis");
-                    exit(1);
-                }
+            // db connection
+            try {
+                $db = new PDO_EXT(@$config["db"]["dsn"], @$config["db"]["username"], @$config["db"]["password"], @$config["db"]["options"]);
+            } catch (Exception $err) {
+                error_log(print_r($err, true));
+                response(500, false, false, "Can't open database " . $config["db"]["dsn"]);
+                exit(1);
+            }
 
-                // db connection
-                try {
-                    $db = new PDO_EXT(@$config["db"]["dsn"], @$config["db"]["username"], @$config["db"]["password"], @$config["db"]["options"]);
-                } catch (Exception $err) {
-                    error_log(print_r($err, true));
-                    response(500, false, false, "Can't open database " . $config["db"]["dsn"]);
-                    exit(1);
-                }
+            if (@$config["db"]["schema"]) {
+                $db->exec("SET search_path TO " . $config["db"]["schema"] . ", public");
+            }
 
-                if (@$config["db"]["schema"]) {
-                    $db->exec("SET search_path TO " . $config["db"]["schema"] . ", public");
-                }
-
-                if (count($_GET)) {
-                    foreach ($_GET as $key => $value) {
-                        if (gettype($value) == "string") {
-                            $params[$key] = urldecode($value);
-                        } else {
-                            $params[$key] = $value;
-                        }
-                    }
-                }
-
-                if (count($_POST)) {
-                    foreach ($_POST as $key => $value) {
-                        if (gettype($value) == "string") {
-                            $params[$key] = urldecode($value);
-                        } else {
-                            $params[$key] = $value;
-                        }
-                    }
-                }
-
-                $_RAW = json_decode(file_get_contents("php://input"), true);
-
-                if ($_RAW && count($_RAW)) {
-                    foreach ($_RAW as $key => $value) {
+            if (count($_GET)) {
+                foreach ($_GET as $key => $value) {
+                    if (gettype($value) == "string") {
+                        $params[$key] = urldecode($value);
+                    } else {
                         $params[$key] = $value;
                     }
                 }
+            }
 
-                $backends = [];
-                foreach ($required_backends as $backend) {
-                    if (loadBackend($backend) === false) {
-                        error_log("noRequiredBackend");
-                        response(555, [
-                            "error" => "noRequiredBackend",
-                        ]);
+            if (count($_POST)) {
+                foreach ($_POST as $key => $value) {
+                    if (gettype($value) == "string") {
+                        $params[$key] = urldecode($value);
+                    } else {
+                        $params[$key] = $value;
                     }
                 }
+            }
 
-                require_once __DIR__ . "/wh/{$method}.php";
+            $_RAW = json_decode(file_get_contents("php://input"), true);
+
+            if ($_RAW && count($_RAW)) {
+                foreach ($_RAW as $key => $value) {
+                    $params[$key] = $value;
+                }
+            }
+
+            $backends = [];
+            foreach ($required_backends as $backend) {
+                if (loadBackend($backend) === false) {
+                    error_log("noRequiredBackend");
+                    response(555, [
+                        "error" => "noRequiredBackend",
+                    ]);
+                }
+            }
+
+            require_once __DIR__ . "/wh/{$method}.php";
+
+            if (function_exists($method)) {
+                if ($res = call_user_func($method, $params)) {
+                    response(200, $res);
+                } else {
+                    response(400);
+                }
             }
         }
     }
