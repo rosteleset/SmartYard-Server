a9986dff4eb9021f3ccb1d414db2e9ba62ef86aa
dfedotov <dfedotov@lanta-net.ru>
2025-10-23 19:56:17 +0300

    Ufanet: add full support for the "white rabbit" feature

--- server/services/event/services/UfanetService.js
+++ server/services/event/services/UfanetService.js
@@ -9,7 +9,7 @@ import { API, mdTimer } from '../utils/index.js';
 class UfanetService extends SyslogService {
     constructor(unit, config, spamWords = []) {
         super(unit, config, spamWords);
-        this.gateRabbits = {};
+        this.lastCall = {};
     }
 
     async handleSyslogMessage(date, host, message) {
@@ -19,44 +19,42 @@ class UfanetService extends SyslogService {
             await mdTimer({ ip: host });
         }
 
-        // Opening door by DTMF or CMS handset
-        if (false) {
-            // TODO: no info about opening with CMS handset, no info about DTMF SIP number
-        }
+        // Outgoing call
+        if (message.includes('CALL_OUTGOING')) {
+            const sipNumber = message.split('sip:')[1].split('@')[0];
 
-        // Call in gate mode with prefix: potential white rabbit
-        if (message.includes('STAT/CALLGATE')) {
-            const number = message.split(':')[1].trim();
-
-            (this.gateRabbits)[host] = {
-                ip: host,
-                prefix: parseInt(number.substring(0, 4)),
-                apartmentNumber: parseInt(number.substring(4)),
-            };
+            if (sipNumber.length === 10) { // Normal mode
+                this.lastCall[host] = {
+                    ip: host,
+                    apartmentId: parseInt(sipNumber.substring(1)),
+                };
+            } else if (sipNumber.length > 4 && sipNumber.length < 9) { // Prefix mode
+                this.lastCall[host] = {
+                    ip: host,
+                    prefix: parseInt(sipNumber.substring(0, 4)),
+                    apartmentNumber: parseInt(sipNumber.substring(4)),
+                };
+            }
         }
 
         // Incoming DTMF
-        if (message.includes('DTMF')) {
-            // Sending a rabbit gate update if the host is in the rabbit gate
-            if ((this.gateRabbits)[host]) {
-                const { ip, prefix, apartmentNumber } = this.gateRabbits[host];
-                await API.setRabbitGates({ date: date, ip, prefix, apartmentNumber });
-            }
+        if (message.includes('DTMF') && this.lastCall[host]) {
+            const { ip, prefix, apartmentNumber, apartmentId } = this.lastCall[host];
+            await API.setRabbitGates({ date: date, ip, prefix, apartmentNumber, apartmentId });
         }
 
-        // Opening a door by RFID key
+        // Opening a door by RFID key or personal code
         if (message.includes('keyhex=') && message.includes('pass=OK')) {
             if (message.includes('type=password')) {
+                // Personal code
                 const code = parseInt(message.split('key=')[1].split(' ')[0]);
                 await API.openDoor({ date: date, ip: host, door: 0, detail: code, by: 'code' });
-            }
-            else {
-                // Same message for internal and external readers
+            } else {
+                // RFID. Same message for internal and external readers
                 const hexRfid = message.split(' ')[1].split('=')[1].padStart(14, '0').toUpperCase();
                 await API.openDoor({ date: date, ip: host, door: 0, detail: hexRfid, by: 'rfid' });
             }
         }
-
 
         // All calls are done
         if (message.includes('pickup 0')) {
