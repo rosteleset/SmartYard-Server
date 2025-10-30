f0caeac73cd2766dd56171d424e38699588afd44
Mikhail Ivanov <mmikel@mail.ru>
2025-10-30 09:09:45 +0300

    WiP

--- client/modules/about/about.js
+++ client/modules/about/about.js
@@ -12,21 +12,19 @@
 
         $.get("modules/custom/version?_=" + Math.random()).
         always((x, y) => {
-            let custom;
-            let realCustom = "-";
+            let custom = "-";
             if (y == "success" && $.trim(x)) {
                 custom = $.trim(x);
-                realCustom = $.trim(x);
             }
             $.get("version.cli?_=" + Math.random()).
             done(cli => {
                 $.get("version.app?_=" + Math.random()).
                 done(app => {
-                    let versionActual = md5(app + "/" + cli + "/" + realCustom);
+                    let versionActual = md5(app + "/" + cli + "/" + custom);
                     GET("server", "version", false, true).
                     done(v => {
                         let h = '';
-                        h += i18n("about.text", cli ? cli : 'unknown', v.appVersion ? v.appVersion : 'unknown', v.dbVersion ? v.dbVersion : 'unknown', custom ? custom : i18n('no'));
+                        h += i18n("about.text", cli ? cli : 'unknown', v.appVersion ? v.appVersion : 'unknown', v.dbVersion ? v.dbVersion : 'unknown', (custom != "-" ) ? custom : i18n('no'));
                         if (version != versionActual) {
                             h += `<br /><button type="button" class="mt-3 btn btn-outline-secondary" onclick="window.location.reload(true);"><i class="fas fa-sync-alt mr-2"></i>${i18n("about.refresh")}</button>`;
                         }
