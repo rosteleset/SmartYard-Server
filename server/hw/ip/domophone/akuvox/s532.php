f5ee28a351fd32b66f28cf78b4b1483e4ba69c63
dfedotov <dfedotov@lanta-net.ru>
2025-11-19 18:44:53 +0300

    Akuvox S532: override `setAdminPassword()` method

--- server/hw/ip/domophone/akuvox/s532.php
+++ server/hw/ip/domophone/akuvox/s532.php
@@ -11,4 +11,34 @@ class s532 extends akuvox
     {
         return 4000; // TODO: check
     }
+
+    public function setAdminPassword(string $password): void
+    {
+        $this->setWebPassword($password);
+        $this->setRtspPassword($password);
+        $this->setApiPassword($password);
+    }
+
+    protected function setApiPassword(string $password): void
+    {
+        $this->setConfigParams(['Config.DoorSetting.APIFCGI.Password' => base64_encode($password)]);
+    }
+
+    protected function setRtspPassword(string $password): void
+    {
+        $this->setConfigParams(['Config.DoorSetting.RTSP.Password' => base64_encode($password)]);
+    }
+
+    protected function setWebPassword(string $password): void
+    {
+        $this->apiCall('', 'POST', [
+            'target' => 'security_basic',
+            'action' => 'set',
+            'data' => [
+                'firstLogin' => '1',
+                'userName' => base64_encode($this->login),
+                'newPassword' => base64_encode($password),
+            ],
+        ]);
+    }
 }
