f5a2fba04d9c9b0f68c5609354c1d7f723232d7b
Pavel D. Kuzin <root@consult-it.pro>
2025-12-16 01:49:45 +0300

    1

--- server/hw/ip/domophone/basip/basip.php
+++ server/hw/ip/domophone/basip/basip.php
@@ -634,6 +634,7 @@ abstract class basip extends domophone implements
     {
         $sipSettings = $this->apiCall('/api/v1/device/sip/settings');
 
+        echo json_encode($sipSettings);
         $realmParts = explode(':', $sipSettings['realm'], 2);
 
         return [
