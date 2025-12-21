425c7c1169e87e7af7649e3616b58178b19c668d
Mikhail Ivanov <mmikel@mail.ru>
2025-12-21 08:56:43 +0300

    WiP

--- server/data/pgsql/v89_devices_tree.sql
+++ server/data/pgsql/v89_devices_tree.sql
@@ -1,3 +1,14 @@
-CREATE TABLE core_devices_tree (leaf_id SERIAL PRIMARY KEY, tree CHARACTER VARYING, name CHARACTER VARYING);
-CREATE INDEX core_devices_tree_tree ON core_devices_tree(tree);
-CREATE INDEX core_devices_tree_name ON core_devices_tree(name);
+CREATE TABLE core_devices_tree
+(
+    leaf_id SERIAL PRIMARY KEY,
+    tree CHARACTER VARYING,
+    name CHARACTER VARYING
+);
+CREATE INDEX IF NOT EXISTS core_devices_tree_tree ON core_devices_tree(tree);
+CREATE INDEX IF NOT EXISTS core_devices_tree_name ON core_devices_tree(name);
+
+ALTER TABLE cameras ADD IF NOT EXISTS tree CHARACTER VARYING;
+CREATE INDEX IF NOT EXISTS cameras_tree ON cameras(tree);
+
+ALTER TABLE houses_domophones ADD IF NOT EXISTS tree CHARACTER VARYING;
+CREATE INDEX IF NOT EXISTS houses_domophones_tree ON houses_domophones(tree);
--- server/data/sqlite/v89_devices_tree.sql
+++ server/data/sqlite/v89_devices_tree.sql
@@ -1,3 +1,14 @@
-CREATE TABLE core_devices_tree (leaf_id INTEGER PRIMARY KEY AUTOINCREMENT, tree TEXT, name TEXT);
+CREATE TABLE core_devices_tree
+(
+    leaf_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tree TEXT,
+    name TEXT
+);
 CREATE INDEX core_devices_tree_tree ON core_devices_tree(tree);
 CREATE INDEX core_devices_tree_name ON core_devices_tree(name);
+
+ALTER TABLE cameras ADD COLUMN tree CHARACTER VARYING;
+CREATE INDEX cameras_tree ON cameras(tree);
+
+ALTER TABLE houses_domophones ADD COLUMN tree CHARACTER VARYING;
+CREATE INDEX houses_domophones_tree ON houses_domophones(tree);
