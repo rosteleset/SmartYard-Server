face3627f1401410150374f2d711b429a4300629
Pavel D. Kuzin <root@consult-it.pro>
2025-12-29 16:18:40 +0300

    fix aa07bd camera

--- server/hw/ip/camera/basip/aa07bd.php
+++ server/hw/ip/camera/basip/aa07bd.php
@@ -13,6 +13,12 @@ class aa07bd extends camera
         transformDbConfig as protected commonTransformDbConfig;
     }
 
+    /**
+     * @var string stored bearer token used for authentication
+     */
+    public string $token;
+
+
     public function configureMotionDetection(array $detectionZones): void
     {
         // Empty implementation
@@ -51,6 +57,67 @@ class aa07bd extends camera
 
     protected function initConnection(): void
     {
-        // Empty implementation
+        $loginResult = $this->login();
+        if ($loginResult && array_key_exists("token", $loginResult)) {
+            $this->token = $loginResult["token"];
+        } else {
+            throw new Exception("Login failed");
+        }
+    }
+
+    protected function login(): array|string
+    {
+        $req = $this->url . $this->apiPrefix . "/v1/login?username=" . $this->login . "&password=" . md5($this->password);
+
+        $ch = curl_init($req);
+
+        curl_setopt_array($ch, [
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_UNRESTRICTED_AUTH => true,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 5,
+        ]);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($res, true) ?? $res;
+    }
+
+    protected function apiCall(
+        string $resource,
+        string $method = 'GET',
+        array  $payload = [],
+        int    $timeout = 0,
+    ): array|string
+    {
+        $req = $this->url . $this->apiPrefix . $resource;
+
+        $ch = curl_init($req);
+
+        curl_setopt_array($ch, [
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_HTTPAUTH => CURLAUTH_BEARER,
+            CURLOPT_UNRESTRICTED_AUTH => true,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_XOAUTH2_BEARER => $this->token,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => $timeout,
+        ]);
+
+        if ($payload) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload, JSON_UNESCAPED_UNICODE));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        }
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($res, true) ?? $res;
     }
 }
