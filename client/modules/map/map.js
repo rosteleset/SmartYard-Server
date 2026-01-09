12d4de77075f4e5a495da367d213c9dc5ca15bef
Mikhail Ivanov <mmikel@mail.ru>
2026-01-09 05:38:10 +0300

    WiP

--- client/modules/map/map.js
+++ client/modules/map/map.js
@@ -78,7 +78,7 @@
         document.title = i18n("windowTitle") + " :: " + i18n("map.map");
 
         $("#altForm").hide();
-        $("#mainForm").html(`<div id='mapContainer' style='width: 100%; height: calc(100vh - ${height}px); border: solid thin #dee2e6; border-radius: .25rem; box-shadow: 0 0 1px rgba(0, 0, 0, .125), 0 1px 3px rgba(0, 0, 0, .2);' class='mt-2'></div>`);
+        $("#mainForm").html(`<div id='mapContainer' style='width: 100%; height: calc(100vh - ${height}px);' class='mt-2 border-with-shadow'></div>`);
 
         modules.map.map = L.map('mapContainer', { editable: true });
 
