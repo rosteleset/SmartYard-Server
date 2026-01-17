77b2d2b30105550fc16ba80b108a2d5bc2b6a66a
Mikhail Ivanov <mmikel@mail.ru>
2026-01-17 15:43:54 +0300

    WiP

---
+++ server/backends/billing/billing.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * backends billing namespace
+ */
+
+namespace backends\billing {
+
+    use backends\backend;
+
+    /**
+     * base billing class
+     */
+
+    abstract class billing extends backend {
+
+    }
+}
