8184e88f6ec783ecd56991f627d28aa35781802c
Mikhail Ivanov <mmikel@mail.ru>
2026-01-20 20:20:20 +0300

    WiP

--- client/patches/adminlte.js
+++ client/patches/adminlte.js
@@ -1761,11 +1761,11 @@
 
       if (offset !== false) {
         if (max === heights.controlSidebar) {
-          $contentSelector.css(this._config.panelAutoHeightMode, max + offset - 1);
+          $contentSelector.css(this._config.panelAutoHeightMode, max + offset - ((window.devicePixelRatio > 1) ? 1 : 0));
         } else if (max === heights.window) {
-          $contentSelector.css(this._config.panelAutoHeightMode, max + offset - heights.header - heights.footer - 1);
+          $contentSelector.css(this._config.panelAutoHeightMode, max + offset - heights.header - heights.footer - ((window.devicePixelRatio > 1) ? 1 : 0));
         } else {
-          $contentSelector.css(this._config.panelAutoHeightMode, max + offset - heights.header - 1);
+          $contentSelector.css(this._config.panelAutoHeightMode, max + offset - heights.header - ((window.devicePixelRatio > 1) ? 1 : 0));
         }
 
         if (this._isFooterFixed()) {
