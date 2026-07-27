c0759bef531037928b858dbd185cc94081ee7a30
subs1stem <subs1stem@yandex.ru>
2026-07-27 15:37:15 +0300

    feat(omny): configure SOS number on VDP-10L

--- server/hw/ip/domophone/omny/vdp10l.php
+++ server/hw/ip/domophone/omny/vdp10l.php
@@ -10,4 +10,11 @@ use hw\ip\domophone\akuvox\s532;
 class vdp10l extends s532
 {
     protected const SHOULD_UPLOAD_CUSTOM_ACCESS_GRANTED_SOUND = false;
+
+    public function setSosNumber(int $sipNumber): void
+    {
+        $this->setConfigParams([
+            'Config.Features.SPEEDDIAL.Num01' => (string)$sipNumber,
+        ]);
+    }
 }
