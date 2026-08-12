335e3e3d4806aefdde4fb837dcc9ea75b6780d30
subs1stem <subs1stem@yandex.ru>
2026-08-12 17:44:11 +0300

    feat(omny): inherit VDP-10M class from Akuvox base

--- server/hw/ip/domophone/omny/vdp10m.php
+++ server/hw/ip/domophone/omny/vdp10m.php
@@ -2,7 +2,41 @@
 
 namespace hw\ip\domophone\omny;
 
-class vdp10m
-{
+use hw\ip\domophone\akuvox\akuvox;
 
+class vdp10m extends akuvox
+{
+    protected static function getMaxUsers(): int
+    {
+        return 5000; // Found by Codex
+    }
+
+    public function configureApartment(
+        int   $apartment,
+        int   $code = 0,
+        array $sipNumbers = [],
+        bool  $cmsEnabled = true,
+        array $cmsLevels = [],
+    ): void
+    {
+        // TODO
+    }
+
+    public function deleteApartment(int $apartment = 0): void
+    {
+        // TODO
+    }
+
+    public function setConciergeNumber(int $sipNumber): void
+    {
+        $this->setConfigParams([
+            'Config.Programable.SOFTKEY01.LocalParam1' => $sipNumber . str_repeat(';', 7),
+        ]);
+    }
+
+    protected function getApartments(): array
+    {
+        // TODO
+        return [];
+    }
 }
