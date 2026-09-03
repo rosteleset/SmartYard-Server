c25d5a53e2c28c9e5663e0b88aede549dd1d263c
Anton Kulakov <anton.kulakoff@ya.ru>
2026-09-03 21:45:01 +0300

    feat: udpdate email utils

--- server/utils/email.php
+++ server/utils/email.php
@@ -16,8 +16,36 @@
                 $mail->SMTPAuth = true;
                 $mail->Username = $config["username"];
                 $mail->Password = $config["password"];
-                $mail->SMTPSecure = 'tls';
-                $mail->Port = $config["port"];
+
+                $port = (int)@$config["port"];
+                if ($port <= 0) {
+                    $port = 587;
+                }
+                $mail->Port = $port;
+
+                $timeout = (int)@$config["timeout"];
+                if ($timeout <= 0) {
+                    $timeout = 10;
+                }
+                $mail->Timeout = $timeout;
+
+                $secure = strtolower(trim((string)@$config["secure"]));
+
+                // Port 465 is SMTPS by convention; don't hang on STARTTLS there.
+                if ($secure === "" && $port === 465) {
+                    $secure = "smtps";
+                }
+
+                if ($secure === "ssl" || $secure === "smtps") {
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                } else
+                if ($secure === "none" || $secure === "false" || $secure === "0") {
+                    $mail->SMTPSecure = false;
+                    $mail->SMTPAutoTLS = false;
+                } else {
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                }
+
                 if (@$config["from_name"]) {
                     $mail->setFrom($config["from"], $config["from_name"]);
                 } else {
@@ -31,7 +59,14 @@
 
                 return true;
             } catch (Exception $ex) {
-                return $ex;
+                $info = trim((string)@$mail->ErrorInfo);
+                $message = $ex->getMessage();
+
+                if ($info !== "" && stripos($message, $info) === false) {
+                    return $message . " | " . $info;
+                }
+
+                return $message ?: ($info ?: "emailSendFailed");
             }
         } else {
             return false;
