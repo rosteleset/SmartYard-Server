45a6b9a4eb8eee375172e65051b4f94558397836
subs1stem <subs1stem@yandex.ru>
2026-09-07 11:15:06 +0300

    feat(omny): add VDP-10M dial plan support

--- server/hw/ip/domophone/omny/vdp10m.php
+++ server/hw/ip/domophone/omny/vdp10m.php
@@ -2,10 +2,16 @@
 
 namespace hw\ip\domophone\omny;
 
-use hw\ip\domophone\akuvox\akuvox;
+use hw\ip\domophone\akuvox\{
+    akuvox,
+    Entities\Dialplan,
+};
 
 class vdp10m extends akuvox
 {
+    /** @var array<int|string, Dialplan>|null */
+    private ?array $dialplans = null;
+
     protected static function getMaxUsers(): int
     {
         return 5000; // Found by Codex
@@ -19,12 +25,48 @@ class vdp10m extends akuvox
         array $cmsLevels = [],
     ): void
     {
-        // TODO
+        $sipNumbers = array_values(array_filter(
+            array_map('strval', $sipNumbers),
+            static fn(string $number) => $number !== '',
+        ));
+
+        $dialplan = $this->findDialplan($apartment);
+
+        if (!$sipNumbers) {
+            if ($dialplan !== null) {
+                $this->deleteDialplan($dialplan);
+            }
+
+            return;
+        }
+
+        if ($dialplan === null) {
+            $dialplan = new Dialplan($apartment);
+        }
+
+        foreach (['replace1', 'replace2', 'replace3', 'replace4', 'replace5'] as $index => $property) {
+            $dialplan->$property = $sipNumbers[$index] ?? '';
+        }
+
+        if ($dialplan->id === '-1') {
+            $this->addDialplan($dialplan);
+        } else {
+            $this->updateDialplan($dialplan);
+        }
     }
 
     public function deleteApartment(int $apartment = 0): void
     {
-        // TODO
+        if ($apartment === 0) {
+            $this->clearDialplans();
+            return;
+        }
+
+        $dialplan = $this->findDialplan($apartment);
+
+        if ($dialplan !== null) {
+            $this->deleteDialplan($dialplan);
+        }
     }
 
     public function setConciergeNumber(int $sipNumber): void
@@ -34,9 +76,101 @@ class vdp10m extends akuvox
         ]);
     }
 
+    public function syncData(): void
+    {
+        $this->dialplans = null;
+    }
+
+    protected function findDialplan(string $prefix): ?Dialplan
+    {
+        return $this->getDialplans()[$prefix] ?? null;
+    }
+
     protected function getApartments(): array
     {
-        // TODO
-        return [];
+        $apartments = [];
+
+        foreach ($this->getDialplans() as $dialplan) {
+            $prefix = $dialplan->prefix;
+
+            if (!ctype_digit($prefix) || (int)$prefix <= 0) {
+                continue;
+            }
+
+            $sipNumbers = array_values(array_filter([
+                $dialplan->replace1,
+                $dialplan->replace2,
+                $dialplan->replace3,
+                $dialplan->replace4,
+                $dialplan->replace5,
+            ], static fn(string $number) => $number !== ''));
+
+            $apartment = (int)$prefix;
+            $apartments[$apartment] = [
+                'apartment' => $apartment,
+                'code' => 0,
+                'sipNumbers' => $sipNumbers,
+                'cmsEnabled' => false,
+                'cmsLevels' => [],
+            ];
+        }
+
+        return $apartments;
+    }
+
+    /** @return array<int|string, Dialplan> */
+    protected function getDialplans(): array
+    {
+        if ($this->dialplans === null) {
+            $response = $this->apiCall('/dialreplace/get');
+
+            $this->dialplans = [];
+            foreach ($response['data']['item'] ?? [] as $item) {
+                $dialplan = Dialplan::fromArray($item);
+                $this->dialplans[$dialplan->prefix] = $dialplan;
+            }
+        }
+
+        return $this->dialplans;
+    }
+
+    private function addDialplan(Dialplan $dialplan): void
+    {
+        $response = $this->apiCall('', 'POST', [
+            'target' => 'dialreplace',
+            'action' => 'add',
+            'data' => ['item' => [$dialplan->toArray()]],
+        ]);
+
+        $dialplan->id = $response['data']['item'][0]['ID'] ?? '-1';
+        $this->dialplans[$dialplan->prefix] = $dialplan;
+    }
+
+    private function clearDialplans(): void
+    {
+        $this->apiCall('/dialreplace/clear');
+        $this->dialplans = [];
+    }
+
+    private function deleteDialplan(Dialplan $dialplan): void
+    {
+        $this->apiCall('', 'POST', [
+            'target' => 'dialreplace',
+            'action' => 'del',
+            'data' => ['item' => [['ID' => $dialplan->id]]],
+        ]);
+
+        unset($this->dialplans[$dialplan->prefix]);
+    }
+
+    private function updateDialplan(Dialplan $dialplan): void
+    {
+        $this->apiCall('', 'POST', [
+            'target' => 'dialreplace',
+            'action' => 'set',
+            'data' => ['item' => [$dialplan->toArray()]],
+        ]);
+
+        $this->dialplans[$dialplan->prefix] = $dialplan;
     }
 }
